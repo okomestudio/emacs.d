16b3dc69cb74920d405ec9bde189c09cfa846ce6
Taro Sato <okomestudio@gmail.com>
2021-08-30 21:31:25 -0700

    Add variables to safe-local-variable

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -6,7 +6,16 @@
   :config
   (projectile-mode +1)
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+  :init
+  (put 'projectile-project-compilation-cmd 'safe-local-variable #'stringp)
+  (put 'projectile-project-compilation-dir 'safe-local-variable #'stringp)
+  (put 'projectile-project-configure-cmd 'safe-local-variable #'stringp)
+  (put 'projectile-project-install-cmd 'safe-local-variable #'stringp)
+  (put 'projectile-project-package-cmd 'safe-local-variable #'stringp)
+  (put 'projectile-project-run-cmd 'safe-local-variable #'stringp)
+  (put 'projectile-project-test-cmd 'safe-local-variable #'stringp))
 
 (provide 'init-projectile)
 ;;; init-projectile.el ends here
