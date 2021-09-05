8bbd808ed3b57dce9606a76768f90da484c47548
Taro Sato <okomestudio@gmail.com>
2021-09-04 22:28:44 -0700

    Clean up yascroll

--- lisp/init-yascroll.el
+++ lisp/init-yascroll.el
@@ -2,12 +2,22 @@
 ;;; Commentary:
 ;;; Code:
 
+
+;; yascroll.el - Yet Another Scroll Bar Mode for GNU Emacs
+;; -------------------------------------------------------
+;; https://github.com/emacsorphanage/yascroll
 (use-package yascroll
   :init
   (ensure-file-from-github "emacsorphanage/yascroll/master/yascroll.el")
-  (when (not window-system)
-    (require 'yascroll)
-    (global-yascroll-bar-mode 1)))
+
+  (if (daemonp)
+      (add-hook 'after-make-frame-functions
+                (lambda (frame)
+                  (with-selected-frame frame
+                    (when (not window-system)
+                      (yascroll-bar-mode +1)))))
+    (when (not window-system)
+      (yascroll-bar-mode +1))))
 
 (provide 'init-yascroll)
 ;;; init-yascroll.el ends here
