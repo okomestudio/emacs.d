5911e1c1696b7c39ce09506b1a81cca79ac5aff2
Taro Sato <okomestudio@gmail.com>
2021-09-04 20:23:22 -0700

    Clean up edit-server

--- lisp/init-edit-server.el
+++ lisp/init-edit-server.el
@@ -3,13 +3,13 @@
 ;;; Code:
 
 ;; Server that responds to edit requests from Chrome
+;; -------------------------------------------------
+;; https://github.com/stsquad/emacs_chrome
+;; https://www.emacswiki.org/emacs/Edit_with_Emacs
 (use-package edit-server
-  :if window-system
-  :ensure t
-  :defer t
   :init
-  (add-hook 'after-init-hook 'server-start t)
-  (add-hook 'after-init-hook 'edit-server-start t))
+  (when (daemonp)
+    (edit-server-start)))
 
 (provide 'init-edit-server)
 ;;; init-edit-server.el ends here
