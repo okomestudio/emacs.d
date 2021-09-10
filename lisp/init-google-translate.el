6f4da5694446705f7570af7b8a193bf3e30d6090
Taro Sato <okomestudio@gmail.com>
2021-09-10 10:11:14 -0700

    Fix google-translate

--- lisp/init-google-translate.el
+++ lisp/init-google-translate.el
@@ -2,22 +2,20 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Visual Popup User Interface
-(use-package popup)
-
 ;; Emacs interface to Google Translate
 ;; -----------------------------------
 ;; https://github.com/atykhonov/google-translate
 (use-package google-translate
-  :after popup
-
   :bind
   (("C-c t" . 'google-translate-smooth-translate))
 
   :custom
+  (google-translate-backend-method 'curl)
+  (google-translate-output-destination nil)
   (google-translate-translation-directions-alist '(("ja" . "en") ("en" . "ja")))
 
   :config
+  (defun google-translate--search-tkk () "Search TKK." (list 430675 2721866130))
   (require 'google-translate-smooth-ui))
 
 (provide 'init-google-translate)
