a0d2ff973e76886e3dcb13303332414cce50aca0
Taro Sato <okomestudio@gmail.com>
2021-09-12 21:16:21 -0700

    Update gnus config

--- lisp/init-gnus.el
+++ lisp/init-gnus.el
@@ -3,13 +3,22 @@
 ;;; Code:
 
 (use-package gnus
-  :custom
-  (gnus-home-directory "~/.config/gnus/")
-  (gnus-init-file (concat gnus-home-directory "gnus.el"))
-  (gnus-directory (concat gnus-home-directory "news/"))
-  (message-directory (concat gnus-home-directory "mail/"))
-  (nnfolder-directory (concat gnus-home-directory "mail/archive/"))
-  (gnus-dribble-directory gnus-home-directory))
+  :custom (gnus-summary-line-format "%U%R%z%I%(%[%o: %-23,23f%]%) %s\\n")
+
+  :hook ((gnus-summary-mode . (lambda () (run-with-idle-timer 0.1 nil 'ts/gnus-show-all))))
+
+  :init
+  (setq gnus-home-directory "~/.config/gnus/")
+  (setq gnus-init-file (concat gnus-home-directory "gnus.el"))
+  (setq gnus-directory (concat gnus-home-directory "news/"))
+  (setq message-directory (concat gnus-home-directory "mail/"))
+  (setq nnfolder-directory (concat gnus-home-directory "mail/archive/"))
+  (setq gnus-dribble-directory gnus-home-directory)
+
+  :config
+  (defun ts/gnus-show-all ()
+    (interactive)
+    (gnus-summary-insert-old-articles t)) )
 
 (provide 'init-gnus)
 ;;; init-gnus.el ends here
