e9ecbf4108ba2944e2adefa2b2fef38f783018ff
Taro Sato <okomestudio@gmail.com>
2021-09-17 21:28:30 -0700

    Add init-slack

---
+++ lisp/init-slack.el
@@ -0,0 +1,54 @@
+;;; init-slack.el --- Slack  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+; (el-get-bundle slack)
+
+(use-package slack
+  :commands (slack-start)
+
+  :init
+  (setq slack-buffer-emojify t) ;; if you want to enable emoji, default nil
+  (setq slack-prefer-current-team t)
+
+  :config
+  (slack-register-team
+   :name "emacs-slack"
+   :default t
+   :token "xoxc-..."
+   :subscribed-channels '(general random)
+   :full-and-display-names t)
+
+  ;; (evil-define-key 'normal slack-info-mode-map
+  ;;   ",u" 'slack-room-update-messages)
+
+  ;; (evil-define-key 'normal slack-mode-map
+  ;;   ",c" 'slack-buffer-kill
+  ;;   ",ra" 'slack-message-add-reaction
+  ;;   ",rr" 'slack-message-remove-reaction
+  ;;   ",rs" 'slack-message-show-reaction-users
+  ;;   ",pl" 'slack-room-pins-list
+  ;;   ",pa" 'slack-message-pins-add
+  ;;   ",pr" 'slack-message-pins-remove
+  ;;   ",mm" 'slack-message-write-another-buffer
+  ;;   ",me" 'slack-message-edit
+  ;;   ",md" 'slack-message-delete
+  ;;   ",u" 'slack-room-update-messages
+  ;;   ",2" 'slack-message-embed-mention
+  ;;   ",3" 'slack-message-embed-channel
+  ;;   "\C-n" 'slack-buffer-goto-next-message
+  ;;   "\C-p" 'slack-buffer-goto-prev-message)
+  ;;  (evil-define-key 'normal slack-edit-message-mode-map
+  ;;   ",k" 'slack-message-cancel-edit
+  ;;   ",s" 'slack-message-send-from-buffer
+  ;;   ",2" 'slack-message-embed-mention
+  ;;   ",3" 'slack-message-embed-channel)
+   )
+
+(use-package alert
+  :commands (alert)
+  :init
+  (setq alert-default-style 'notifier))
+
+(provide 'init-slack)
+;;; init-slack.el ends here
