01f6eb136979bdddbe310c547b9614b1e91cbcb8
Taro Sato <okomestudio@gmail.com>
2021-11-05 10:01:28 -0700

    Add winum

---
+++ lisp/init-winum.el
@@ -0,0 +1,10 @@
+;;; init-winum.el --- Window numbers  -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(use-package winum
+  :init
+  (winum-mode))
+
+(provide 'init-winum)
+;;; init-winum.el ends here
