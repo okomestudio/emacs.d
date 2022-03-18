490b111f1a4391138aa902dcfcf6e38c1ab24ff2
Taro Sato <okomestudio@gmail.com>
2022-03-18 15:28:49 -0700

    Tweak org-modern

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -26,6 +26,7 @@
    (org-startup-folded t)
    (org-startup-indented t)
    (org-support-shift-select t)
+   (org-tags-column 0)
    (org-todo-keywords '((sequence "TODO" "|" "DONE" "SKIP"))))
 
   :hook
@@ -78,10 +79,54 @@
 ;; org-modern - Modern Org Style
 ;; https://github.com/minad/org-modern
 (use-package org-modern
-  :disabled
-  :hook
-  ((org-mode . org-modern-mode)
-   (org-agenda-finalize . org-modern-agenda)))
+  :config
+  ;; (set-face-attribute 'default nil :family "Hack")
+  ;; (set-face-attribute 'variable-pitch nil :family "Hack")
+
+  ;; (modify-all-frames-parameters
+  ;;  '((right-divider-width . 40)
+  ;;    (internal-border-width . 40)))
+  (dolist (face '(window-divider
+                  window-divider-first-pixel
+                  window-divider-last-pixel))
+    (face-spec-reset-face face)
+    (set-face-foreground face (face-attribute 'default :background)))
+  (set-face-background 'fringe (face-attribute 'default :background))
+
+  (setq org-auto-align-tags nil
+        org-tags-column 0
+        org-catch-invisible-edits 'show-and-error
+        org-special-ctrl-a/e t
+        org-insert-heading-respect-content t
+
+        ;; Org styling, hide markup etc.
+        org-hide-emphasis-markers t
+        org-pretty-entities t
+        org-ellipsis "…"
+
+        ;; Agenda styling
+        org-agenda-block-separator ?─
+        org-agenda-time-grid
+        '((daily today require-timed)
+          (800 1000 1200 1400 1600 1800 2000)
+          " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+        org-agenda-current-time-string
+        "⭠ now ─────────────────────────────────────────────────")
+
+  ;; :hook
+  ;; ((org-mode . ts/org-modern-mode)
+  ;;  (org-agenda-finalize . org-modern-agenda))
+
+  :init
+  (defun ts/org-modern-mode ()
+    (interactive)
+    (menu-bar-mode -1)
+    (tool-bar-mode -1)
+    (scroll-bar-mode -1)
+    (modus-themes-load-operandi)
+    (org-indent-mode -1)
+    (org-modern-mode))
+  )
 
 (use-package org-roam
   :custom
