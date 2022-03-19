7ee6a170a1ff163cecb70cdf77a2eac0de715331
Taro Sato <okomestudio@gmail.com>
2022-03-18 23:57:30 -0700

    Clean up org-modern

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -79,54 +79,22 @@
 ;; org-modern - Modern Org Style
 ;; https://github.com/minad/org-modern
 (use-package org-modern
-  :config
-  ;; (set-face-attribute 'default nil :family "Hack")
-  ;; (set-face-attribute 'variable-pitch nil :family "Hack")
+  :custom
+  (org-modern-block nil)
+  (org-modern-keyword t)
+  (org-modern-priority t)
+  (org-modern-star nil)
+  (org-modern-statistics t)
+  (org-modern-table nil)
+  (org-modern-tag t)
+  (org-modern-timestamp t)
+  (org-modern-todo t)
+  (org-modern-variable-pitch nil)
 
-  ;; (modify-all-frames-parameters
-  ;;  '((right-divider-width . 40)
-  ;;    (internal-border-width . 40)))
-  (dolist (face '(window-divider
-                  window-divider-first-pixel
-                  window-divider-last-pixel))
-    (face-spec-reset-face face)
-    (set-face-foreground face (face-attribute 'default :background)))
-  (set-face-background 'fringe (face-attribute 'default :background))
-
-  (setq org-auto-align-tags nil
-        org-tags-column 0
-        org-catch-invisible-edits 'show-and-error
-        org-special-ctrl-a/e t
-        org-insert-heading-respect-content t
-
-        ;; Org styling, hide markup etc.
-        org-hide-emphasis-markers t
-        org-pretty-entities t
-        org-ellipsis "…"
-
-        ;; Agenda styling
-        org-agenda-block-separator ?─
-        org-agenda-time-grid
-        '((daily today require-timed)
-          (800 1000 1200 1400 1600 1800 2000)
-          " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
-        org-agenda-current-time-string
-        "⭠ now ─────────────────────────────────────────────────")
-
-  ;; :hook
-  ;; ((org-mode . ts/org-modern-mode)
-  ;;  (org-agenda-finalize . org-modern-agenda))
-
-  :init
-  (defun ts/org-modern-mode ()
-    (interactive)
-    (menu-bar-mode -1)
-    (tool-bar-mode -1)
-    (scroll-bar-mode -1)
-    (modus-themes-load-operandi)
-    (org-indent-mode -1)
-    (org-modern-mode))
-  )
+  :hook
+  ((org-mode . org-modern-mode)
+   ;; (org-agenda-finalize . org-modern-agenda)
+   ))
 
 (use-package org-roam
   :custom
