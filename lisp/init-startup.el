c02ab9a5f4ba01fc82b763300300c073f8f9e906
Taro Sato <okomestudio@gmail.com>
2022-03-26 14:26:44 -0700

    Move shackle

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -216,6 +216,20 @@ top down to the current directory.")
   :custom
   (aw-dispatch-always t))
 
+;; shackle - Enforce rules for popup windows
+;; https://depp.brause.cc/shackle/
+(use-package shackle
+  :custom
+  (shackle-default-alignment 'below)
+  (shackle-default-size 0.4)
+  ;; (shackle-rules '(("*Warnings*"
+  ;;                   :select nil :size 0.25)
+  ;;                  (magit-status-mode
+  ;;                   :align right :size 0.5 :inhibit-window-quit t :other t)))
+
+  :config
+  (shackle-mode 1))
+
 (use-package mwheel
   :ensure nil
   :custom
