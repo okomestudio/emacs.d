e8b7cc709c7e8cfd744d1e4027e265c99b9d2724
Taro Sato <okomestudio@gmail.com>
2022-03-26 14:17:22 -0700

    Update vterm-mode

--- lisp/init-vterm.el
+++ lisp/init-vterm.el
@@ -15,9 +15,10 @@
   (cmake . "sudo apt install -y cmake libtool-bin libvterm-dev")
 
   :hook
-  ((vterm-mode . ts/vterm-configure-faces)
-   (vterm-mode . (lambda () (setq-local global-hl-line-mode nil)))
-   (vterm-mode . (lambda () (setq-local solaire-mode nil))))
+  ((vterm-mode . (lambda ()
+                   (ts/vterm-configure-faces)
+                   (setq-local global-hl-line-mode nil
+                               solaire-mode nil))))
 
   :init
   (defun ts/vterm-configure-faces ()
@@ -32,10 +33,12 @@
     ;; (set-face-attribute 'vterm-color-white nil :foreground "#000000" :background "#000000")
     (set (make-local-variable 'buffer-face-mode-face) '(:family "Hack" :foreground "#000000"))
     (buffer-face-mode t))
-  )
 
-(use-package multi-vterm
-  :after vterm)
+  (require 'vterm))
+
+;; multi-vterm.el - Managing multiple vterm buffers
+;; https://github.com/suonlight/multi-vterm
+(use-package multi-vterm)
 
 (provide 'init-vterm)
 ;;; init-vterm.el ends here
