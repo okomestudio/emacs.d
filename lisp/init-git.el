7e002c46525466eac4364cfc5e4de9c92e69658b
Taro Sato <okomestudio@gmail.com>
2022-03-26 14:17:36 -0700

    Update magit

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -2,7 +2,13 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package magit)
+;; magit.el - An interface to Git.
+;; https://github.com/magit/magit
+(use-package magit
+  :init
+  ;; workaround for https://github.com/magit/magit/pull/4445
+  (if (not (boundp 'project-switch-commands))
+      (setq project-switch-commands nil)))
 
 (provide 'init-git)
 ;;; init-git.el ends here
