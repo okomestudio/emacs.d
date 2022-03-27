29bf28b2af3aa887b85f4277ed14523f9a156560
Taro Sato <okomestudio@gmail.com>
2022-03-26 19:38:28 -0700

    Clean up projectile

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -3,27 +3,29 @@
 ;;; Code:
 
 (use-package projectile
-  :custom
-  (projectile-auto-discover t)
-  (projectile-enable-caching nil)
-  (projectile-indexing-method 'alien)
-  (projectile-project-root-functions '(projectile-root-top-down-recurring
-                                       projectile-root-top-down
-                                       projectile-root-bottom-up
-                                       projectile-root-local))
-  (projectile-project-search-path '("~/.config/emacs/" ("~/github.com/" . 2)))
-  (projectile-mode-line-function '(lambda () (format " [%s]" (projectile-project-name))))
-
-  :ensure-system-package
-  ((fdfind . "sudo apt install fd-find")
-   (ag . "sudo apt-get install silversearcher-ag"))
-
   :config
   (projectile-mode +1)
   (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map) ; "s-" is "super"
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
+  :custom
+  (projectile-auto-discover t)
+  (projectile-enable-caching nil)
+  (projectile-indexing-method 'alien)
+  (projectile-mode-line-function '(lambda () (format " [%s]" (projectile-project-name))))
+  (projectile-project-root-functions '(projectile-root-top-down-recurring
+                                       projectile-root-top-down
+                                       projectile-root-bottom-up
+                                       projectile-root-local))
+  (projectile-project-search-path '("~/.config/emacs/" ("~/github.com/" . 2)))
+
+  :ensure-system-package
+  ((ag . "sudo apt-get install silversearcher-ag")
+   (fdfind . "sudo apt install fd-find"))
+
   :init
+  (use-package ag)
+
   (put 'projectile-project-compilation-cmd 'safe-local-variable #'stringp)
   (put 'projectile-project-compilation-dir 'safe-local-variable #'stringp)
   (put 'projectile-project-configure-cmd 'safe-local-variable #'stringp)
@@ -31,8 +33,6 @@
   (put 'projectile-project-package-cmd 'safe-local-variable #'stringp)
   (put 'projectile-project-run-cmd 'safe-local-variable #'stringp)
   (put 'projectile-project-test-cmd 'safe-local-variable #'stringp))
-
-(use-package ag)
 
 (provide 'init-projectile)
 ;;; init-projectile.el ends here
