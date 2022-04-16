44b356dcaa034d9c656f63ece8967b075dc35fcb
Taro Sato <okomestudio@gmail.com>
2022-04-16 14:06:16 -0700

    Use elfeed-org

--- lisp/init-elfeed.el
+++ lisp/init-elfeed.el
@@ -11,5 +11,13 @@
     (text-scale-set 2.0)
     (toggle-truncate-lines +1)))
 
+(use-package elfeed-org
+  :config (elfeed-org)
+  :custom (rmh-elfeed-org-files (ts/rmh-elfeed-org-files))
+  :init
+  (defun ts/rmh-elfeed-org-files ()
+    (let ((conf-d-dir (concat user-emacs-directory "conf.d/")))
+      (if (file-exists-p conf-d-dir) (directory-files conf-d-dir t "^elfeed.*\\.org$")))))
+
 (provide 'init-elfeed)
 ;;; init-elfeed.el ends here
