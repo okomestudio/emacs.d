19ceffe5849c1a7e46d94cfae99df8777cda1323
Taro Sato <okomestudio@gmail.com>
2022-05-02 22:22:10 -0700

    Adjust pitch heights

--- lisp/init-faces.el
+++ lisp/init-faces.el
@@ -8,7 +8,6 @@
     (let ((monn (length (display-monitor-attributes-list))))
       (/ (display-pixel-width) monn)))
 
-
 (defcustom ts/default-font-size
   (let ((display-width (ts/display-width)))
       (if (and display-width (> display-width 2550))
@@ -17,8 +16,39 @@
   :type '(float)
   :group 'ts)
 
+(defcustom ts/fixed-pitch-height
+  (let ((display-width (ts/display-width)))
+    (if (and display-width (> display-width 2550))
+        180 150))
+  "Default fixed pitch font height."
+  :type '(integer)
+  :group 'ts)
+
+(defcustom ts/variable-pitch-height
+  (let ((display-width (ts/display-width)))
+    (if (and display-width (> display-width 2550))
+        180 180))
+  "Default variable pitch font height."
+  :type '(integer)
+  :group 'ts)
+
+(defun ts/font-exists-p (font)
+  "Check if FONT exists."
+  (if (null (x-list-fonts font)) nil t))
+
+(defun ts/set-fallback-cjk-font (fontset-name)
+  (let ((font-family (seq-find #'ts/font-exists-p '(;; "HackGen"
+                                                    "VL Gothic"
+                                                    "Noto Sans Mono CJK JP"))))
+    (set-fontset-font fontset-name
+                      'unicode
+                      (font-spec :family font-family)
+                      nil
+                      'append)))
+
 
 (use-package faces
+  :disabled
   :ensure nil
 
   :init
@@ -36,10 +66,6 @@
             (select-frame (selected-frame))
             (apply action)))))
 
-  (defun ts/font-exists-p (font)
-    "Check if font exists."
-    (if (null (x-list-fonts font)) nil t))
-
   (defun ts/create-cjk-hybrid-fontset (size name)
     "Create a CJK hybrid fontset of SIZE named fontset-NAME
 
@@ -50,22 +76,33 @@ See https://knowledge.sakura.ad.jp/8494/"
       (ts/set-fallback-cfk-font fontset-name)
       fontset-name))
 
-  (defun ts/set-fallback-cjk-font (fontset-name)
-    (let ((font-family (seq-find #'ts/font-exists-p '(;; "HackGen"
-                                                      "VL Gothic"
-                                                      "Noto Sans Mono CJK JP"))))
-      (set-fontset-font fontset-name
-                        'unicode
-                        (font-spec :family font-family)
-                        nil
-                        'append)))
-
   (defun ts/setup-frame ()
     (defvar ts/default-font (font-spec :family "Hack" :size ts/default-font-size))
     (set-frame-font ts/default-font)
     (ts/set-fallback-cjk-font nil))
 
-  (ts/apply-if-gui 'ts/setup-frame))
+  (ts/apply-if-gui 'ts/setup-frame)
+
+  (add-hook 'after-make-frame-functions
+            (lambda (frame)
+              (select-frame frame)
+              (ts/apply-if-gui 'ts/setup-frame))))
+
+(use-package mixed-pitch
+  :hook (text-mode . mixed-pitch-mode)
+
+  :config
+  (set-face-attribute 'default nil :font "Hack" :height ts/fixed-pitch-height)
+  (set-face-attribute 'fixed-pitch nil :font "Hack" :height ts/fixed-pitch-height)
+  (set-face-attribute 'variable-pitch nil :font "EB Garamond" :height ts/variable-pitch-height)
+  (ts/set-fallback-cjk-font nil)
+
+  (dolist (element '(("VL Gothic" . 0.85)))
+    (add-to-list 'face-font-rescale-alist element))
+
+  :custom
+  (mixed-pitch-set-height t)
+  (mixed-pitch-variable-pitch-cursor nil))
 
 ;; eaw.el - East Asian Ambiguous Width問題と絵文字の横幅問題の修正ロケール
 ;; https://github.com/hamano/locale-eaw
