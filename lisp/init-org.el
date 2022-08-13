4a9400f609938d5a5f92a2722f6b7dc29e8ecc6f
Taro Sato <okomestudio@gmail.com>
2022-08-13 08:16:32 -0700

    Fix override

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -179,45 +179,6 @@
   ("C-c n d" . org-roam-dailies-map)
 
   :config
-  (cl-defmethod org-roam-node-slug ((node org-roam-node))
-    "Return the slug of NODE. Overridden to use hyphens instead of underscores."
-    (let ((title (org-roam-node-title node))
-          (slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
-                             768      ; U+0300 COMBINING GRAVE ACCENT
-                             769      ; U+0301 COMBINING ACUTE ACCENT
-                             770      ; U+0302 COMBINING CIRCUMFLEX ACCENT
-                             771      ; U+0303 COMBINING TILDE
-                             772      ; U+0304 COMBINING MACRON
-                             774      ; U+0306 COMBINING BREVE
-                             775      ; U+0307 COMBINING DOT ABOVE
-                             776      ; U+0308 COMBINING DIAERESIS
-                             777      ; U+0309 COMBINING HOOK ABOVE
-                             778      ; U+030A COMBINING RING ABOVE
-                             779      ; U+030B COMBINING DOUBLE ACUTE ACCENT
-                             780      ; U+030C COMBINING CARON
-                             795      ; U+031B COMBINING HORN
-                             803      ; U+0323 COMBINING DOT BELOW
-                             804      ; U+0324 COMBINING DIAERESIS BELOW
-                             805      ; U+0325 COMBINING RING BELOW
-                             807      ; U+0327 COMBINING CEDILLA
-                             813      ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
-                             814      ; U+032E COMBINING BREVE BELOW
-                             816      ; U+0330 COMBINING TILDE BELOW
-                             817      ; U+0331 COMBINING MACRON BELOW
-                             )))
-      (cl-flet* ((nonspacing-mark-p (char) (memq char slug-trim-chars))
-                 (strip-nonspacing-marks (s) (string-glyph-compose
-                                              (apply #'string
-                                                     (seq-remove #'nonspacing-mark-p
-                                                                 (string-glyph-decompose s)))))
-                 (cl-replace (title pair) (replace-regexp-in-string (car pair) (cdr pair) title)))
-        (let* ((pairs `(("[^[:alnum:][:digit:]]" . "-") ;; convert anything not alphanumeric
-                        ("--*" . "-") ;; remove sequential underscores
-                        ("^-" . "")   ;; remove starting underscore
-                        ("-$" . ""))) ;; remove ending underscore
-               (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs)))
-          (downcase slug)))))
-
   (require 'org-roam-dailies)
   (org-roam-db-autosync-mode)
 
@@ -234,6 +195,47 @@
   (org-roam-node-display-template "${title} ${tags}")
 
   :init
+  ;; Override some functions here:
+  (eval-after-load 'org-roam-node
+    '(cl-defmethod org-roam-node-slug ((node org-roam-node))
+       "Return the slug of NODE. Overridden to use hyphens instead of underscores."
+       (let ((title (org-roam-node-title node))
+             (slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
+                                768   ; U+0300 COMBINING GRAVE ACCENT
+                                769   ; U+0301 COMBINING ACUTE ACCENT
+                                770   ; U+0302 COMBINING CIRCUMFLEX ACCENT
+                                771   ; U+0303 COMBINING TILDE
+                                772   ; U+0304 COMBINING MACRON
+                                774   ; U+0306 COMBINING BREVE
+                                775   ; U+0307 COMBINING DOT ABOVE
+                                776   ; U+0308 COMBINING DIAERESIS
+                                777   ; U+0309 COMBINING HOOK ABOVE
+                                778   ; U+030A COMBINING RING ABOVE
+                                779   ; U+030B COMBINING DOUBLE ACUTE ACCENT
+                                780   ; U+030C COMBINING CARON
+                                795   ; U+031B COMBINING HORN
+                                803   ; U+0323 COMBINING DOT BELOW
+                                804   ; U+0324 COMBINING DIAERESIS BELOW
+                                805   ; U+0325 COMBINING RING BELOW
+                                807   ; U+0327 COMBINING CEDILLA
+                                813   ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
+                                814   ; U+032E COMBINING BREVE BELOW
+                                816   ; U+0330 COMBINING TILDE BELOW
+                                817   ; U+0331 COMBINING MACRON BELOW
+                                )))
+         (cl-flet* ((nonspacing-mark-p (char) (memq char slug-trim-chars))
+                    (strip-nonspacing-marks (s) (string-glyph-compose
+                                                 (apply #'string
+                                                        (seq-remove #'nonspacing-mark-p
+                                                                    (string-glyph-decompose s)))))
+                    (cl-replace (title pair) (replace-regexp-in-string (car pair) (cdr pair) title)))
+           (let* ((pairs `(("[^[:alnum:][:digit:]]" . "-") ;; convert anything not alphanumeric
+                           ("--*" . "-") ;; remove sequential underscores
+                           ("^-" . "")   ;; remove starting underscore
+                           ("-$" . ""))) ;; remove ending underscore
+                  (slug (-reduce-from #'cl-replace (strip-nonspacing-marks title) pairs)))
+             (downcase slug))))))
+
   (put 'org-roam-capture-templates 'safe-local-variable #'listp)
   (put 'org-roam-dailies-capture-templates 'safe-local-variable #'listp)
   (put 'org-roam-db-location 'safe-local-variable #'stringp)
