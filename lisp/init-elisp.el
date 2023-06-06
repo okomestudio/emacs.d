0dfea6d926ae02cdbb3e965f0fe740bcb2d12131
Taro Sato <okomestudio@gmail.com>
2023-06-05 23:54:25 -0700

    Clean up elisp

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -2,24 +2,65 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package emacs-lisp-mode
-  :straight nil
-  :hook ((emacs-lisp-mode . eldoc-mode)))
+(use-package elsa
+  :disabled
+  :ensure-system-package
+  (eask . "curl -fsSL https://raw.githubusercontent.com/emacs-eask/cli/master/webinstall/install.sh | sh")
 
-(use-package lispy
+  ;; :config (elsa-lsp-register)
+  )
+
+(use-package erefactor)
+
+(use-package eros
+  :hook (emacs-lisp-mode . eros-mode))
+
+(use-package flycheck-elsa
+  :disabled
+  :custom (flycheck-elsa-backend 'eask)
+  :hook (emacs-lisp-mode . flycheck-elsa-setup))
+
+(use-package flycheck-package
+  :hook (emacs-lisp-mode . flycheck-package-setup))
+
+(use-package ipretty
   :defer t
-  :hook ((emacs-lisp-mode . lispy-mode))
-  :config
-  (unbind-key "M-o" lispy-mode-map)
-  (unbind-key "M-S-o" lispy-mode-map))
+  :config (ipretty-mode 1))
+
+(use-package nameless
+  :hook (emacs-lisp-mode .  nameless-mode)
+
+  :custom
+  (nameless-global-aliases '())
+  (nameless-private-prefix t))
 
 (use-package paredit
   :defer t
-  :hook ((emacs-lisp-mode . my/paredit-hook))
+  :hook ((emacs-lisp-mode . ts/paredit-hook))
   :config
-  (defun my/paredit-hook ()
+  (defun ts/paredit-hook ()
     (electric-pair-mode -1)
     (enable-paredit-mode)))
+
+(use-package suggest
+  :defer t)
+
+;; Syntax highlighting
+
+(use-package highlight-defined
+  :custom (highlight-defined-face-use-itself t)
+
+  :hook
+  (help-mode . highlight-defined-mode)
+  (emacs-lisp-mode . highlight-defined-mode))
+
+(use-package highlight-quoted
+  :hook
+  (emacs-lisp-mode . highlight-quoted-mode))
+
+(use-package highlight-sexp
+  :custom (hl-sexp-background-color "gray95")
+  :hook (emacs-lisp-mode . highlight-sexp-mode))
 
 (provide 'init-elisp)
 ;;; init-elisp.el ends here
