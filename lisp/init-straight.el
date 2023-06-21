a01ff9d3d49bd5cd4b8efe14e09ada177b573b6a
Taro Sato <okomestudio@gmail.com>
2023-06-21 15:23:52 -0700

    Clean up

--- lisp/init-straight.el
+++ lisp/init-straight.el
@@ -2,30 +2,22 @@
 ;;; Commentary:
 ;;; Code:
 
-(defvar bootstrap-version)
-
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 6))
+(let ((bootstrap-file (expand-file-name "straight/repos/straight.el/bootstrap.el"
+                                        user-emacs-directory))
+      (url (concat "https://raw.githubusercontent.com/"
+                   "radian-software/straight.el/develop/install.el")))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
-	      (url-retrieve-synchronously
-	       "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-	       'silent 'inhibit-cookies)
+	      (url-retrieve-synchronously url
+	                                  'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
 (straight-use-package 'use-package)
 (straight-use-package 'use-package-ensure-system-package)
-(setq straight-use-package-by-default t)
 
-;; (use-package auto-package-update
-;;   :config
-;;   (auto-package-update-maybe)
-;;   :custom
-;;   (auto-package-update-delete-old-versions t)
-;;   (auto-package-update-interval 7))
+(setq straight-use-package-by-default t)
 
 (provide 'init-straight)
 ;;; init-straight.el ends here
