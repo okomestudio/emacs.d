6db692e315801133e57a63350282de07b40958be
Taro Sato <okomestudio@gmail.com>
2023-06-21 14:57:25 -0700

    Clean up

--- early-init.el
+++ early-init.el
@@ -1,40 +1,36 @@
 ;;; early-init.el  -*- lexical-binding: t -*-
-
+;;;
 ;;; Commentary:
-
-;; Provides early initialization for Emacs > 27.1.
-
+;;;
+;;; Provides early initialization for Emacs > 27.1.
+;;;
 ;;; Code:
 
-;; Set to t when debugging startup issues:
-(setq debug-on-error nil)
+(setq debug-on-error nil)               ; set t when debugging startup issues:
 
-
-(let ((minver "27.2"))
+(let ((minver "29.0"))
   (when (version< emacs-version minver)
-    (error "The minimum Emacs version for init.el is Version %s" minver)))
+    (error "The minimum Emacs version expected is %s" minver)))
 
-
-;; Reduce GC usage while initialization. 800 kb is the default at 2021-08-01.
+;; Reduce GC usage while initialization. 800 kb is the default (2021-08-01):
 (let ((init-gc-cons-threshold most-positive-fixnum))
   (setq gc-cons-threshold init-gc-cons-threshold)
   (add-hook 'emacs-startup-hook
             (lambda () (setq gc-cons-threshold 800000))))
-
 
 (setq package-enable-at-startup nil)
 
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(vertical-scroll-bars) default-frame-alist)
+
 (menu-bar-mode 1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
 (setq frame-inhibit-implied-resize t)
 
-(setq native-comp-deferred-compilation t)
+(setq native-comp-jit-compilation t)
 
 (provide 'early-init)
-
-;;; early-init ends here
+;;; early-init.el ends here
