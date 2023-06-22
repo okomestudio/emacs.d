388912104c006bf9a8c3de4e7d4aa71b8937dc55
Taro Sato <okomestudio@gmail.com>
2023-06-21 20:06:14 -0700

    Clean up

--- lisp/init-ime.el
+++ lisp/init-ime.el
@@ -7,11 +7,16 @@
   (default-input-method "japanese-mozc")
   (mozc-candidate-style 'posframe)
 
-  :ensure-system-package ("/usr/bin/mozc_emacs_helper" . "sudo apt install -y emacs-mozc-bin mozc-server"))
+  :ensure-system-package
+  ("/usr/bin/mozc_emacs_helper" . "sudo apt install -y emacs-mozc-bin mozc-server"))
+
 
 (use-package mozc-posframe
-  :straight (mozc-posframe :type git :host github :repo "derui/mozc-posframe")
-  :config (mozc-posframe-register))
+  :straight
+  (mozc-posframe :type git :host github :repo "derui/mozc-posframe")
+
+  :config
+  (mozc-posframe-register))
 
 (provide 'init-ime)
 ;;; init-ime.el ends here
