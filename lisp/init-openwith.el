7ca662907e0242c0789120c086d9d6853ab06fe7
Taro Sato <okomestudio@gmail.com>
2023-06-21 20:14:50 -0700

    Clean up

--- lisp/init-openwith.el
+++ lisp/init-openwith.el
@@ -2,12 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
-;; openwith.el - Associate external applications with files
+
 (use-package openwith
-  :custom ((openwith-associations '(("\\.pdf\\'" "okular" (file)))))
+  ;; Associate external applications with files.
+  :custom
+  (openwith-associations '(("\\.pdf\\'" "okular" (file))))
+
   :init
-  (ensure-file-from-url "https://www.metalevel.at/misc/openwith.el")
   (openwith-mode t))
+
 
 (provide 'init-openwith)
 ;;; init-openwith.el ends here
