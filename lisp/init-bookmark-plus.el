7c793ff11844eed9d74db37e1638913f47e3667a
Taro Sato <okomestudio@gmail.com>
2023-07-01 12:37:41 -0700

    Update bookmarks plus

--- lisp/init-bookmark-plus.el
+++ lisp/init-bookmark-plus.el
@@ -2,10 +2,21 @@
 ;;; Commentary:
 ;;; Code:
 
+
 (use-package bookmark+
   ;; Enhances vanilla Emacs bookmarks in many way.
   :defer 2
-  :init (require 'bookmark+))
+
+  :custom
+  (bmkp-bmenu-state-file (concat user-emacs-directory
+                                 ".cache/bmk-bmenu-state.el"))
+  (bmkp-bmenu-commands-file (concat user-emacs-directory
+                                    ".cache/bmk-bmenu-commands.el"))
+  (bmkp-last-as-first-bookmark-file nil)
+
+  :init
+  (require 'bookmark+))
+
 
 (provide 'init-bookmark-plus)
 ;;; init-bookmark-plus.el ends here
