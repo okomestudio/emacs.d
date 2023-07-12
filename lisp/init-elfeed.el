dd3cbd92629f6a054550ec8fb7fb6bf5ecebb1db
Taro Sato <okomestudio@gmail.com>
2023-07-12 16:56:12 -0700

    Update Elfeed

--- lisp/init-elfeed.el
+++ lisp/init-elfeed.el
@@ -1,4 +1,4 @@
-;;; init-elfeed.el --- Elisp  -*- lexical-binding: t -*-
+;;; init-elfeed.el --- Elfeed  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
@@ -25,12 +25,33 @@
 
   :preface
   (defun init-elfeed--rmh-elfeed-org-files ()
+    "Read elfeed-org files under conf.d."
     (let ((conf-d-dir (concat user-emacs-directory "conf.d/")))
       (if (file-exists-p conf-d-dir)
           (directory-files conf-d-dir t "^elfeed.*\\.org$"))))
 
   :config
   (elfeed-org))
+
+
+(use-package elfeed-goodies
+  :custom
+  (elfeed-goodies/entry-pane-position 'bottom)
+  (elfeed-goodies/entry-pane-size 0.8)
+
+  :config
+  (elfeed-goodies/setup)
+
+  ;; Keymap hack to prevent elfeed-goodies from stealing M-v. See
+  ;; https://emacs.stackexchange.com/a/65138/599.
+  (defvar init-elfeed--map (make-keymap))
+  (define-minor-mode init-elfeed--mode
+    "Dummy minor mode for keymap hack."
+    :init-value t
+    :global t
+    :keymap init-elfeed--map)
+  (add-to-list 'emulation-mode-map-alists `((init-elfeed--mode) . ,init-elfeed--map))
+  (define-key init-elfeed--map (kbd "M-v") 'scroll-down-command))
 
 
 (provide 'init-elfeed)
