40509680a6fe0ea7b0fff0ba388f10a339abc4e9
Taro Sato <okomestudio@gmail.com>
2023-10-12 13:23:51 -0700

    Adjust margin

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -158,7 +158,7 @@ node."
                     :image-output-type "svg"
                     :image-size-adjust (1.7 . 1.5)
                     :latex-compiler ("lualatex -interaction nonstopmode --shell-escape -output-directory %o %f")
-                    :image-converter ("pdfcropmargins -v -p 0 %f -o /tmp/cropped.pdf ; dvisvgm -P /tmp/cropped.pdf -n -b min -c %S -o %O"))
+                    :image-converter ("pdfcropmargins -v -p 0 -a -5 %f -o /tmp/cropped.pdf ; dvisvgm -P /tmp/cropped.pdf -n -b min -c %S -o %O"))
           (lualatexdvi :programs ("lualatex" "dvisvgm")
                     :description "dvi > svg"
                     :message "you need to install the programs: lualatex and dvisvgm."
