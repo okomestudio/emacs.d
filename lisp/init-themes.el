d9710c360b55cc8f7680c5ce5c1e3d91561e616c
Taro Sato <okomestudio@gmail.com>
2023-10-17 12:21:33 -0700

    Update theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -3,47 +3,59 @@
 ;;; Code:
 
 
+(defun init-themes--configure-theme (theme)
+  "Configure and load THEME."
+  (if (daemonp)
+      (add-hook 'after-make-frame-functions
+                (lambda (frame)
+                  (with-selected-frame frame
+                    (load-theme theme t))))
+    (load-theme theme t)))
+
+
+(defun init-themes--scale-color (orig factor)
+  "Make color ORIG lighter or darker by FACTOR."
+  (let* ((result 0.0))
+    (cl-loop for pow downfrom 2
+             for x in orig
+             do
+             (setq result
+                   (+ result (* (expt 256 pow) (round (* x factor))))))
+    (format "#%X" result)))
+
+
+;; THEMES
+
 (use-package spacemacs-theme
+  :disabled
   :defer t
-
-  :preface
-  (defun ts/configure-theme (theme)
-    "Configure and load THEME."
-    (if (daemonp)
-        (add-hook 'after-make-frame-functions
-                  (lambda (frame)
-                    (with-selected-frame frame
-                      (load-theme theme t))))
-      (load-theme theme t)))
-
-  (defun init-themes--scale-color (orig factor)
-    (let* ((result 0.0))
-      (cl-loop for pow downfrom 2
-               for x in orig
-               do
-               (setq result
-                     (+ result (* (expt 256 pow) (round (* x factor))))))
-      (format "#%X" result)))
 
   :init
   (setq spacemacs-theme-custom-colors
-        `((base . "#322938")  ; #655370 for light, true-color
+        `((base . "#322938") ;; #655370 for light, true-color
 
           ;; Make some colors slightly darker
           (head3 . ,(init-themes--scale-color '(#x67 #xb1 #x1d) 0.80))
           (head4 . ,(init-themes--scale-color '(#xb1 #x95 #x1d) 0.80))
           (cyan . ,(init-themes--scale-color '(#x21 #xb8 #xc7) 0.95))))
 
-  (ts/configure-theme 'spacemacs-light))
+  (init-themes--configure-theme 'spacemacs-light))
 
+
+(use-package flexoki-themes
+  :defer t
+  :config (init-themes--configure-theme 'flexoki-themes-light))
+
+
+;; MINOR THEME ADJUSTMENTS
 
 (use-package solaire-mode
   ;; Distinguish "real" buffers from "unreal" buffers.
   :custom
-  (solaire-mode-real-buffer-fn 'ts/solaire-mode-real-buffer-p)
+  (solaire-mode-real-buffer-fn 'init-themes--solaire-mode-real-buffer-p)
 
   :preface
-  (defun ts/solaire-mode-real-buffer-p ()
+  (defun init-themes--solaire-mode-real-buffer-p ()
     (cond ((string-prefix-p "*elfeed" (buffer-name)) t)
           ((string-prefix-p "*Colors*" (buffer-name)) nil)
           (t (solaire-mode-real-buffer-p))))
