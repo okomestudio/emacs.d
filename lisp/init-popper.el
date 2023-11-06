b8618a35d69be0622d97a5788c20440c17bc6da7
Taro Sato <okomestudio@gmail.com>
2023-11-06 11:46:45 -0800

    Update popper

--- lisp/init-popper.el
+++ lisp/init-popper.el
@@ -1,23 +1,34 @@
-;;; init-popper.el --- GPT clients  -*- lexical-binding: t -*-
+;;; init-popper.el --- Popper  -*- lexical-binding: t -*-
 ;;; Commentary:
+;;
+;; Emacs minor-mode to summon and dismiss buffers easily.
+;;
 ;;; Code:
 
 
 (use-package popper
-  :custom
-  (popper-reference-buffers
-   '("\\*Messages\\*"
-    "Output\\*$"
-    "\\*Async Shell Command\\*"
-    "elfeed-entry" elfeed-show-mode
-    "magit:" magit-mode
-    helpful-mode
-    help-mode
-    compilation-mode))
-
   :bind (("C-~"   . popper-toggle)
          ("C-`"   . popper-cycle)
          ("C-M-`" . popper-toggle-type))
+
+  :custom
+  (popper-reference-buffers
+   '(
+     "\\*Async Shell Command\\*"
+     "\\*Messages\\*"
+     "CAPTURE-.*\\.org$"
+     "Output\\*$"
+     compilation-mode
+     ;;"elfeed-entry" elfeed-show-mode elfeed-goodies-show-mode
+     help-mode
+     helpful-mode
+     magit-mode "magit:"
+     ))
+  (popper-window-height (lambda (win)
+                          (fit-window-to-buffer
+                           win
+                           (* 2 (floor (frame-height) 5))
+                           (floor (frame-height) 3))))
 
   :init
   (popper-mode +1)
