0b64a55764a1eb3cfae0033b44432c7ce9da9039
Taro Sato <okomestudio@gmail.com>
2023-11-18 18:50:54 -0800

    Use posframe

--- lisp/init-ime.el
+++ lisp/init-ime.el
@@ -4,9 +4,11 @@
 
 
 (use-package mozc
+  :after mozc-posframe
+
   :custom
   (default-input-method "japanese-mozc")
-  (mozc-candidate-style 'overlay) ;; or 'posframe
+  (mozc-candidate-style 'posframe) ;; or 'overlay or 'echo-area
 
   :ensure-system-package
   ("/usr/bin/mozc_emacs_helper" . "sudo apt install -y emacs-mozc-bin"))
