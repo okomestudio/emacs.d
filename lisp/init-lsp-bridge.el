ae38c9da1e436b5387083ff83260078f26b8b648
Taro Sato <okomestudio@gmail.com>
2023-12-29 17:07:20 -0800

    Add lsp-bridge

---
+++ lisp/init-lsp-bridge.el
@@ -0,0 +1,34 @@
+;;; init-lsp-bridge.el --- lsp-bridge  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Fast, multi-threaded LSP client.
+;;
+;;; Code:
+
+
+(use-package posframe
+  :straight t
+  :after dashboard)
+
+
+(use-package markdown-mode
+  :straight t
+  :after dashboard)
+
+
+(use-package lsp-bridge
+  :straight
+  (lsp-bridge
+   :type git
+   :host github
+   :repo "manateelazycat/lsp-bridge"
+   :files
+   (:defaults "*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
+   :build (:not compile))
+
+  :init
+  (global-lsp-bridge-mode))
+
+
+(provide 'init-lsp-bridge)
+;;; init-lsp-bridge.el ends here
