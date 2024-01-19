e3c29b19865cfdc105aa5385b9e735a2ce0978e1
Taro Sato <okomestudio@gmail.com>
2024-01-18 22:11:42 -0800

    Clean up

--- lisp/init-org-roam.el
+++ lisp/init-org-roam.el
@@ -1,5 +1,9 @@
 ;;; init-org-roam.el --- Org Roam  -*- lexical-binding: t -*-
 ;;; Commentary:
+;;
+;; A tool for networked thought. It reproduces some of Roam Research's key
+;; features within Org.
+;;
 ;;; Code:
 
 
@@ -7,7 +11,10 @@
   :after (org)
 
   :straight
-  (:type git :host github :repo "okomestudio/org-roam"
+  (;; pull from fork for custom features
+   :type git
+   :host github
+   :repo "okomestudio/org-roam"
    :branch "okomestudio"
    :fork "okomestudio")
 
@@ -27,7 +34,8 @@
   (org-roam-completion-everywhere nil)
   (org-roam-dailies-capture-templates '(("d" "default" entry "* %?\n<%<%Y-%m-%d %a %H:%M>>"
                                          :target
-                                         (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+                                         (file+head "%<%Y-%m-%d>.org"
+                                                    "#+title: %<%Y-%m-%d>\n"))))
   (org-roam-dailies-directory "journal/")
   (org-roam-database-connector 'sqlite-builtin)
   (org-roam-db-location (file-truename "~/.config/emacs/roam/.roam.db"))
@@ -116,27 +124,27 @@
 
     (defun init-org-roam--org-roam-node-slug (title)
       (let* (;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
-            (slug-trim-chars '(768    ; U+0300 COMBINING GRAVE ACCENT
-                               769    ; U+0301 COMBINING ACUTE ACCENT
-                               770    ; U+0302 COMBINING CIRCUMFLEX ACCENT
-                               771    ; U+0303 COMBINING TILDE
-                               772    ; U+0304 COMBINING MACRON
-                               774    ; U+0306 COMBINING BREVE
-                               775    ; U+0307 COMBINING DOT ABOVE
-                               776    ; U+0308 COMBINING DIAERESIS
-                               777    ; U+0309 COMBINING HOOK ABOVE
-                               778    ; U+030A COMBINING RING ABOVE
-                               779    ; U+030B COMBINING DOUBLE ACUTE ACCENT
-                               780    ; U+030C COMBINING CARON
-                               795    ; U+031B COMBINING HORN
-                               803    ; U+0323 COMBINING DOT BELOW
-                               804    ; U+0324 COMBINING DIAERESIS BELOW
-                               805    ; U+0325 COMBINING RING BELOW
-                               807    ; U+0327 COMBINING CEDILLA
-                               813    ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
-                               814    ; U+032E COMBINING BREVE BELOW
-                               816    ; U+0330 COMBINING TILDE BELOW
-                               817))) ; U+0331 COMBINING MACRON BELOW
+             (slug-trim-chars '(768   ; U+0300 COMBINING GRAVE ACCENT
+                                769   ; U+0301 COMBINING ACUTE ACCENT
+                                770   ; U+0302 COMBINING CIRCUMFLEX ACCENT
+                                771   ; U+0303 COMBINING TILDE
+                                772   ; U+0304 COMBINING MACRON
+                                774   ; U+0306 COMBINING BREVE
+                                775   ; U+0307 COMBINING DOT ABOVE
+                                776   ; U+0308 COMBINING DIAERESIS
+                                777   ; U+0309 COMBINING HOOK ABOVE
+                                778   ; U+030A COMBINING RING ABOVE
+                                779   ; U+030B COMBINING DOUBLE ACUTE ACCENT
+                                780   ; U+030C COMBINING CARON
+                                795   ; U+031B COMBINING HORN
+                                803   ; U+0323 COMBINING DOT BELOW
+                                804   ; U+0324 COMBINING DIAERESIS BELOW
+                                805   ; U+0325 COMBINING RING BELOW
+                                807   ; U+0327 COMBINING CEDILLA
+                                813   ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
+                                814   ; U+032E COMBINING BREVE BELOW
+                                816   ; U+0330 COMBINING TILDE BELOW
+                                817)))  ; U+0331 COMBINING MACRON BELOW
         (cl-flet* ((nonspacing-mark-p (char)
                      (memq char slug-trim-chars))
                    (strip-nonspacing-marks (s)
