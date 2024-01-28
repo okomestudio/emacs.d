07a0bc2381f969d68b3f6fb475b177aabbd25368
Taro Sato <okomestudio@gmail.com>
2024-01-27 19:07:32 -0800

    Move OSM

--- init.d/80-osm.el
+++ init.d/80-osm.el
@@ -1,15 +1,12 @@
-;;; init-osm.el --- OpenStreetMap  -*- lexical-binding: t -*-
+;;; 80-osm.el --- OpenStreetMap  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
 ;; OpenStreetMap viewer for Emacs.
 ;;
 ;;; Code:
 
-
 (use-package osm
   :custom
   (osm-copyright nil))
 
-
-(provide 'init-osm)
-;;; init-osm.el ends here
+;;; 80-osm.el ends here
