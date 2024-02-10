a9f6bd81bc8afe61b290f555a6ddeb96a377cfbb
Taro Sato <okomestudio@gmail.com>
2024-02-09 22:18:04 -0800

    Clean up mixed-pitch mode

--- init.d/05-faces.el
+++ init.d/05-faces.el
@@ -105,70 +105,14 @@ See https://knowledge.sakura.ad.jp/8494/"
 
   :custom
   (mixed-pitch-variable-pitch-cursor nil)
-  (mixed-pitch-fixed-pitch-faces '(diff-added
-                                   diff-context
-                                   diff-file-header
-                                   diff-function
-                                   diff-header
-                                   diff-hunk-header
-                                   diff-removed
-                                   font-latex-math-face
-                                   font-latex-sedate-face
-                                   font-latex-warning-face
-                                   font-latex-sectioning-5-face
-                                   font-lock-builtin-face
-                                   font-lock-comment-delimiter-face
-                                   font-lock-comment-face
-                                   font-lock-constant-face
-                                   font-lock-doc-face
-                                   font-lock-function-name-face
-                                   font-lock-keyword-face
-                                   font-lock-negation-char-face
-                                   font-lock-preprocessor-face
-                                   font-lock-regexp-grouping-backslash
-                                   font-lock-regexp-grouping-construct
-                                   font-lock-string-face
-                                   font-lock-type-face
-                                   font-lock-variable-name-face
-                                   line-number
-                                   line-number-current-line
-                                   line-number-major-tick
-                                   line-number-minor-tick
-                                   markdown-code-face
-                                   markdown-gfm-checkbox-face
-                                   markdown-inline-code-face
-                                   markdown-language-info-face
-                                   markdown-language-keyword-face
-                                   markdown-math-face
-                                   message-header-name
-                                   message-header-to
-                                   message-header-cc
-                                   message-header-newsgroups
-                                   message-header-xheader
-                                   message-header-subject
-                                   message-header-other
-                                   mu4e-header-key-face
-                                   mu4e-header-value-face
-                                   mu4e-link-face
-                                   mu4e-contact-face
-                                   mu4e-compose-separator-face
-                                   mu4e-compose-header-face
-                                   org-block
-                                   org-block-begin-line
-                                   org-block-end-line
-                                   org-document-info-keyword
-                                   org-code
-                                   org-indent
-                                   org-latex-and-related
-                                   org-modern-bracket-line
-                                   org-checkbox
-                                   org-formula
-                                   org-meta-line
-                                   ;; org-table
-                                   org-verbatim))
 
   :hook
-  (org-mode . mixed-pitch-mode))
+  (org-mode . mixed-pitch-mode)
+
+  :config
+  (delete 'org-table mixed-pitch-fixed-pitch-faces)
+  (add-to-list 'mixed-pitch-fixed-pitch-faces 'font-lock-comment-face)
+  (add-to-list 'mixed-pitch-fixed-pitch-faces 'org-modern-bracket-line))
 
 
 (use-package eaw
