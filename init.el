ef56cef7a5c1f275c40d42bf5a2fede6ea7c4dad
Taro Sato <okomestudio@gmail.com>
2024-02-10 14:03:19 -0800

    Clean up custom.el handling

--- init.el
+++ init.el
@@ -20,11 +20,8 @@
                         (emacs-pid) (emacs-init-time))))
 
 ;; custom.el
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(unless (file-exists-p custom-file)
-  (write-region "" nil custom-file))
-(when (file-exists-p custom-file)
-  (load custom-file))
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file 'noerror 'nomessage)
 
 ;; Configure use-package, init-straight.el or init-package.el:
 (load-file (expand-file-name "init.d/init-straight.el" user-emacs-directory))
