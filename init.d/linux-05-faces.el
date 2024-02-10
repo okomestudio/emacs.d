7248f4b7288847da4fa25741a11cfcb8a44cd981
Taro Sato <okomestudio@gmail.com>
2024-02-10 00:00:21 -0800

    Consolidate text scaling

--- init.d/linux-05-faces.el
+++ init.d/linux-05-faces.el
@@ -68,8 +68,7 @@ See: https://knowledge.sakura.ad.jp/8494/"
                                       font-specs)))
     (set-fontset-font nil 'unicode matched-font-spec frame 'append)))
 
-(use-package 05-faces
-  ;; :disabled
+(use-package emacs
   :defer t
   :straight nil
 
@@ -84,7 +83,20 @@ See: https://knowledge.sakura.ad.jp/8494/"
   (after-init . (lambda ()
                   (dolist (element ok-face-face-font-rescale-alist)
                     (add-to-list 'face-font-rescale-alist element))
-                  (ok-face--set-up-action 'ok-face--setup-faces-for-frame))))
+                  (ok-face--set-up-action 'ok-face--setup-faces-for-frame)))
+
+  ;; Scale texts by mode
+  (elfeed-search-mode . (lambda () (text-scale-set 1.0)))
+  (elfeed-show-mode . (lambda () (text-scale-set 1.5)))
+  (eww-mode . (lambda () (text-scale-set 1.5)))
+  (org-mode
+   . (lambda ()
+       (text-scale-set 1.5)
+       (let ((factor (expt text-scale-mode-step text-scale-mode-amount)))
+         (plist-put org-format-latex-options :scale (* 3.33 factor)))))
+  (prog-mode . (lambda () (text-scale-set 0.5)))
+  (text-mode . (lambda () (text-scale-set 0.5)))
+  (treemacs-mode . (lambda () (text-scale-decrease 0.4))))
 
 
 (use-package mixed-pitch
