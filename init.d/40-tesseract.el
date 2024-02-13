1c4fc22926dca589f3b18ae5452b1bb6ec1a947b
Taro Sato <okomestudio@gmail.com>
2024-02-13 14:52:18 -0800

    Clean up OCR using tesseract package

---
+++ init.d/40-tesseract.el
@@ -0,0 +1,36 @@
+;;; 40-tesseract.el --- Tesseract  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Configure Tesseract.
+;;
+;;; Code:
+
+(use-package tesseract
+  ;; :straight (:host github :repo "SebastianMeisel/tesseract.el")
+  :straight (:host github :repo "okomestudio/tesseract.el" :fork "okomestudio")
+  :commands (tesseract-change-language)
+  :bind
+  ("C-x C-y" . ok-tesseract/ocr-clipboard-image)
+  
+  :custom
+  (doc-view-scale-internally nil)
+  (tessearct/default-language "eng")
+  
+  :ensure-system-package
+  ("/usr/bin/tesseract" . "sudo apt install -y tesseract-ocr")
+  ("/usr/share/tesseract-ocr/5/tessdata/eng.traineddata" . "sudo apt install -y tesseract-ocr-eng")
+  ("/usr/share/tesseract-ocr/5/tessdata/jpn.traineddata" . "sudo apt install -y tesseract-ocr-jpn")
+  ("/usr/share/tesseract-ocr/5/tessdata/jpn_vert.traineddata" . "sudo apt install -y tesseract-ocr-jpn-vert")
+
+  :config
+  (defun ok-tesseract/ocr-clipboard-image ()
+    "Run OCR on an image in clipboard and paste into current position."
+    (interactive)
+    (let* ((image-type "image/png")
+           (paste-cmd (format "wl-paste -t %s" image-type))
+           (paste-cmd-x (format "xclip -selection clipboard -t %s -l 1 -o" image-type))
+           (lang tesseract/current-language))
+      (insert (shell-command-to-string
+               (format "%s | tesseract -l %s stdin stdout" paste-cmd lang))))))
+
+;;; 40-tesseract.el ends here
