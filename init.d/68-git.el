bff9009062cbff2315e0f0515583981cca5c6afa
Taro Sato <okomestudio@gmail.com>
2024-02-13 15:05:32 -0800

    Add magit-todos

--- init.d/68-git.el
+++ init.d/68-git.el
@@ -1,12 +1,17 @@
 ;;; 68-git.el --- Git  -*- lexical-binding: t -*-
 ;;; Commentary:
+;;
+;; Configure Git related utilities.
+;;
 ;;; Code:
 
 (use-package magit)  ;; not derived from prog-mode
 
+(use-package magit-todos
+  :after magit
+  :hook (magit-mode . (lambda () (magit-todos-mode 1))))
 
 (use-package devdocs
-  :hook
-  (magit-mode . (lambda () (setq-local devdocs-current-docs '("git")))))
+  :hook (magit-mode . (lambda () (setq-local devdocs-current-docs '("git")))))
 
 ;;; 68-git.el ends here
