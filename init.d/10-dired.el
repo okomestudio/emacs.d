ce14d9f83b271a55902fc87a20deae0ca8f7a1c4
Taro Sato <okomestudio@gmail.com>
2024-02-20 01:14:58 -0800

    Activate dired config

---
+++ init.d/10-dired.el
@@ -0,0 +1,14 @@
+;;; 10-dired.el --- dired  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Configure Dired and related utilities.
+;;
+;;; Code:
+
+(use-package dired
+  :straight nil
+  :custom
+  ;; (dired-omit-files "^\\.$\\|^\\.\\.$\\|\\.pyc$\\|\\.pyo$\\|\#$")
+  (dired-listing-switches "-alh"))
+
+;;; 10-dired.el ends here
