5c1703b4cb64dc9bdf7f59fe57f7a58cb993de56
Taro Sato <okomestudio@gmail.com>
2024-02-23 15:53:50 -0800

    Update Anki templates

--- etc/tempel/templates.el
+++ etc/tempel/templates.el
@@ -1,6 +1,32 @@
 org-mode
 
-(anki "* Anki notes :anki:
+(anki-note-basic "* Anki note :anki:
+:PROPERTIES:
+:VISIBILITY: folded
+:ANKI_DECK: Default
+:ANKI_NOTE_TYPE: Basic
+:END:
+** Front
+** Back" n)
+
+(anki-note-basic-reverse "* Anki note :anki:
+:PROPERTIES:
+:VISIBILITY: folded
+:ANKI_DECK: Default
+:ANKI_NOTE_TYPE: Basic (and reversed card)
+:END:
+** Front
+** Back" n)
+
+(anki-note-cloze "* Anki note :anki:
+:PROPERTIES:
+:VISIBILITY: folded
+:ANKI_DECK: Default
+:ANKI_NOTE_TYPE: Cloze
+:END:
+** Text" n)
+
+(anki-note-list "* Anki notes :anki:
 :PROPERTIES:
 :VISIBILITY: folded
 :ANKI_DECK: Default
@@ -9,7 +35,6 @@ org-mode
 ** Note
 :PROPERTIES:
 :ANKI_NOTE_TYPE: Basic (and reversed card)
-:ANKI_NOTE_ID:
 :END:
 *** Front
 *** Back" n)
--- init.d/80-anki.el
+++ init.d/80-anki.el
@@ -10,10 +10,23 @@
   :straight (:fork "orgtre")
   :commands (anki-editor-note-at-point)
   :ensure-system-package (curl . "sudo apt install -y curl")
+
   :custom
   ;; (request-log-level 'debug) ;; for debugging
   (anki-editor-latex-style 'mathjax)
   (anki-editor-ignored-org-tags '("export" "noexport" "anki"))
-  (anki-editor-org-tags-as-anki-tags t))
+  (anki-editor-org-tags-as-anki-tags t)
+
+  :init
+  (with-eval-after-load 'org
+    (add-to-list 'org-global-properties-fixed '("ANKI_DECK_ALL" . "Default"))
+    (add-to-list 'org-global-properties-fixed
+                 `("ANKI_NOTE_TYPE_ALL"
+                   . ,(string-join '("Basic"
+                                     "\"Basic (and reversed card)\""
+                                     "Cloze") " ")))
+    (add-to-list 'org-default-properties "ANKI_DECK")
+    (add-to-list 'org-default-properties "ANKI_NOTE_ID")
+    (add-to-list 'org-default-properties "ANKI_NOTE_TYPE")))
 
 ;;; 80-anki.el ends here
