9868ff7c633d5612c761b76d7dece2a8002c3451
Taro Sato <okomestudio@gmail.com>
2024-02-24 00:48:44 -0800

    Refactor org-roam helpers

--- init.d/16-org-roam.el
+++ init.d/16-org-roam.el
@@ -49,6 +49,8 @@
   (put 'org-roam-ui-port 'safe-local-variable #'integerp)
 
   :config
+  (require 'ok-org-roam)
+
   (setq find-file-visit-truename t) ;; See 5.3 Setting up Org-roam
 
   (defun ok-org-roam-tag-add (&optional r)
@@ -152,7 +154,7 @@
 
     (cl-defmethod org-roam-node-slug ((node org-roam-node))
       "Return the slug of NODE. Overridden to use hyphens instead of underscores."
-      (ok-string-to-slug (org-roam-node-title node))))
+      (string-to-org-slug (org-roam-node-title node))))
 
   ;; UNLINKED REFERENCES SECTION
   (with-eval-after-load 'org-roam-mode
@@ -288,50 +290,6 @@
                      ;; are easier to see.
                      (setq-local adaptive-wrap-extra-indent 4)
                      (adaptive-wrap-prefix-mode +1))))
-
-;; UTILITY FUNCTIONS
-
-(defun ok-string-to-slug (title)
-  "Turn the TITLE into a slug."
-  (let (;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
-        (slug-trim-chars '(768 ;; U+0300 COMBINING GRAVE ACCENT
-                           769 ;; U+0301 COMBINING ACUTE ACCENT
-                           770 ;; U+0302 COMBINING CIRCUMFLEX ACCENT
-                           771 ;; U+0303 COMBINING TILDE
-                           772 ;; U+0304 COMBINING MACRON
-                           774 ;; U+0306 COMBINING BREVE
-                           775 ;; U+0307 COMBINING DOT ABOVE
-                           776 ;; U+0308 COMBINING DIAERESIS
-                           777 ;; U+0309 COMBINING HOOK ABOVE
-                           778 ;; U+030A COMBINING RING ABOVE
-                           779 ;; U+030B COMBINING DOUBLE ACUTE ACCENT
-                           780 ;; U+030C COMBINING CARON
-                           795 ;; U+031B COMBINING HORN
-                           803 ;; U+0323 COMBINING DOT BELOW
-                           804 ;; U+0324 COMBINING DIAERESIS BELOW
-                           805 ;; U+0325 COMBINING RING BELOW
-                           807 ;; U+0327 COMBINING CEDILLA
-                           813 ;; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
-                           814 ;; U+032E COMBINING BREVE BELOW
-                           816 ;; U+0330 COMBINING TILDE BELOW
-                           817))) ;; U+0331 COMBINING MACRON BELOW
-    (cl-flet* ((nonspacing-mark-p (char)
-                 (memq char slug-trim-chars))
-               (strip-nonspacing-marks (s)
-                 (string-glyph-compose
-                  (apply #'string
-                         (seq-remove #'nonspacing-mark-p
-                                     (string-glyph-decompose s)))))
-               (cl-replace (title pair)
-                 (replace-regexp-in-string (car pair) (cdr pair) title)))
-      (let* ((pairs `(("[^[:alnum:][:digit:]]" . "-") ;; convert anything not alphanumeric
-                      ("--*" . "-") ;; remove sequential underscores
-                      ("^-" . "")   ;; remove starting underscore
-                      ("-$" . ""))) ;; remove ending underscore
-             (slug (-reduce-from #'cl-replace
-                                 (strip-nonspacing-marks title)
-                                 pairs)))
-        (downcase slug)))))
 
 ;; Local Variables:
 ;; nameless-aliases: (("" . "ok-org-roam"))
