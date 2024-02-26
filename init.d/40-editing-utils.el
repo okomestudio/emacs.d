bb667cde060c52570e24f2f85a749dd1f43d0677
Taro Sato <okomestudio@gmail.com>
2024-02-26 02:16:32 -0800

    Match symbols

--- init.d/40-editing-utils.el
+++ init.d/40-editing-utils.el
@@ -22,8 +22,8 @@
 (use-package multiple-cursors
   :bind
   ("C-S-c C-S-c" . mc/edit-lines)
-  ("C->" . mc/mark-next-like-this)
-  ("C-<" . mc/mark-previous-like-this)
+  ("C->" . mc/mark-next-symbol-like-this)
+  ("C-<" . mc/mark-previous-symbol-like-this)
   ("C-S-c C-<" . mc/mark-all-like-this))
 
 
