2edf28e911341bb9e0753ad140247b5981b50a78
Taro Sato <okomestudio@gmail.com>
2024-03-01 15:37:54 -0800

    Clean up shell

--- init.d/65-shell.el
+++ init.d/65-shell.el
@@ -1,49 +1,36 @@
-;;; 65-shell.el --- Shell  -*- lexical-binding: t -*-
+;;; 65-shell.el --- shell  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Configure shell related utilities.
+;; Configure the shell mode and related utilities.
 ;;
 ;;; Code:
 
 (use-package sh-script
   :straight nil
-  :interpreter "sh"
-  :mode ".*\\.sh\\'"
+  :interpreter (("bash" . bash-ts-mode)
+                ("sh" . sh-mode))
+  :mode ((("\\.?bash_.*\\'" "\\.?bashrc\\'") . bash-ts-mode)
+         (".*\\.sh\\'" . sh-mode))
+  :hook ((bash-ts-mode . lsp-deferred)
+         (sh-mode . flymake-mode))
   :custom
   (sh-basic-offset 2)
-  (sh-indentation 2)
-
-  :hook
-  (sh-mode . flymake-mode))
-
-
-(use-package bash-ts-mode
-  :straight nil
-  :interpreter "bash"
-  :mode ("\\.?bash_.*\\'" "\\.?bashrc\\'")
-  :hook (bash-ts-mode . flymake-mode))
-
-
-(use-package bats-mode
-  :interpreter "bats")
-
-
-(use-package lsp-mode
   ;; Run the Explainshell service on start:
   ;;
   ;;   docker container run --name explainshell --restart always \
   ;;          -p 5023:5000 -d spaceinvaderone/explainshell
   ;;
-  :custom
   (lsp-bash-explainshell-endpoint "http://localhost:5023")
-  (lsp-bash-highlight-parsing-errors t)
+  (lsp-bash-highlight-parsing-errors t))
 
-  :hook
-  (bash-ts-mode . (lambda ()
-                    (lsp-ensure-server 'bash-ls)
-                    (lsp-deferred)))
 
-  :ensure-system-package
-  (shellcheck . "sudo apt install -y shellcheck"))
+(use-package sh-script
+  :if (eq system-type 'gnu/linux)
+  :straight nil
+  :ensure-system-package (shellcheck . "sudo apt install -y shellcheck"))
+
+
+(use-package bats-mode
+  :interpreter "bats")
 
 ;;; 65-shell.el ends here
