525f569ba7851f105849570835154870143ae292
Taro Sato <okomestudio@gmail.com>
2024-03-01 13:55:20 -0800

    Clean up RST mode config

--- init.d/55-rst.el
+++ init.d/55-rst.el
@@ -1,27 +1,22 @@
-;;; 55-rst.el --- rst-mode  -*- lexical-binding: t -*-
+;;; 55-rst.el --- rst  -*- lexical-binding: t -*-
 ;;; Commentary:
+;;
+;; Configure the RST mode and related utilities.
+;;
 ;;; Code:
 
 (use-package rst-mode
   :straight nil
-
-  :ensure-system-package
-  (sphinx-quickstart . "pip install sphinx")
-
-  :mode
-  "\\.rst\\'")
-
-
-(use-package lsp-mode
-  :hook
-  (rst-mode . lsp-deferred)
-
-  :config
-  (add-to-list 'lsp-language-id-configuration '(rst-mode . "rst"))
-  (lsp-register-client
-   (make-lsp-client :new-connection
-		                (lsp-stdio-connection '("~/.config/emacs/bin/esbonio"))
-                    :activation-fn (lsp-activate-on "rst")
-                    :server-id 'esbonio)))
+  :hook (rst-mode . lsp-deferred)
+  :init
+  (with-eval-after-load 'lsp-mode
+    (add-to-list 'lsp-language-id-configuration '(rst-mode . "rst"))
+    (lsp-register-client
+     (make-lsp-client
+      :new-connection
+		  (lsp-stdio-connection `(,(expand-file-name "bin/esbonio"
+                                                 user-emacs-directory)))
+      :activation-fn (lsp-activate-on "rst")
+      :server-id 'esbonio))))
 
 ;;; 55-rst.el ends here
