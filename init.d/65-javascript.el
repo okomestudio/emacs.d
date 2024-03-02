1360ea5ab69e885d89abe3fae8d108807252180d
Taro Sato <okomestudio@gmail.com>
2024-03-02 04:36:36 -0800

    Clean up js-mode

--- init.d/65-javascript.el
+++ init.d/65-javascript.el
@@ -1,25 +1,36 @@
 ;;; 65-javascript.el --- javascript  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Configure JavaScript related utilities.
+;; Configure js-mode and related utilities.
 ;;
 ;;; Code:
 
-(use-package js-ts-mode
+(use-package js
   :straight nil
-  :mode "\\.js\\'"
-  :hook (js-ts-mode . lsp))
+  :bind (nil
+         :map js-ts-mode-map
+         ("C-c b" . (lambda () (interactive) (prettier-js))))
+  :mode (("\\.js\\'" . js-ts-mode)
+         ("\\.jsx\\'" . js-ts-mode)     ; js-jsx-mode is equivalent
+         ("\\.tsx?\\'" . typescript-ts-mode))
+  :hook ((js-ts-mode
+          typescript-ts-mode) . lsp-deferred)
+  :custom
+  (js-jsx-indent-level 2)
+
+  :config
+  (setq js-indent-level 2)              ; Setting this in :custom
+                                        ; doens't get respected.
+  )
 
 
-(use-package js-jsx-mode
-  :straight nil
-  :mode "\\.jsx\\'"
-  :hook (js-jsx-mode . lsp))
-
-
-(use-package typescript-ts-mode
-  :straight nil
-  :mode ("\\.tsx?\\'")
-  :hook (typescript-ts-mode . lsp))
+(use-package prettier-js
+  :commands (prettier-js)
+  :ensure-system-package (prettier . "npm install -g prettier")
+  :config
+  (setq prettier-js-args '("--arrow-parens" "always"
+                           "--print-width" "88"
+                           "--single-quote"
+                           "--trailing-comma" "all")))
 
 ;;; 65-javascript.el ends here
