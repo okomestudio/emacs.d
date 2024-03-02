bd217531d4d1d07d5653cc4c2e65a382bde2a554
Taro Sato <okomestudio@gmail.com>
2024-03-02 12:03:47 -0800

    Add projectile Python template

--- etc/tempel/templates.el
+++ etc/tempel/templates.el
@@ -4,6 +4,18 @@ emacs-lisp-mode
 ;; nameless-aliases: ((\"\" . \"prefix\"))
 ;; End:")
 
+lisp-data-mode
+
+(dir-locals-python
+ "(
+ (nil
+  . ((projectile-project-compilation-cmd . \"pip install -e .[dev,test] && pre-commit install\")
+     (projectile-project-configure-cmd . \"pyenv virtualenv 3.12.2 $(basename $PWD) && pyenv local $(basename $PWD)\")
+     (projectile-project-run-cmd . \"python -m mypackage \")
+     (projectile-project-test-cmd . \"pytest\")))
+ )
+")
+
 org-mode
 
 (latex-equation "\\begin{equation}" r> n> "\\end{equation}")
