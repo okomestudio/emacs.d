e866756f50ce4cef269481d3f31483fbf93e3606
Taro Sato <okomestudio@gmail.com>
2024-03-03 16:05:35 -0800

    Activate OpenStreetMap

---
+++ init.d/80-osm.el
@@ -0,0 +1,11 @@
+;;; 80-osm.el --- osm  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Configure the OpenStreetMap viewer.
+;;
+;;; Code:
+
+(use-package osm
+  :custom (osm-copyright nil))
+
+;;; 80-osm.el ends here
