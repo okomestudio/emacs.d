803ac7172ca8475221a7afd9fae52dc1d59e2228
Taro Sato <okomestudio@gmail.com>
2024-03-10 19:02:58 -0700

    Update openwith

--- init.d/70-openwith.el
+++ init.d/70-openwith.el
@@ -1,19 +1,17 @@
-;;; 70-openwith.el --- Openwith  -*- lexical-binding: t -*-
+;;; 70-openwith.el --- openwith  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Initialize openwith.
+;; Configure openwith and the related utilities.
 ;;
 ;;; Code:
 
 (use-package openwith
   ;; Associate external applications with files.
   :custom
-  (openwith-associations '(("\\.pdf\\'" "okular" (file))))
-
-  :config
-  (openwith-mode -1)
-
+  (openwith-associations '(("\\.pdf\\'" "okular" (file))
+                           ("\\.svg\\'" "gwenview" (file))))
+  :init
   (with-eval-after-load 'mm-util
     (add-to-list 'mm-inhibit-file-name-handlers 'openwith-file-handler)))
 
-;;; init-openwith.el ends here
+;;; 70-openwith.el ends here
