fc1b08bda60a28a74a370d212b52471af94f808f
Taro Sato <okomestudio@gmail.com>
2024-03-10 19:03:09 -0700

    Add image preview with external app

--- init.d/65-plantuml.el
+++ init.d/65-plantuml.el
@@ -1,24 +1,46 @@
-;;; 65-plantuml.el --- Plantuml  -*- lexical-binding: t -*-
+;;; 65-plantuml.el --- plantuml  -*- lexical-binding: t -*-
 ;;; Commentary:
+;;
+;; Configure plantuml-mode and the related utilities.
+;;
 ;;; Code:
 
-(defconst init-plantuml-path-plantuml
-  (expand-file-name "/usr/share/plantuml/plantuml.jar")
-  "Path to PlantUML JAR.")
-
-
 (use-package plantuml-mode
+  :mode ("\\.p\\(\\|lant\\)uml\\'" . plantuml-mode)
+  :ensure-system-package (plantuml . "sudo apt install -y plantuml")
   :custom
-  (org-plantuml-jar-path init-plantuml-path-plantuml)
   (plantuml-default-exec-mode 'jar)
   (plantuml-indent-level 4)
-  (plantuml-jar-path init-plantuml-path-plantuml)
+  (plantuml-jar-path "/usr/share/plantuml/plantuml.jar")
 
-  :ensure-system-package
-  (plantuml . "sudo apt install -y plantuml")
+  :init
+  (with-eval-after-load 'ob-plantuml
+    (setopt org-plantuml-jar-path plantuml-jar-path)))
 
-  :mode
-  ("\\.plantuml\\'" . plantuml-mode)
-  ("\\.puml\\'" . plantuml-mode))
+
+(use-package image-mode
+  :straight nil
+  :bind (nil
+         :map image-mode-map
+         ("C-c o" . image-preview-with-external-app))
+  :config
+  (defun image-preview-with-external-app ()
+    (interactive)
+    (let* ((image-type (image-type-from-buffer))
+           (preview-file (format "plantuml-mode-preview.%s" image-type))
+           (preview-command (pcase image-type
+                              ("svg" "display")
+                              (_ "display")))
+           (image (image--get-image)))
+      (with-temp-buffer
+        (let ((file (plist-get (cdr image) :file)))
+          (if file
+              (if (not (file-exists-p file))
+                  (error "File %s no longer exists" file)
+                (insert-file-contents-literally file))
+            (insert (plist-get (cdr image) :data))))
+        (let ((coding-system-for-write 'utf-8))
+          (write-region (point-min) (point-max) preview-file)))
+      (shell-command (format "%s %s &" preview-command preview-file)))))
 
 ;;; 65-plantuml.el ends here
