cc3651f6244449fd36e8bb46cd81ea9ea4930c21
Taro Sato <okomestudio@gmail.com>
2024-06-10 22:09:49 -0700

    Use setopt

--- init.d/init-straight.el
+++ init.d/init-straight.el
@@ -34,13 +34,13 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 
-(setq straight-use-package-by-default t)
-(setq use-package-always-defer t ;; use :demand t to override
-      use-package-compute-statistics ok-debug ;; for use-package-report
-      use-package-enable-imenu-support t
-      use-package-minimum-reported-time 0.001
-      use-package-verbose ok-debug)
-(setq message-log-max t)
+(setopt straight-use-package-by-default t)
+(setopt use-package-always-defer t ;; use :demand t to override
+        use-package-compute-statistics ok-debug ;; for use-package-report
+        use-package-enable-imenu-support t
+        use-package-minimum-reported-time 0.001
+        use-package-verbose ok-debug)
+(setopt message-log-max t)
 
 (straight-use-package 'use-package)
 (straight-use-package 'use-package-ensure-system-package)
