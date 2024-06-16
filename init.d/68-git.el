c24211daa0f45dcaf2ccd73a65d3c7f1565241ec
Taro Sato <okomestudio@gmail.com>
2024-06-16 12:50:26 -0700

    Add magit-file-icons

--- init.d/68-git.el
+++ init.d/68-git.el
@@ -8,6 +8,10 @@
 (use-package magit ;; not derived from prog-mode
   )
 
+(use-package magit-file-icons
+  :after magit
+  :hook (magit-mode . (lambda () (magit-file-icons-mode 1))))
+
 (use-package magit-todos
   :after magit
   :hook (magit-mode . (lambda () (magit-todos-mode 1))))
