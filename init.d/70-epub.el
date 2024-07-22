b0ed98088956db786cc75ad997851ba325851f56
Taro Sato <okomestudio@gmail.com>
2024-07-22 14:58:40 -0700

    Add epub

---
+++ init.d/70-epub.el
@@ -0,0 +1,15 @@
+;;; 70-epub.el --- ePub  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Initialize ePub utilities.
+;;
+;;; Code:
+
+(use-package nov
+  ;; To activate this mode, open the ePub doc and
+  ;;
+  ;;   M-x nov-mode
+  ;;
+  )
+
+;;; 70-epub.el ends here
