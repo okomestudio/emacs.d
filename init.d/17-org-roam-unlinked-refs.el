7c8238a00aa20bcd3b127e2f6be1939f7469824e
Taro Sato <okomestudio@gmail.com>
2024-08-09 16:57:31 -0700

    Clean up and refactor

--- init.d/17-org-roam-unlinked-refs.el
+++ init.d/17-org-roam-unlinked-refs.el
@@ -62,13 +62,16 @@ used to control rendering."
   (defun ok-org-roam-unlinked-references-title-regex (titles)
     "Construct a ripgrep regex pattern from TITLES.
 The output expression should be sanitized for the shell use."
-    (format "\"\\[([^[]]++|(?R))+\\]%s\""
+    (format "'\\[([^[]]++|(?R))+\\]%s'"
             (mapconcat 'ok-org-roam-unlinked-references-apply-word-boundary-re titles "")))
 
   (defun ok-org-roam-unlinked-references-apply-word-boundary-re (title)
     "Wrap TITLE with word boundary regex.
 The output expression should be sanitized for the shell use."
-    (format ok-org-roam-unlinked-references-word-boundary-re (shell-quote-argument title)))
+    (format ok-org-roam-unlinked-references-word-boundary-re
+            (mapconcat #'shell-quote-argument
+                       (split-string title "'")
+                       "'\"'\"'")))
 
   (defun ok-org-roam-unlinked-references-file-glob-args ()
     "Construct file glob arguments for ripgrep."
@@ -105,25 +108,31 @@ References from FILE are excluded."
                         col (string-to-number (match-string 3 line))
                         matched-text (match-string 4 line))
                   (when (and matched-text
-                             (ok-org-roam-unlinked-references-result-filter-p matched-text f row col titles node))
-                    (magit-insert-section section (org-roam-grep-section)
-                                          (oset section file f)
-                                          (oset section row row)
-                                          (oset section col col)
-                                          (insert (propertize (format "%s:%s:%s"
-                                                                      (truncate-string-to-width (file-name-base f) 15 nil nil t)
-                                                                      row col) 'font-lock-face 'org-roam-dim)
-                                                  " "
-                                                  (org-roam-fontify-like-in-org-mode
-                                                   (ok-org-roam-unlinked-references-preview-line f row col f-prev row-prev col-prev))
-                                                  "\n")
-                                          (setq f-prev f
-                                                row-prev row
-                                                col-prev col
-                                                match_count (+ match_count 1))
-                                          (if (= match_count ok-org-roam-unlinked-references-max-results-count)
-                                              (insert (format "WARNING: Results truncated to %d items (%d potential matches)"
-                                                              match_count (length results))))))))
+                             (ok-org-roam-unlinked-references-result-filter-p
+                              matched-text f row col titles node))
+                    (magit-insert-section
+                        section (org-roam-grep-section)
+                        (oset section file f)
+                        (oset section row row)
+                        (oset section col col)
+                        (insert (propertize
+                                 (format "%s:%s:%s"
+                                         (truncate-string-to-width (file-name-base f)
+                                                                   15 nil nil t)
+                                         row col)
+                                 'font-lock-face 'org-roam-dim)
+                                " "
+                                (org-roam-fontify-like-in-org-mode
+                                 (ok-org-roam-unlinked-references-preview-line
+                                  f row col f-prev row-prev col-prev))
+                                "\n")
+                        (setq f-prev f
+                              row-prev row
+                              col-prev col
+                              match_count (+ match_count 1))
+                        (if (= match_count ok-org-roam-unlinked-references-max-results-count)
+                            (insert (format "WARNING: Results truncated to %d items (%d potential matches)"
+                                            match_count (length results))))))))
               (if (= match_count ok-org-roam-unlinked-references-max-results-count)
                   ;; Throw outside of magit-insert-section to render correct item count.
                   (throw 'limit-result match_count))))
@@ -138,95 +147,135 @@ References from FILE are excluded."
 ;; Customize the unlinked references section for use with Japanese.
 ;;
 (with-eval-after-load 'org-roam-mode
-  ;; Stricter:
-  ;; 助詞 (https://ja.wikipedia.org/wiki/%E5%8A%A9%E8%A9%9E)
-  (setq joshi_r '(;; タイトルを名詞と前提
-                  "か" "が" "かしら" "がてら" "から" "きり" "くらい" "ぐらい" "こそ"
-                  "さ" "さえ" "しか" "ずつ"
-                  "だけ" "だの" "で" "では" "でも" "と" "とは" "とも"
-                  "ながら" "なぞ" "など" "なり" "なんぞ" "に" "ね" "の" "のみ"
-                  "は" "ばかり" "へ" "ほど"
-                  "まで" "も"
-                  "や" "やら" "よ" "より"
-                  "を"))
-  (setq joshi_l (append joshi_r
-                        '("かい" "かり" "けど" "けれど" "けれども"
-                          "し" "ぜ" "ぞ"
-                          "たり" "つつ" "ってば" "て" "ても" "ところで" "とも"
-                          "な" "ので" "のに"
-                          "ば"
-                          "まま" "ものか" "ものの" "もん"
-                          "わ")))
-  (setq ok-org-roam-unlinked-references-word-boundary-re
-        (concat "|(\\b%1$s\\b"
-                "|(?<=" (s-join "|" joshi_l) ")%1$s(?=" (s-join "|" joshi_r) "))"))
-  ;; Lenient version:
-  ;;   "|(\\b%1$s\\b|(?<=[^\x20-\x7e\xff61-\xff9f])%1$s(?=[^\x20-\x7e\xff61-\xff9f]))"
+  ;; WORD BOUNDARY
+  (let* (;; Strict version:
+         ;;
+         ;; Unlike English, Japanese words are not delimited by character
+         ;; word boundary. In this stricter version, we rely on "joshi"
+         ;; particles (助詞; ja.wikipedia.org/wiki/%E5%8A%A9%E8%A9%9E) as
+         ;; boundary indicator. This implicitly assumes titles (words to be
+         ;; delimited by the word boundary) to be "meishi" (名詞).
 
-  (defun ok-org-roam-roam--title-regex (orig-fun titles)
-    (let ((bounded-re (substring (mapconcat #'ok-org-roam-unlinked-references-apply-word-boundary-re titles "") 1))
-          ;; See http://www.drregex.com/2019/02/variable-length-lookbehinds-actually.html
-          (positive-lookbehind "(?=(?'a'[\\s\\S]*))(?'b'(%s)(?=\\k'a'\\z)|(?<=(?=x^|(?&b))[\\s\\S]))")
-          (negative-lookbehind "(?!(?=(?<a>[\\s\\S]*))(?<b>(%s)(?=\\k<a>\\z)|(?<=(?=x^|(?&b))[\\s\\S])))"))
-      (format "\"\\[\\[id:[0-9a-f-]+\\]\\[[^][]*(%s)[^][]*\\]\\]|%s(%s)\""
-              bounded-re
-              (format negative-lookbehind
-                      (string-join '("begin_src +"
-                                     "filetags:( [-_0-9A-Za-z]+)* "
-                                     "header-args:"
-                                     "PYTHONDONTWRITEBYTECODE=1 ")
-                                   "|"))
-              bounded-re)))
+         ;; The collection of joshi that comes on the right side of word:
+         (joshi_r '("か" "が" "かしら" "がてら" "から" "きり" "くらい" "ぐらい" "こそ"
+                    "さ" "さえ" "しか" "ずつ"
+                    "だけ" "だの" "で" "では" "でも" "と" "とは" "とも"
+                    "ながら" "なぞ" "など" "なり" "なんぞ" "に" "ね" "の" "のみ"
+                    "は" "ばかり" "へ" "ほど"
+                    "まで" "も"
+                    "や" "やら" "よ" "より"
+                    "を"))
+         ;; The collection of joshi that comes on the left side of word:
+         (joshi_l (append joshi_r
+                          '("かい" "かり" "けど" "けれど" "けれども"
+                            "し" "ぜ" "ぞ"
+                            "たり" "つつ" "ってば" "て" "ても" "ところで" "とも"
+                            "な" "ので" "のに"
+                            "ば"
+                            "まま" "ものか" "ものの" "もん"
+                            "わ")))
 
-  (advice-add 'ok-org-roam-unlinked-references-title-regex
-              :around #'ok-org-roam-roam--title-regex)
+         (word-boundary-re-strict
+          (concat "|(\\b%1$s\\b"
+                  "|(?<="
+                  (s-join "|" joshi_l) ")%1$s(?=" (s-join "|" joshi_r) "))"))
 
-  (defun ok-org-roam-roam--apply-word-boundary-re (orig-fun title)
-    (let ((s title))
-      ;; Expand and match quote variants:
-      (setq s (replace-regexp-in-string " [\'\‘]\\(\\w\\)" " [\'\‘]\\1" s))
-      (setq s (replace-regexp-in-string "\\(\\w\\)[\'\’]" "\\1[\'\’]" s))
-      (setq s (replace-regexp-in-string " [\"\“]\\(\\w\\)" " [\"\“]\\1" s))
-      (setq s (replace-regexp-in-string "\\(\\w\\)[\"\”]" "\\1[\"\”]" s))
-      (let ((s (funcall orig-fun s)))
-        ;; Since orig-fun shell-quotes special chars, some needs unescape:
-        (setq s (replace-regexp-in-string "[\\][[]\\([^][]+\\)[\\][]]" "[\\1]" s))
-        s)))
+         ;; Lenient version:
+         ;;
+         ;; Use any Japanese characters as word boundary:
+         (word-boundary-re-lenient
+          (concat "|(\\b%1$s\\b"
+                  "|(\\b%1$s\\b"
+                  "|(?<=[^\x20-\x7e\xff61-\xff9f])%1$s(?=[^\x20-\x7e\xff61-\xff9f]))")))
+    (setopt ok-org-roam-unlinked-references-word-boundary-re word-boundary-re-strict))
 
-  (advice-add 'ok-org-roam-unlinked-references-apply-word-boundary-re
-              :around #'ok-org-roam-roam--apply-word-boundary-re)
+  ;; TITLE EXTRACTION REGEX
+  (advice-add
+   'ok-org-roam-unlinked-references-title-regex
+   :override
+   (lambda (titles)
+     (let ((bounded-re (substring (mapconcat
+                                   #'ok-org-roam-unlinked-references-apply-word-boundary-re
+                                   titles "")
+                                  1))
 
-  (defun ok-org-roam-roam--result-filter-p (orig-fun matched-text matched-file row col titles node)
-    (let* ((linked-re (format "\\[\\[id:%s\\]\\[.*\\]\\]" (org-roam-node-id node)))
-           result)
-      (if (not (file-equal-p (org-roam-node-file node) matched-file))
-          (setq result (not (string-match linked-re matched-text))) ;; Test if unlinked ref
-        ;; Matched text within the same file, possibly of a different node
-        (let* ((other-node (save-match-data
-                             (with-current-buffer (find-file-noselect matched-file)
-                               (save-excursion
-                                 (goto-char (point-min))
-                                 (forward-line (1- row))
-                                 (move-to-column col)
-                                 (org-roam-node-at-point))))))
-          (if (not (string-equal (org-roam-node-id node)
-                                 (org-roam-node-id other-node)))
-              ;; Matched text within a different node within the same file
-              (setq result (not (string-match linked-re matched-text))) ;; Text if unlinked ref
-            )))
-      result))
+           ;; For variable-lengths lookbehinds in PCRE, see:
+           ;;
+           ;;   - www.drregex.com/2019/02/variable-length-lookbehinds-actually.html
+           ;;
+           ;; `plb' is for positive lookbehind; `nlb' is for negative lookbehind.
+           (plb "(?=(?'a'[\\s\\S]*))(?'b'(%s)(?=\\k'a'\\z)|(?<=(?=x^|(?&b))[\\s\\S]))")
+           (nlb "(?!(?=(?<a>[\\s\\S]*))(?<b>(%s)(?=\\k<a>\\z)|(?<=(?=x^|(?&b))[\\s\\S])))")
 
-  (advice-add 'ok-org-roam-unlinked-references-result-filter-p
-              :around #'ok-org-roam-roam--result-filter-p)
+           ;; The list of substrings for negative matching:
+           (lines-to-ignore '("begin_src +"
+                              "filetags:( [-_0-9A-Za-z]+)* "
+                              "header-args:"
+                              "PYTHONDONTWRITEBYTECODE=1 ")))
+       (format "\"\\[\\[id:[0-9a-f-]+\\]\\[[^][]*(%s)[^][]*\\]\\]|%s(%s)\""
+               bounded-re
+               (format nlb (string-join lines-to-ignore "|"))
+               bounded-re))))
 
-  (defun ok-org-roam-roam--unlinked-references-preview-line (orig-fun file row col file-prev row-prev col-prev)
-    "Use ellipsis for duplicate line."
-    (if (and (string= file file-prev) (= row row-prev))
-        "⎯〃⎯"
-      (funcall orig-fun file row col file-prev row-prev col-prev)))
+  ;; TITLE TRANSFORMATION AND SANITIZATION
+  (advice-add
+   'ok-org-roam-unlinked-references-apply-word-boundary-re
+   :override
+   (lambda (title)
+     (let* ((s title)
 
-  (advice-add 'ok-org-roam-unlinked-references-preview-line
-              :around #'ok-org-roam-roam--unlinked-references-preview-line))
+            ;; Expand quote variants:
+            (s (replace-regexp-in-string " [\'\‘]\\(\\w\\)" " [\'\‘]\\1" s))
+            (s (replace-regexp-in-string "\\(\\w\\)[\'\’]" "\\1[\'\’]" s))
+            (s (replace-regexp-in-string " [\"\“]\\(\\w\\)" " [\"\“]\\1" s))
+            (s (replace-regexp-in-string "\\(\\w\\)[\"\”]" "\\1[\"\”]" s))
+
+            ;; Apply the original sanitization:
+            (s (format ok-org-roam-unlinked-references-word-boundary-re
+                       (shell-quote-argument s)))
+            ;; (s (format ok-org-roam-unlinked-references-word-boundary-re
+            ;;            (mapconcat #'shell-quote-argument
+            ;;                       (split-string s "'")
+            ;;                       "'\"'\"'")))
+
+            ;; Some special chars needs unescaping after `shell-quotes':
+            (s (replace-regexp-in-string "[\\][[]\\([^][]+\\)[\\][]]" "[\\1]" s)))
+       s)))
+
+  ;; PREDICATE FOR UNLINKED REFERENCE CHECK
+  (advice-add
+   'ok-org-roam-unlinked-references-result-filter-p
+   :override
+   (lambda (matched-text matched-file row col titles node)
+     (let ((linked-re (format "\\[\\[id:%s\\]\\[.*\\]\\]" (org-roam-node-id node))))
+       (if (not (file-equal-p (org-roam-node-file node) matched-file))
+           ;; The matched text is in a different file from the current node.
+           (not (string-match linked-re matched-text)) ;; Is this reference unlinked?
+         ;; The matched text is in the same file of the current node.
+         (let* ((other-node (save-match-data
+                              (with-current-buffer (find-file-noselect matched-file)
+                                (save-excursion
+                                  (goto-char (point-min))
+                                  (forward-line (1- row))
+                                  (move-to-column col)
+                                  (org-roam-node-at-point))))))
+           (if (not (string-equal (org-roam-node-id node)
+                                  (org-roam-node-id other-node)))
+               ;; The matched text is in a different node from the
+               ;; current node within the same file.
+               (not (string-match linked-re matched-text)) ;; Is this reference unlinked?
+             ))))))
+
+  ;; PREVIEW LINE RENDERING
+  (advice-add
+   'ok-org-roam-unlinked-references-preview-line
+   :around
+   (lambda (orig-fun file row col file-prev row-prev col-prev)
+     (if (and (string= file file-prev) (= row row-prev))
+         ;; Use a "ditto" mark if the current line is
+         ;; similar to the previous line.
+         "⎯〃⎯"
+       (funcall orig-fun file row col file-prev row-prev col-prev)))))
 
 ;; Local Variables:
 ;; nameless-aliases: (("" . "ok-org-roam"))
