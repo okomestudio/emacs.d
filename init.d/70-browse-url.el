e9d53e8efd63e0b3b63027eec109aba66aaf3760
Taro Sato <okomestudio@gmail.com>
2024-08-21 02:54:24 -0700

    Enable switching between eww and external browser

--- init.d/70-browse-url.el
+++ init.d/70-browse-url.el
@@ -6,10 +6,18 @@
 ;;; Code:
 
 (use-package browse-url
+  :straight nil
   :after (eww)
   :custom
-  (browse-url-browser-function 'eww-browse-url)
+  (browse-url-browser-function 'ok-browse-url-browser-function)
   (browse-url-generic-program "xdg-open")
-  (browse-url-handlers '(("localhost" . browse-url-generic))))
+  (browse-url-handlers '(("localhost" . browse-url-generic)))
+
+  :config
+  (defun ok-browse-url-browser-function (url &optional arg)
+    (interactive "sURL: \nP")
+    (pcase arg
+      ('(4) (browse-url-default-browser url))
+      (_ (eww-browse-url url)))))
 
 ;;; 70-brose-url.el ends here
