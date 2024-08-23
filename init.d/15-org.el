ea9e54c2683d78a2f65cfa62bcd7c718972348fb
Taro Sato <okomestudio@gmail.com>
2024-08-23 08:05:47 -0700

    Migrate Subtack exporter to plugin

--- init.d/15-org.el
+++ init.d/15-org.el
@@ -265,36 +265,7 @@ node."
   (require 'ox-gfm)
   (require 'ox-hugo)
   (require 'ox-md)
-
-  ;; SUBSTACK EXPORTER
-
-  (defun ok-org--org-html-link (link desc info)
-    (let* ((raw-link (org-element-property :raw-link link))
-           (raw-path (org-element-property :path link))
-           (type (org-element-property :type link))
-           (link-is-url (member type '("http" "https" "ftp" "mailto")))
-           (desc (org-string-nw-p desc)))
-      (if link-is-url
-          (format "<a href=\"%s\">%s</a>" raw-link (or desc raw-link))
-        (if (string= (substring raw-link 0 3) "id:")
-            desc
-          (if (member (file-name-extension raw-link)
-                      '("gif" "jpeg" "jpg" "png" "webp"))
-              (format "<img src=\"%s\" />" raw-link)
-            (format "<a href=\"%s\">%s</a>" raw-link desc))))))
-
-  (org-export-define-derived-backend
-      'substack 'html
-    :menu-entry
-    '(?S "Export to Substack article"
-         ((?o "As HTML file and open"
-	            (lambda (a s v b)
-	              (if a
-                    (org-export-to-buffer t s v b)
-                  (let ((f (concat (file-name-sans-extension buffer-file-name)
-                                   ".html")))
-                    (org-open-file (org-export-to-file 'substack f nil s v b))))))))
-    :translate-alist '((link . ok-org--org-html-link))))
+  (require 'orp-ok-ox-substack))
 
 (use-package ox-gfm ;; GitHub-flavored markdown
   :after ox)
