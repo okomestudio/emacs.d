86c0cb3def973e94227495b415394acdef27c46f
Taro Sato <okomestudio@gmail.com>
2024-08-25 22:20:39 -0700

    Add ignored tags

--- init.d/80-anki.el
+++ init.d/80-anki.el
@@ -1,21 +1,36 @@
 ;;; 80-anki.el --- anki  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Configure Anki and related utilities.
+;; Anki (apps.ankiweb.net) is a tool for spaced repetition.
+;;
+;; The `anki-editor' minor mode enables editing of Anki notes in Org
+;; mode. Sync is unidirectional, i.e., the notes can only be pushed
+;; from Org, but not the other way.
+;;
+;; To use the `anki-editor' mode, the AnkiConnect add-on needs to be
+;; installed on the Anki app client (Tools -> Add-ons).
 ;;
 ;;; Code:
 
 (use-package anki-editor
   ;; Emacs minor mode for making Anki cards with Org.
-  :straight (:fork "anki-editor")
+  :straight (:repo "anki-editor/anki-editor")
   :commands (anki-editor-note-at-point)
   :ensure-system-package (curl . "sudo apt install -y curl")
 
   :custom
-  ;; (request-log-level 'debug) ;; for debugging
   (anki-editor-latex-style 'mathjax)
-  (anki-editor-ignored-org-tags '("export" "noexport" "anki"))
+
+  ;; Use Org tags for Anki, but specify those to be ignored:
   (anki-editor-org-tags-as-anki-tags t)
+  (anki-editor-ignored-org-tags '("anki"
+                                  "export"
+                                  "m"
+                                  "main"
+                                  "noexport"
+                                  "z"))
+
+  ;; (request-log-level 'debug) ;; for debugging
 
   :init
   (with-eval-after-load 'org
