1babf0da1c74be0c4e10649e19271b67e605a802
Taro Sato <okomestudio@gmail.com>
2024-09-05 16:17:16 -0700

    Clean up

--- lisp/projectile.el
+++ lisp/projectile.el
@@ -1,32 +1,30 @@
 ;;; projectile.el --- Projectile  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Configure Projectile.
+;; Projectile configuration.
 ;;
 ;;; Code:
 
 (use-package projectile
-  :bind-keymap
-  ("s-p" . projectile-command-map) ;; "s-" is "super"
-  ("C-c p" . projectile-command-map)
-
-  :custom
-  (projectile-auto-discover nil)
-  (projectile-enable-caching nil)
-  (projectile-git-fd-args "-H -0 -E .git -tf")
-  (projectile-ignored-projects '("~/.pyenv/"))
-  (projectile-indexing-method 'alien)
-  (projectile-mode-line-function '(lambda ()
-                                    (format " [%s]" (projectile-project-name))))
-  (projectile-project-root-functions '(projectile-root-local
-                                       projectile-root-bottom-up
-                                       projectile-root-top-down
-                                       projectile-root-top-down-recurring))
-  (projectile-project-search-path `(,(expand-file-name "straight/repos/"
-                                                       user-emacs-directory)
-                                    (,(expand-file-name "github.com/"
-                                                        (getenv "HOME")) . 2)))
-
+  :bind-keymap (("s-p" . projectile-command-map)  ; "s-" is "super"
+                ("C-c p" . projectile-command-map))
+  :custom ((projectile-auto-discover nil)
+           (projectile-enable-caching nil)
+           (projectile-git-fd-args "-H -0 -E .git -tf")
+           (projectile-ignored-projects '("~/" "~/.pyenv/"))
+           (projectile-indexing-method 'alien)
+           (projectile-mode-line-function
+            '(lambda ()
+               (format " [%s]" (projectile-project-name))))
+           (projectile-project-root-functions
+            '(projectile-root-local
+              projectile-root-bottom-up
+              projectile-root-top-down
+              projectile-root-top-down-recurring))
+           (projectile-project-search-path
+            `(,(locate-user-emacs-file "straight/repos/")
+              (,(expand-file-name "github.com/"
+                                  (getenv "HOME")) . 2))))
   :ensure-system-package
   (ag . "sudo apt install -y silversearcher-ag")
   (fdfind . "sudo apt install -y fd-find")
@@ -40,7 +38,6 @@
                               projectile-project-root stringp
                               projectile-project-run-cmd stringp
                               projectile-project-test-cmd stringp)
-
   :config
   (projectile-mode +1)
 
