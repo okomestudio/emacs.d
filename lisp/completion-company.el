80b3219681c2497d564a7e9d6e2c6dca4a85b22e
Taro Sato <okomestudio@gmail.com>
2024-09-05 11:25:14 -0700

    Move completion-company.el

--- lisp/completion-company.el
+++ lisp/completion-company.el
@@ -1,92 +1,79 @@
-;;; 40-completion-company.el --- completion  -*- lexical-binding: t -*-
+;;; completion-company.el --- Completion with Company  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Configure Company and related utilities.
+;; Completion setup with `company'.
 ;;
 ;;; Code:
 
 (use-package company
-  :disabled
   ;; Do not use `global-company-mode' when `lsp' completion is also in use.
-  :bind
-  (;; See https://emacs.stackexchange.com/a/24800/599 for key binding tips.
-   :map company-mode-map
-   ("C-<tab>" . company-indent-or-complete-common)
+  :bind (; See emacs.stackexchange.com/a/24800/599 for tips.
+         :map company-mode-map
+         ("C-<tab>" . company-indent-or-complete-common)
 
-   :map company-active-map
-   ("SPC" . nil)
-   ;; See https://emacs.stackexchange.com/q/9631/599:
-   ("<tab>" . company-complete-selection)
-   ("TAB" . company-complete-selection)
+         :map company-active-map
+         ("SPC" . nil)
 
-   :map company-active-map
-   :filter (company-explicit-action-p)
-   ("<return>" . company-complete-selection)
-   ("RET" . company-complete-selection))
+         ;; See https://emacs.stackexchange.com/q/9631/599:
+         ("<tab>" . company-complete-selection)
+         ("TAB" . company-complete-selection)
 
-  :custom
-  (company-idle-delay 1.0)
-  (company-minimum-prefix-length 2)
-  (company-selection-wrap-around t)
-  (company-show-numbers 'left)
-  (company-tooltip-align-annotations t)
-  (company-tooltip-limit 10))
-
+         :map company-active-map
+         :filter (company-explicit-action-p)
+         ("<return>" . company-complete-selection)
+         ("RET" . company-complete-selection))
+  :custom ((company-idle-delay 1.0)
+           (company-minimum-prefix-length 2)
+           (company-selection-wrap-around t)
+           (company-show-numbers 'left)
+           (company-tooltip-align-annotations t)
+           (company-tooltip-limit 10)))
 
 ;; COMPANY FRONTENDS
 
 (use-package company-quickhelp
   :disabled
-  :bind (:map company-active-map
-              ("C-c h" . company-quickhelp-manual-begin))
+  :bind (;
+         :map company-active-map
+         ("C-c h" . company-quickhelp-manual-begin))
   :custom (company-quickhelp-delay 2.0)
   :hook (company-mode . company-quickhelp-mode))
-
 
 (use-package company-box
   :disabled
   :hook (company-mode . company-box-mode)
   :custom (company-box-doc-delay 1.0))
 
-
 (use-package company-posframe
   :disabled
   :hook (company-mode . (lambda () (company-posframe-mode 1))))
-
 
 ;; COMPANY BACKENDS
 
 (use-package company-graphviz-dot
   :disabled
-  :after (graphviz-dot-mode)
   :straight nil
-
+  :after (graphviz-dot-mode)
   :init
   (ok-file-ensure-from-github
    "ppareit/graphviz-dot-mode/master/company-graphviz-dot.el"))
 
-
 (use-package company-restclient
   :disabled
   :after (company restclient)
-
   :config
   (add-to-list 'company-backends 'company-restclient))
-
 
 (use-package company-shell
   :disabled
   :after company
-
   :config
   (add-to-list 'company-backends '(company-shell company-shell-env)))
 
-
 (use-package company-tern
   :disabled
-  :after (company dash dash-functional tern)
   :straight nil
-
+  :after (company dash dash-functional tern)
   :init
   (ok-file-ensure-from-url
    (concat "https://gist.githubusercontent.com/"
@@ -94,12 +81,10 @@
            "raw/1193992ffeeca8193ebf459b377c27f628ac3246/company-tern.el"))
   (add-to-list 'company-backends 'company-tern))
 
-
 (use-package company-web
   :disabled
   :after company
-
   :init
   (add-to-list 'company-backends 'company-web-html))
 
-;;; 40-completion-company.el ends here
+;;; completion-company.el ends here
