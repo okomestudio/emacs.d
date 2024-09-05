5a3887440b5888bde4ce5d8d40c7961e952239f6
Taro Sato <okomestudio@gmail.com>
2024-09-05 15:40:20 -0700

    Clean up

--- lisp/popper.el
+++ lisp/popper.el
@@ -6,42 +6,40 @@
 ;;; Code:
 
 (use-package popper
-  :bind
-  (("C-~"   . popper-toggle)
-   ("C-`"   . popper-cycle)
-   ("C-M-`" . popper-toggle-type))
+  :bind (("C-~"   . popper-toggle)
+         ("C-`"   . popper-cycle)
+         ("C-M-`" . popper-toggle-type))
 
-  :custom
-  (popper-reference-buffers
-   '("\\*Async Shell Command\\*\\'"
-     "\\*Async-native-compile-log\\*\\'"
-     "\\*Backtrace\\*\\'"
-     "\\*Buffer List\\*\\'"
-     "\\*ChatGPT>"
-     "\\*devdocs\\*\\'"
-     "\\*direnv\\*\\'"
-     "\\*elfeed-log\\*\\'"
-     "\\*init log\\*\\'"
-     "\\*lsp-log\\*\\'"
-     "\\*Messages\\*\\'"
-     "\\*scratch\\*\\'"
-     "\\*straight-process\\*\\'"
-     "\\*Warnings\\*\\'"
-     ;; "CAPTURE-.*\\.org\\'"
-     "Output\\*\\'"
-     comint-mode
-     compilation-mode
-     help-mode
-     helpful-mode
-     magit-process-mode
-     magit-status-mode))
-  (popper-display-control t)
-  (popper-display-function #'display-buffer-pop-up-window)
-  (popper-window-height (lambda (win)
-                          (fit-window-to-buffer
-                           win
-                           (* 2 (floor (frame-height) 5))
-                           (floor (frame-height) 3))))
+  :custom ((popper-reference-buffers
+            '("\\*Async Shell Command\\*\\'"
+              "\\*Async-native-compile-log\\*\\'"
+              "\\*Backtrace\\*\\'"
+              "\\*Buffer List\\*\\'"
+              "\\*ChatGPT>"
+              "\\*devdocs\\*\\'"
+              "\\*direnv\\*\\'"
+              "\\*elfeed-log\\*\\'"
+              "\\*init log\\*\\'"
+              "\\*lsp-log\\*\\'"
+              "\\*Messages\\*\\'"
+              "\\*scratch\\*\\'"
+              "\\*straight-process\\*\\'"
+              "\\*Warnings\\*\\'"
+              ;; "CAPTURE-.*\\.org\\'"
+              "Output\\*\\'"
+              comint-mode
+              compilation-mode
+              help-mode
+              helpful-mode
+              magit-process-mode
+              magit-status-mode))
+           (popper-display-control t)
+           (popper-display-function #'display-buffer-pop-up-window)
+           (popper-window-height (lambda (win)
+                                   (fit-window-to-buffer
+                                    win
+                                    (* 2 (floor (frame-height) 5))
+                                    (floor (frame-height) 3)))))
 
   :init
   (popper-mode +1)
