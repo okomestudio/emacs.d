5c4cb9d5049fbe2aff1c13f0ec4f8e431bed0823
Taro Sato <okomestudio@gmail.com>
2024-09-05 14:53:03 -0700

    Clean up anki

--- lisp/anki.el
+++ lisp/anki.el
@@ -1,4 +1,4 @@
-;;; anki.el --- anki  -*- lexical-binding: t -*-
+;;; anki.el --- Anki  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
 ;; Anki (apps.ankiweb.net) is a tool for spaced repetition.
@@ -13,35 +13,40 @@
 ;;; Code:
 
 (use-package anki-editor
-  ;; Emacs minor mode for making Anki cards with Org.
-  :straight (:repo "anki-editor/anki-editor")
-  :commands (anki-editor-note-at-point)
+  :straight (:host github :repo "anki-editor/anki-editor")
+  :commands (anki-editor-note-at-point
+             anki-editor-push-note-at-point)
   :ensure-system-package (curl . "sudo apt install -y curl")
+  :custom ((anki-editor-latex-style 'mathjax)
 
-  :custom
-  (anki-editor-latex-style 'mathjax)
-
-  ;; Use Org tags for Anki, but specify those to be ignored:
-  (anki-editor-org-tags-as-anki-tags t)
-  (anki-editor-ignored-org-tags '("anki"
-                                  "export"
-                                  "m"
-                                  "main"
-                                  "noexport"
-                                  "z"))
-
-  ;; (request-log-level 'debug) ;; for debugging
-
+           ;; Use Org tags for Anki, but specify those to be ignored
+           (anki-editor-org-tags-as-anki-tags t)
+           (anki-editor-ignored-org-tags '("anki"
+                                           "export"
+                                           "m"
+                                           "main"
+                                           "noexport"
+                                           "z"))
+           ;; (request-log-level 'debug)  ; for debugging
+           )
   :init
   (with-eval-after-load 'org
-    (add-to-list 'org-global-properties-fixed '("ANKI_DECK_ALL" . "Default"))
-    (add-to-list 'org-global-properties-fixed
-                 `("ANKI_NOTE_TYPE_ALL"
-                   . ,(string-join '("Basic"
-                                     "\"Basic (and reversed card)\""
-                                     "Cloze") " ")))
-    (add-to-list 'org-default-properties "ANKI_DECK")
-    (add-to-list 'org-default-properties "ANKI_NOTE_ID")
-    (add-to-list 'org-default-properties "ANKI_NOTE_TYPE")))
+    (let ((props '(("ANKI_DECK" . ("Default"))
+                   ("ANKI_NOTE_ID")
+                   ("ANKI_NOTE_TYPE" . ("Basic"
+                                        "Basic (and reversed card)"
+                                        "Cloze")))))
+      (dolist (prop props)
+        (let ((keyword (car prop))
+              (values (cdr prop)))
+          (when values
+            (add-to-list 'org-global-properties-fixed
+                         `(,(concat keyword "_ALL")
+                           .
+                           ,(mapconcat (lambda (s)
+                                         (format "\"%s\"" s))
+                                       values
+                                       " "))))
+          (add-to-list 'org-default-properties keyword))))))
 
 ;;; anki.el ends here
