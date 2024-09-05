6bd79e5fe4ade565eb4db036256fd7af2f053c4e
Taro Sato <okomestudio@gmail.com>
2024-09-05 13:33:58 -0700

    Clean up

--- lisp/org.el
+++ lisp/org.el
@@ -1,61 +1,50 @@
-;;; org.el --- org  -*- lexical-binding: t -*-
+;;; org.el --- Org Mode  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Configure Org and related utilities.
+;; `org-mode' initialization.
 ;;
 ;;; Code:
 
 (use-package org
-  :bind (nil
+  :bind (;
          :map org-mode-map
          ("C-c C-l" . org-insert-link)
          ("C-c C-q" . org-set-tags-command)
-         ("C-c i h" . org-insert-heading) ;; or use M-RET
+         ("C-c i h" . org-insert-heading)  ; or use M-RET
          ("C-c i l" . ok-org-insert-item)
          ("C-c i u" . org-cliplink)
          ("C-c l" . org-store-link)
          ("M-g i" . consult-org-heading))
-  :hook (org-mode . ok-org-mode-hook)
-  :custom
-  (org-adapt-indentation nil)
-  (org-blank-before-new-entry '((heading . nil) (plain-list-item . nil)))
-  (org-ellipsis "⮷")
-  (org-file-apps '(("\\.mp4\\'" . "vlc --repeat %s")))
-  (org-footnote-section nil)
-  (org-hide-emphasis-markers t)
-  (org-image-actual-width nil)
-  (org-imenu-depth 6)
-  (org-list-allow-alphabetical t)
-  (org-list-indent-offset 2)
-  (org-M-RET-may-split-line '((headline . nil) (default . t)))
-  (org-preview-latex-image-directory ".ltximg/")
-  (org-return-follows-link t)
-  (org-startup-folded nil)
-  (org-startup-indented t)
-  (org-support-shift-select t)
-  (org-tags-column 0)
-  (org-todo-keywords '((sequence "TODO" "WIP" "|" "SKIP" "DONE")))
-
+  :hook (org-mode . ok-org--init-visuals)
+  :custom ((org-adapt-indentation nil)
+           (org-blank-before-new-entry '((heading . nil)
+                                         (plain-list-item . nil)))
+           (org-ellipsis "⮷")
+           (org-file-apps '(("\\.mp4\\'" . "vlc --repeat %s")))
+           (org-footnote-section nil)
+           (org-hide-emphasis-markers t)
+           (org-image-actual-width nil)
+           (org-imenu-depth 6)
+           (org-list-allow-alphabetical t)
+           (org-list-indent-offset 2)
+           (org-M-RET-may-split-line '((headline . nil)
+                                       (default . t)))
+           (org-preview-latex-image-directory ".ltximg/")
+           (org-return-follows-link t)
+           (org-startup-folded nil)
+           (org-startup-indented t)
+           (org-support-shift-select t)
+           (org-tags-column 0)
+           (org-todo-keywords '((sequence "TODO" "WIP" "|" "SKIP" "DONE"))))
   :ensure-system-package
   (latex . "sudo apt install -y texlive texlive-latex-extra texlive-lang-cjk texlive-extra-utils texlive-luatex texlive-science")
   (pdfcropmargins . "pip install pdfCropMargins")
 
   :config
-  (defun ok-org-mode-hook ()
+  ;; HELPER FUNCTIONS
+  (defun ok-org--init-visuals ()
     (setq-local fill-column 80)
     (turn-on-visual-line-mode))
-
-  ;; HELPER FUNCTIONS
-  (defun ok-org-link-get (&optional arg)
-    "Extract URL from org-mode link and add it to kill ring.
-
-See https://emacs.stackexchange.com/a/60555/599."
-    (interactive "P")
-    (let* ((link (org-element-lineage (org-element-context) '(link) t))
-           (type (org-element-property :type link))
-           (url (org-element-property :path link))
-           (url (concat type ":" url)))
-      (kill-new url)))
 
   (defun ok-org-insert-item (begin end)
     "See Section 14.44 at https://takaxp.github.io/init.html."
@@ -73,98 +62,56 @@ See https://emacs.stackexchange.com/a/60555/599."
         (setq end (+ end len)))
       (goto-char begin)))
 
-  (defun org-ensure-all-headings-with-ids ()
-    "Ensure all headings have IDs."
-    (interactive)
-    (save-excursion
-      (goto-char (point-max))
-      (while (outline-previous-heading)
-        (org-id-get-create))))
-
-  (defun org-interpolate-leaf-nodes-for-export ()
-    "Extrapolate leaf heading nodes for export.
-
-When invoked within an Org buffer, the headings are traversed in
-its copy, each leaf heading expanded with the body of the target
-node."
-    (interactive)
-    (let* ((tmp-buffer (org-export-copy-buffer)))
-      (with-current-buffer tmp-buffer
-        (beginning-of-buffer)
-        (while (outline-next-heading)
-          (while (org-goto-first-child) t)
-          (end-of-line)
-          (backward-char)
-          (when (link-hint--org-link-at-point-p)
-            (let* ((has-content nil))
-              (save-excursion
-                (org-open-at-point +1)
-                (beginning-of-line)
-                (if (eq ?* (char-after))
-                    (setq has-content t))
-                (with-current-buffer (current-buffer)
-                  (org-preserve-local-variables
-                   (let* ((end (org-end-of-subtree t t)))
-                     (previous-line)
-                     (org-back-to-heading)
-                     (copy-region-as-kill (re-search-forward "^\\s-*$") end)))
-                  (kill-buffer)))
-              (end-of-line)
-              (org-return-and-maybe-indent)
-              (when has-content
-                (org-yank))))))
-      (switch-to-buffer tmp-buffer)))
-
   ;; EMPHASIS
-  (setq org-emphasis-regexp-components
-        `(;; pre match
-          ,(concat (string ?\[ ?\( ?{)
-                   "[:space:][:multibyte:]"
-                   (string ?\N{ZERO WIDTH SPACE}
-                           ?' ?‘ ?\" ?“
-                           ?| ?│
-                           ?— ?-))     ;; "-" must be the last char
-          ;; post match
-          ,(concat (string ?\] ?\) ?}) ;; "]" must be the first char
-                   "[:space:][:multibyte:]"
-                   (string ?\N{ZERO WIDTH SPACE}
-                           ?' ?’ ?\" ?”
-                           ?| ?│
-                           ?. ?, ?? ?! ?\; ?:
-                           ?s      ;; allow use like =def=s
-                           ?— ?-)) ;; "-" must be the last char
-          "[:space:]"              ;; forbidden border chars
-          "."                      ;; body "."
-          1))                      ;; max newlines
-  ;; See `org-emph-re' and `org-verbatim-re' for the final regexps
-  (org-set-emph-re 'org-emphasis-regexp-components
-                   org-emphasis-regexp-components)
+  (let ((regexp-components
+         `(;; pre match
+           ,(concat (string ?\[ ?\( ?{)
+                    "[:space:][:multibyte:]"
+                    (string ?\N{ZERO WIDTH SPACE}
+                            ?' ?‘ ?\" ?“
+                            ?| ?│
+                            ?— ?-))      ; "-" must be the last char
+           ;; post match
+           ,(concat (string ?\] ?\) ?})  ; "]" must be the first char
+                    "[:space:][:multibyte:]"
+                    (string ?\N{ZERO WIDTH SPACE}
+                            ?' ?’ ?\" ?”
+                            ?| ?│
+                            ?. ?, ?? ?! ?\; ?:
+                            ?s       ; allow use like =def=s
+                            ?— ?-))  ; "-" must be the last char
+           "[:space:]"               ; forbidden border chars
+           "."                       ; body "."
+           1)))                      ; max newlines
+    ;; See `org-emph-re' and `org-verbatim-re' for the final regexps
+    (org-set-emph-re 'org-emphasis-regexp-components regexp-components))
 
   ;; LATEX PREVIEW
+  (setopt org-preview-latex-default-process 'lualatexpdf)
   ;; (setq org-latex-inputenc-alist '(("utf8" . "utf8x")))
   ;; (add-to-list 'org-latex-packages-alist '("" "unicode-math"))
-  (setopt org-preview-latex-default-process 'lualatexpdf)
-  (add-to-list 'org-preview-latex-process-alist
-               '(lualatexpdf
-                 :programs ("lualatex" "dvisvgm")
-                 :description "pdf > svg"
-                 :message "you need to install the programs: lualatex and dvisvgm."
-                 :image-input-type "pdf"
-                 :image-output-type "svg"
-                 :image-size-adjust (1.7 . 1.5)
-                 :latex-compiler ("lualatex -interaction nonstopmode --shell-escape -output-directory %o %f")
-                 :image-converter ("pdfcropmargins -v -p 0 -a -5 %f -o /tmp/cropped.pdf ; dvisvgm -P /tmp/cropped.pdf -n -b min -c %S -o %O")))
-  (add-to-list 'org-preview-latex-process-alist
-               '(lualatexdvi
-                 :programs ("lualatex" "dvisvgm")
-                 :description "dvi > svg"
-                 :message "you need to install the programs: lualatex and dvisvgm."
-                 :image-input-type "dvi"
-                 :image-output-type "svg"
-                 :image-size-adjust (1.7 . 1.5)
-                 :latex-compiler ("dvilualatex -interaction nonstopmode --shell-escape -output-directory %o %f")
-                 :image-converter ("dvisvgm %f -n -b min -c %S -o %O"))))
-
+  (add-to-list
+   'org-preview-latex-process-alist
+   '(lualatexpdf
+     :programs ("lualatex" "dvisvgm")
+     :description "pdf > svg"
+     :message "you need to install the programs: lualatex and dvisvgm."
+     :image-input-type "pdf"
+     :image-output-type "svg"
+     :image-size-adjust (1.7 . 1.5)
+     :latex-compiler ("lualatex -interaction nonstopmode --shell-escape -output-directory %o %f")
+     :image-converter ("pdfcropmargins -v -p 0 -a -5 %f -o /tmp/cropped.pdf ; dvisvgm -P /tmp/cropped.pdf -n -b min -c %S -o %O")))
+  (add-to-list
+   'org-preview-latex-process-alist
+   '(lualatexdvi
+     :programs ("lualatex" "dvisvgm")
+     :description "dvi > svg"
+     :message "you need to install the programs: lualatex and dvisvgm."
+     :image-input-type "dvi"
+     :image-output-type "svg"
+     :image-size-adjust (1.7 . 1.5)
+     :latex-compiler ("dvilualatex -interaction nonstopmode --shell-escape -output-directory %o %f")
+     :image-converter ("dvisvgm %f -n -b min -c %S -o %O"))))
 
 ;; ORG BABEL
 
@@ -257,10 +204,8 @@ node."
 (use-package ox
   :straight nil
   :after (org)
-  :custom
-  (org-export-with-broken-links t)
-  (org-export-with-section-numbers nil)
-
+  :custom ((org-export-with-broken-links t)
+           (org-export-with-section-numbers nil))
   :config
   (require 'ox-gfm)
   (require 'ox-hugo)
@@ -276,10 +221,9 @@ node."
 (use-package ox-latex
   :straight nil
   :after ox
-  :custom
-  (org-latex-pdf-process
-   '("lualatex -interaction nonstopmode -shell-escape %f"
-     "lualatex -interaction nonstopmode -shell-escape %f")))
+  :custom ((org-latex-pdf-process
+            '("lualatex -interaction nonstopmode -shell-escape %f"
+              "lualatex -interaction nonstopmode -shell-escape %f"))))
 
 (use-package ox-md ;; markdown
   :straight nil
@@ -290,95 +234,88 @@ node."
 (use-package org-agenda
   :straight nil
   :after (org)
-  :custom
-  (org-agenda-current-time-string "⭠ NOW ────────────────────")
-  (org-agenda-include-diary t)
-  (org-agenda-inhibit-startup t)
-  (org-agenda-start-on-weekday 0)
-  (org-agenda-use-tag-inheritance t)    ; set nil to speed up parsing
+  :custom ((org-agenda-current-time-string "⭠ NOW ────────────────────")
+           (org-agenda-include-diary t)
+           (org-agenda-inhibit-startup t)
+           (org-agenda-start-on-weekday 0)
+           (org-agenda-use-tag-inheritance t))  ; set nil to speed up parsing
   :preface
   (put 'org-agenda-custom-commands 'safe-local-variable #'listp))
 
 ;; APPEARANCE
 
 (use-package org-modern
-  :custom
-  (org-modern-block-name t) ;; use org-modern-indent
-  (org-modern-checkbox '((?X . #("▢𐄂" 0 2 (composition ((2)))))
-                         (?- . #("▢–" 0 2 (composition ((2)))))
-                         (?\s . #("▢" 0 1 (composition ((1)))))))
-  (org-modern-hide-stars 'nil)
-  (org-modern-keyword "‣ ")
-  (org-modern-list '((?+ . "▷")
-                     (?- . "𑁋") ;; "–"
-                     (?* . "▶")))
-  (org-modern-priority t)
-  (org-modern-star '("◉" "🞛" "○" "▷"))
-  (org-modern-statistics t)
-  (org-modern-table nil)
-  (org-modern-tag t)
-  (org-modern-timestamp t)
-  (org-modern-todo t)
-  :hook
-  (org-mode . org-modern-mode)
-  (org-agenda-finalize . org-modern-agenda))
+  :custom ((org-modern-block-name t)  ; use `org-modern-indent'
+           (org-modern-checkbox '((?X . #("▢𐄂" 0 2 (composition ((2)))))
+                                  (?- . #("▢–" 0 2 (composition ((2)))))
+                                  (?\s . #("▢" 0 1 (composition ((1)))))))
+           (org-modern-hide-stars nil)
+           (org-modern-keyword "‣ ")
+           (org-modern-list '((?+ . "▷")
+                              (?- . "𑁋")  ; "–"
+                              (?* . "▶")))
+           (org-modern-priority t)
+           (org-modern-star '("◉" "🞛" "○" "▷"))
+           (org-modern-statistics t)
+           (org-modern-table nil)
+           (org-modern-tag t)
+           (org-modern-timestamp t)
+           (org-modern-todo t))
+  :hook ((org-mode . org-modern-mode)
+         (org-agenda-finalize . org-modern-agenda)))
 
 ;; APPEARANCE - INDENTATION
+
+(defun ok-invoke-only-on-current-buffer-window (func &rest rest)
+  "Invoke FUNC only when buffer window is visible."
+  (when (get-buffer-window (current-buffer) t)
+    (apply func rest)))
 
 (use-package org-indent
   :straight nil
   :after (org)
   :hook (org-mode . (lambda () (org-indent-mode 1)))
   :config
-  (advice-add #'org-indent-refresh-maybe :around
-              (lambda (fun &rest args)
-                "Refresh only when buffer is visible."
-                ;; This could speed up org-agenda in some cases.
-                (when (get-buffer-window (current-buffer) t)
-                  (apply fun args)))))
+  (advice-add #'org-indent-refresh-maybe
+              :around #'ok-invoke-only-on-current-buffer-window))
 
 (use-package org-margin
-  :disabled ;; conflicts with `org-indent-mode'
-  :after org
+  :disabled  ; conflicts with `org-indent-mode'
   :straight (:host github :repo "rougier/org-margin")
+  :after org
   :hook (org-mode . (lambda () (org-margin-mode 1)))
-  :init
-  (use-package svg-lib))
+  :init (use-package svg-lib))
 
 (use-package org-modern-indent
   :straight (:host github :repo "jdtsmith/org-modern-indent")
-  :init (add-hook 'org-mode-hook #'org-modern-indent-mode 90)
-  :hook
-  (org-indent-mode . (lambda()
-                       ;; See github.com/jdtsmith/org-modern-indent/issues/10
-                       (require 'org-indent)
-                       (if org-indent--text-line-prefixes
-                           (aset org-indent--text-line-prefixes
-                                 0 (propertize " " 'face 'org-indent)))))
+  :hook (org-indent-mode . org-modern-indent-ok--fix-top-level-indent)
+  :init
+  (add-hook 'org-mode-hook #'org-modern-indent-mode 90)
+
   :config
-  (advice-add #'org-modern-indent--refresh-watch :around
-              (lambda (fun beg end &rest r)
-                "Refresh only when buffer is visible."
-                ;; This could speed up org-agenda in some cases.
-                (when (get-buffer-window (current-buffer) t)
-                  (apply fun beg end r)))))
+  (advice-add #'org-modern-indent--refresh-watch
+              :around #'ok-invoke-only-on-current-buffer-window)
+
+  (defun org-modern-indent-ok--fix-top-level-indent ()
+    ;; See github.com/jdtsmith/org-modern-indent/issues/10
+    (if org-indent--text-line-prefixes
+        (aset org-indent--text-line-prefixes
+              0 (propertize " " 'face 'org-indent)))))
 
 ;;; APPEARANCE - TABLE
 
 (use-package valign
   ;; Pixel-perfect visual alignment for Org and Markdown tables.
-  :hook (org-mode . valign--maybe-activate)
-  :custom
-  (valign-fancy-bar t)
-  (valign-max-table-size 4000)
-  (valign-signal-parse-error t)
-
+  :hook (org-mode . valign-ok--maybe-activate)
+  :custom ((valign-fancy-bar t)
+           (valign-max-table-size 4000)
+           (valign-signal-parse-error t))
   :config
-  (defvar ok-org--valign-max-buffer-size 100000
+  (defvar valign-ok--max-buffer-size 100000
     "Default max-buffer-size above which `valign-mode' will not activate.")
 
-  (defun valign--maybe-activate ()
-    (when (<= (buffer-size) ok-org--valign-max-buffer-size)
+  (defun valign-ok--maybe-activate ()
+    (when (<= (buffer-size) valign-ok--max-buffer-size)
       (valign-mode 1))))
 
 ;; THEME
@@ -396,7 +333,7 @@ node."
 (use-package org-web-tools :disabled)
 
 (use-package org-transclusion
-  :bind (;; no globals
+  :bind (;
          :map org-mode-map
          :prefix "C-c C-n"
          :prefix-map ok-org-transclusion-map
@@ -426,7 +363,4 @@ node."
   (add-to-list 'org-transclusion-extensions 'org-transclusion-indent-mode)
   (require 'org-transclusion-indent-mode))
 
-;; Local Variables:
-;; nameless-aliases: (("" . "ok-org"))
-;; End:
 ;;; org.el ends here
