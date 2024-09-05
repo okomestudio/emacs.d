99563e9ca6b121d8e5e117130c79ff6694b9bb82
Taro Sato <okomestudio@gmail.com>
2024-09-05 16:07:19 -0700

    Add themes-modeline.el

---
+++ lisp/themes-modeline.el
@@ -0,0 +1,45 @@
+;;; themes-modeline.el --- Modeline  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Modeline.
+;;
+;;; Code:
+
+(use-package doom-modeline
+  ;; A fancy and fast mode-line inspired by minimalism design.
+  :custom ((doom-modeline-buffer-encoding nil)
+           (doom-modeline-buffer-file-name-style 'buffer-name)
+           (doom-modeline-height 1)
+           (doom-modeline-minor-modes t)
+           (doom-modeline-vcs-max-length 12)
+           (mode-line-percent-position nil)
+           (doom-modeline-checker-simple-format t))
+  :hook
+  (after-load-theme . (lambda ()
+                        (if (member (car custom-enabled-themes)
+                                    '(flexoki-themes-dark
+                                      flexoki-themes-light))
+                            (doom-modeline-mode +1)
+                          (if (default-value 'doom-modeline-mode)
+                              (doom-modeline-mode -1)))))
+
+  :config
+  (doom-modeline-def-modeline 'lsp-full
+    '(bar workspace-name window-number modals matches follow buffer-info remote-host buffer-position word-count parrot selection-info)
+    '(compilation objed-state persp-name battery grip irc mu4e gnus github debug repl minor-modes input-method indent-info buffer-encoding major-mode vcs lsp misc-info time))
+
+  (dolist (mode '(python-ts-mode python-sql-ts-mode))
+    (add-to-list 'doom-modeline-mode-alist `(,mode . lsp-full))))
+
+(use-package doom-nano-modeline
+  :disabled
+  :requires (nano-theme)
+  :straight (:host github :repo "ronisbr/doom-nano-modeline")
+  :config (doom-nano-modeline-mode 1))
+
+(use-package minions
+  ;; A minor-mode menu for the mode line.
+  :custom (minions-direct '(projectile-mode))
+  :hook (after-load-theme . (lambda () (minions-mode 1))))
+
+;;; themes-modeline.el ends here
