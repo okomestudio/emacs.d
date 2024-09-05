4e5dd6917aae24e25b234b07a45ad6e69389448f
Taro Sato <okomestudio@gmail.com>
2024-09-05 15:46:39 -0700

    Clean up

--- lisp/treemacs.el
+++ lisp/treemacs.el
@@ -1,31 +1,27 @@
-;;; treemacs.el --- treemacs  -*- lexical-binding: t -*-
+;;; treemacs.el --- Treemacs  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Configure Treemacs and related utilities.
+;; Treemacs configuration.
 ;;
 ;;; Code:
 
 (use-package treemacs
-  :bind
-  (([f8] . ok-treemacs-show)
-   ([mouse-1] . treemacs-single-click-expand-action)
-   ("M-0" . treemacs-select-window))
-
-  :custom
-  (treemacs-indentation 2)
-  (treemacs-indentation-string " ")
-  (treemacs-no-png-images t) ;; set to nil for image icons
-  (treemacs-show-cursor t)
-  (treemacs-wide-toggle-width 80)
-  (treemacs-width 25)
-
+  :bind (([f8] . treemacs-ok-toggle)
+         ([mouse-1] . treemacs-single-click-expand-action)
+         ("M-0" . treemacs-select-window))
+  :custom ((treemacs-indentation 2)
+           (treemacs-indentation-string " ")
+           (treemacs-no-png-images t)  ; set to nil for image icons
+           (treemacs-show-cursor t)
+           (treemacs-wide-toggle-width 80)
+           (treemacs-width 25))
   :config
   (use-package cfrs
     ;; A simple alternative to read-string that allows reading input via a small
     ;; child-frame spawned at the position of the cursor.
     :defer t)
 
-  (defun ok-treemacs-show ()
+  (defun treemacs-ok-toggle ()
     (interactive)
     (push-mark)
     (treemacs)
@@ -40,37 +36,30 @@
 
   (pcase (cons (not (null (executable-find "git")))
                (not (null treemacs-python-executable)))
-    (`(t . t)
-     (treemacs-git-mode 'deferred))
-    (`(t . _)
-     (treemacs-git-mode 'simple)))
+    (`(t . t) (treemacs-git-mode 'deferred))
+    (`(t . _) (treemacs-git-mode 'simple)))
 
   (treemacs-project-follow-mode t)
   (setq treemacs--project-follow-delay 1.0
         treemacs--project-follow-timer nil))
 
-
 (use-package treemacs-nerd-icons
   :disabled ;; NOTE(2023-12-29): Disabled since char alignment is poor
   :config (treemacs-load-theme "nerd-icons"))
 
-
 (use-package treemacs-all-the-icons
-  :disabled ;; NOTE(2023-05-11): Disabled due to some formatting issue.
+  :disabled ;; NOTE(2023-05-11): Disabled due to formatting issue
   :after (all-the-icons)
   :custom (all-the-icons-scale-factor 1.0)
   :config
   (treemacs-load-theme "all-the-icons")
   (treemacs-resize-icons 20))
 
-
 (use-package treemacs-icons-dired
   :hook (dired-mode . treemacs-icons-dired-enable-once))
 
-
 (use-package treemacs-magit
   :after (treemacs magit))
-
 
 (use-package treemacs-projectile
   :after (treemacs projectile))
