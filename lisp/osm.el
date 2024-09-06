4107375eb8f8da7e8ae07a585e0b887e4d29412c
Taro Sato <okomestudio@gmail.com>
2024-09-05 21:50:49 -0700

    Move osm.el

---
+++ lisp/osm.el
@@ -0,0 +1,11 @@
+;;; osm.el --- OpenStreetMap viewer  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; The OpenStreetMap viewer configuration.
+;;
+;;; Code:
+
+(use-package osm
+  :custom (osm-copyright nil))
+
+;;; osm.el ends here
