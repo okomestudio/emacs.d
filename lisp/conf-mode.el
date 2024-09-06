4d557b24a31bed11ca7a99fcbfbeaa876e69a823
Taro Sato <okomestudio@gmail.com>
2024-09-05 21:10:08 -0700

    Clean up

--- lisp/conf-mode.el
+++ lisp/conf-mode.el
@@ -12,19 +12,15 @@
   ;; (ok-file-ensure-from-url
   ;;  "https://www.emacswiki.org/emacs/download/any-ini-mode.el")
 
-  :mode
-  ("\\.ini\\'"
-   "\\.conf\\'"))
-
+  :mode ("\\.ini\\'"
+         "\\.conf\\'"))
 
 (use-package git-modes
   ;; Major modes for .git(attributes|config|ignore)
   )
 
-
 (use-package systemd
   :mode ("\\.\\(service|timer\\)\\'" . systemd-mode))
-
 
 ;; Config modes not derived from `conf-mode'
 
