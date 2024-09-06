05cad8493f1f59952d85acc4022d86479296780b
Taro Sato <okomestudio@gmail.com>
2024-09-05 17:24:06 -0700

    Clean up

--- lisp/completion.el
+++ lisp/completion.el
@@ -12,10 +12,7 @@
           text-mode
           conf-mode
           lsp-completion-mode) . ok-cape--set-super-capf)
-
-  :custom
-  (cape-dabbrev-check-other-buffers nil)
-
+  :custom (cape-dabbrev-check-other-buffers nil)
   :config
   (require 'tempel)
 
@@ -42,56 +39,50 @@
   (add-to-list 'completion-at-point-functions #'cape-dabbrev t)
   (add-to-list 'completion-at-point-functions #'cape-keyword t))
 
-
 (use-package tabnine
   ;; One-time `tabnine-install-binary' may be necessary.
   :disabled t                           ; Disabled till
                                         ; `tabnine-util--infer-indentation-offset'
                                         ; error gets resolved
   :commands (tabnine-start-process)
-  :hook ((prog-mode . tabnine-mode)
-         (kill-emacs . tabnine-kill-process))
   :bind (nil
          :map tabnine-completion-map
 	       ("TAB" . nil)
          ("<tab>" . nil))
+  :hook ((prog-mode . tabnine-mode)
+         (kill-emacs . tabnine-kill-process))
   :config (tabnine-start-process))
-
 
 ;; TEMPLATING
 
 (use-package tempel
   :bind (("M-+" . tempel-complete)
          ("M-*" . tempel-insert))
-  :custom
-  (tempel-path `(,(no-littering-expand-etc-file-name "tempel/templates.el"))))
-
+  :custom (tempel-path `(,(no-littering-expand-etc-file-name
+                           "tempel/templates.el"))))
 
 ;; CORFU
 
 (use-package corfu
   :straight (:host github :repo "minad/corfu" :files (:defaults "extensions/*"))
-  :hook (((conf-mode
-           prog-mode
-           text-mode) . corfu-mode))
-  :custom
-  (corfu-auto t)
-  (corfu-auto-delay 0.5)
-  (corfu-auto-prefix 1)
-  (corfu-cycle t)
-  (corfu-on-exact-match nil)
-  (corfu-preselect 'prompt)
-  (corfu-quit-no-match 'separator)
-  (corfu-scroll-margin 5)
-
+  :hook ((conf-mode
+          prog-mode
+          text-mode) . corfu-mode)
+  :custom ((corfu-auto t)
+           (corfu-auto-delay 0.5)
+           (corfu-auto-prefix 1)
+           (corfu-cycle t)
+           (corfu-on-exact-match nil)
+           (corfu-preselect 'prompt)
+           (corfu-quit-no-match 'separator)
+           (corfu-scroll-margin 5))
   :init (global-corfu-mode)
-
   :config
   (defun corfu-enable-in-minibuffer ()
     "Enable Corfu in the minibuffer."
     (when (local-variable-p 'completion-at-point-functions)
-      ;; (setq-local corfu-auto nil) ;; Enable/disable auto completion
-      (setq-local corfu-echo-delay nil ;; Disable automatic echo and popup
+      ;; (setq-local corfu-auto nil)  ; Enable/disable auto completion
+      (setq-local corfu-echo-delay nil  ; Disable automatic echo and popup
                   corfu-popupinfo-delay nil)
       (corfu-mode 1)))
   ;; (add-hook 'minibuffer-setup-hook #'corfu-enable-in-minibuffer)
@@ -108,18 +99,15 @@
         ((debug error) (signal (car e) (cdr e)))))
     (advice-add #'corfu--post-command :around #'force-debug)))
 
-
 (use-package corfu-info
   ;; `M-h' toggles the info on selected item.
-  :disabled                             ; use popupinfo
+  :disabled  ; use popupinfo
   :straight nil)
-
 
 (use-package corfu-popupinfo
   :straight nil
   :hook (corfu-mode . corfu-popupinfo-mode)
   :custom (corfu-popupinfo-delay '(0.75 . 0.4)))
-
 
 (use-package nerd-icons-corfu
   ;; An SVG alternative is `kind-icon'.
@@ -127,13 +115,11 @@
   :hook (corfu-mode . (lambda () (require 'nerd-icons-corfu)))
   :config (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
 
-
 (use-package orderless
-  :custom
-  (completion-styles '(orderless basic))
-  ;; (completion-category-defaults nil)
-  (completion-category-overrides '((file (styles partial-completion))))
-
+  :custom ((completion-styles '(orderless basic))
+           ;; (completion-category-defaults nil)
+           (completion-category-overrides
+            '((file (styles partial-completion)))))
   :config
   ;; orderless-fast completion style:
   ;;
@@ -154,12 +140,10 @@
               (lambda ()
                 (setq-local orderless-matching-styles '(orderless-literal))))))
 
-
 (use-package prescient
   :autoload (prescient-persist-mode)
   :hook (corfu-mode . (lambda () (prescient-persist-mode 1)))
   :custom (prescient-aggressive-file-save t))
-
 
 (use-package corfu-prescient
   :after corfu
