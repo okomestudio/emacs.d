4bf0acc70f44422fa86ef90d62c10d15553caa29
Taro Sato <okomestudio@gmail.com>
2024-09-05 21:07:45 -0700

    Clean up

--- lisp/yaml.el
+++ lisp/yaml.el
@@ -8,12 +8,10 @@
 (use-package yaml-ts-mode
   :straight nil
   :mode "\\.ya?ml\\'"
-  :hook
-  (yaml-ts-mode . ok-yaml--patch-yaml-ts-mode)
-  (yaml-ts-mode . lsp-deferred)
-
+  :hook ((yaml-ts-mode . yaml-ok--patch-yaml-ts-mode)
+         (yaml-ts-mode . lsp-deferred))
   :config
-  (defun ok-yaml--patch-yaml-ts-mode ()
+  (defun yaml-ok--patch-yaml-ts-mode ()
     "Patch `yaml-ts-mode'."
     ;; The built-in `yaml-ts-mode' does not implement much of
     ;; anything. This patch loads variables from `yaml-mode' and
@@ -27,19 +25,17 @@
     (set (make-local-variable 'fill-paragraph-function) 'yaml-fill-paragraph)
     (set (make-local-variable 'syntax-propertize-function)
          'yaml-mode-syntax-propertize-function)
-    (setq font-lock-defaults '(yaml-font-lock-keywords))))
-
+    (setq-local font-lock-defaults '(yaml-font-lock-keywords))))
 
 (use-package jq-mode
   :after yaml-ts-mode
-  :bind (nil
+  :bind (;
          :map yaml-ts-mode-map
          ("C-c C-j" . jq-interactively))
   :config
   (defun jq-interactively-on-yaml (&rest r)
     (when (derived-mode-p 'yaml-ts-mode)
-      (setq-local jq-interactive-command (expand-file-name "bin/yq"
-                                                           user-emacs-directory)
+      (setq-local jq-interactive-command (locate-user-emacs-file "bin/yq")
                   jq-interactive-font-lock-mode #'yaml-ts-mode
                   jq-interactive-default-options "--yaml-roundtrip")))
   (advice-add #'jq-interactively :before #'jq-interactively-on-yaml))
