1d8564d1d5033d439e4a7468a5201f38b99e54f3
Taro Sato <okomestudio@gmail.com>
2024-09-05 20:52:47 -0700

    Move yasnippet.el

--- lisp/yasnippet.el
+++ lisp/yasnippet.el
@@ -1,10 +1,9 @@
-;;; 40-yasnippet.el --- Yasnippet  -*- lexical-binding: t -*-
+;;; yasnippet.el --- Yasnippet  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;; Code:
 
 (use-package yasnippet
-  :disabled
   :hook (prog-mode . yas-minor-mode)
   :config (yas-reload-all))
 
-;;; 40-yasnippet.el ends here
+;;; yasnippet.el ends here
