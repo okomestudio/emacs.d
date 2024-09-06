9418be49ed0f439b6d880c72bcc332393e454a4b
Taro Sato <okomestudio@gmail.com>
2024-09-05 21:33:36 -0700

    Clean up

--- lisp/ai.el
+++ lisp/ai.el
@@ -1,7 +1,7 @@
-;;; ai.el --- ai  -*- lexical-binding: t -*-
+;;; ai.el --- AI  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Configure AI and related tools, including GPT clients.
+;; AI configuration.
 ;;
 ;;; Code:
 
@@ -9,16 +9,14 @@
   :straight (:host github :repo "xenodium/chatgpt-shell")
   :autoload (chatgpt-shell-send-to-buffer)
   :commands (ask-chatgpt)
-  :custom
-  (chatgpt-shell-model-version "gpt-3.5-turbo")
-  (chatgpt-shell-openai-key (lambda ()
-                              (auth-source-pick-first-password
-                               :host "api.openai.com")))
+  :custom ((chatgpt-shell-model-version "gpt-3.5-turbo")
+           (chatgpt-shell-openai-key (lambda ()
+                                       (auth-source-pick-first-password
+                                        :host "api.openai.com"))))
   :config
   (defun ask-chatgpt (str)
     (interactive (list (ok-prompt-or-string-from-region "Ask ChatGPT: ")))
     (chatgpt-shell-send-to-buffer str)))
-
 
 (use-package gptel
   :config
@@ -29,18 +27,13 @@
            (auth-source-pick-first-password
             :host "console.anthropic.com"))))
 
-
 (use-package org-ai
   :disabled
-  :custom
-  (org-ai-default-chat-model "gpt-3.5-turbo")
-  (org-ai-image-directory "~/Downloads/org-ai/")
-  (org-ai-sd-directory "~/Downloads/org-ai/")
-
+  :custom ((org-ai-default-chat-model "gpt-3.5-turbo")
+           (org-ai-image-directory "~/Downloads/org-ai/")
+           (org-ai-sd-directory "~/Downloads/org-ai/"))
   :hook (org-mode . org-ai-mode)
-
-  :commands
-  (org-ai-mode
-   org-ai-global-mode))
+  :commands (org-ai-mode
+             org-ai-global-mode))
 
 ;;; ai.el ends here
