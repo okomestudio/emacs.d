bc56f6e73821b4e84c76309b43d005099fb2b801
Taro Sato <okomestudio@gmail.com>
2024-09-05 21:38:10 -0700

    Clean up

--- lisp/elfeed.el
+++ lisp/elfeed.el
@@ -6,16 +6,13 @@
 ;;; Code:
 
 (use-package elfeed
-  :hook ((elfeed-show-mode . ok-elfeed-show-setup)
-         (elfeed-search-update . ok-elfeed-search-setup))
-
-  :custom
-  (elfeed-curl-max-connections 16)
-  (elfeed-search-title-max-width 100)
-  (elfeed-search-title-min-width 16)
-  (elfeed-search-trailing-width 30)
-  (elfeed-show-unique-buffers nil)
-
+  :hook ((elfeed-show-mode . elfeed-ok-show-setup)
+         (elfeed-search-update . elfeed-ok-search-setup))
+  :custom ((elfeed-curl-max-connections 16)
+           (elfeed-search-title-max-width 100)
+           (elfeed-search-title-min-width 16)
+           (elfeed-search-trailing-width 30)
+           (elfeed-show-unique-buffers nil))
   :config
   (setq elfeed-log-level 'info)  ; 'info or 'debug
 
@@ -24,7 +21,7 @@
   (set-face-attribute 'elfeed-search-unread-title-face nil
                       :foreground (face-attribute 'default :foreground))
 
-  (defun ok-elfeed-show-setup ()
+  (defun elfeed-ok-show-setup ()
     ;; Remove underline from zenkaku space
     (setq-local nobreak-char-display nil)
 
@@ -35,25 +32,22 @@
                 shr-use-fonts t
                 shr-width 80))
 
-  (defun ok-elfeed-search-setup ()
+  (defun elfeed-ok-search-setup ()
     (setq-local nobreak-char-display nil)))
-
 
 (use-package elfeed-goodies
   :disabled
-  :custom
-  (elfeed-goodies/entry-pane-position 'bottom)
-  (elfeed-goodies/entry-pane-size 0.8)
-  (elfeed-goodies/feed-source-column-width 28)
-  (elfeed-goodies/tag-column-width 16)
-
+  :custom ((elfeed-goodies/entry-pane-position 'bottom)
+           (elfeed-goodies/entry-pane-size 0.8)
+           (elfeed-goodies/feed-source-column-width 28)
+           (elfeed-goodies/tag-column-width 16))
   :config
   (elfeed-goodies/setup)
 
   ;; Keymap hack to prevent elfeed-goodies from stealing M-v. See
   ;; https://emacs.stackexchange.com/a/65138/599.
   (defvar dummy-elfeed-goodies--map (make-keymap))
-  (define-minor-mode dummy-elfeed-goodies--mode
+  (define-minor-mode dummy-elfeed-goodies--modex
     "Dummy minor mode for keymap hack."
     :init-value t
     :global t
