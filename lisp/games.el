c3f0e30b3e6c7567e2e203a7b01aa53165c1ed77
Taro Sato <okomestudio@gmail.com>
2024-09-05 21:49:40 -0700

    Clean up

--- lisp/games.el
+++ lisp/games.el
@@ -6,23 +6,20 @@
 ;;; Code:
 
 (use-package tetris
-  :bind
-  (;;
-   :map tetris-mode-map
-   ("i" . tetris-rotate-prev)
-   ("j" . tetris-move-left)
-   ("k" . tetris-move-right)
-   ("m" . tetris-move-down))
-
-  :custom
-  (tetris-use-color nil)
-  (tetris-x-colors [[0.5 0.5 1]
-                    [0.7 0 1]
-                    [1 1 0]
-                    [1 0 1]
-                    [0 1 1]
-                    [0 1 0]
-                    [1 0 0]]))
+  :bind (;
+         :map tetris-mode-map
+         ("i" . tetris-rotate-prev)
+         ("j" . tetris-move-left)
+         ("k" . tetris-move-right)
+         ("m" . tetris-move-down))
+  :custom ((tetris-use-color nil)
+           (tetris-x-colors [[0.5 0.5 1]
+                             [0.7 0 1]
+                             [1 1 0]
+                             [1 0 1]
+                             [0 1 1]
+                             [0 1 0]
+                             [1 0 0]])))
 
 ;; Misc.
 
