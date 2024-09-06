493debfb51f7f6ba3bd8a7ca11b141a9f9300ba2
Taro Sato <okomestudio@gmail.com>
2024-09-05 21:21:11 -0700

    Clean up

--- lisp/lookup.el
+++ lisp/lookup.el
@@ -7,45 +7,42 @@
 
 (use-package emacs
   :straight nil
-  :bind
-  (nil
-   :prefix-map lookup-map
-   :prefix-docstring "Keymap for lookup"
-   :prefix "C-h C-l"
-   ("c" . ask-chatgpt)
-   ("g" . eww-search-goodreads)
-   ("t" . gt-do-translate)
+  :bind (;
+         :prefix-map lookup-map
+         :prefix-docstring "Keymap for lookup"
+         :prefix "C-h C-l"
+         ("c" . ask-chatgpt)
+         ("g" . eww-search-goodreads)
+         ("t" . gt-do-translate)
 
-   :prefix-map lookup-english-map
-   :prefix-docstring "Keymap for English lookup"
-   :prefix "C-h C-l e"
-   ("a" . eww-search-amazon-en)
-   ("d" . define-word-at-point)
-   ("h" . hatsuon-play-audio)
-   ("j" . eww-search-justapedia-en)
-   ("p" . powerthesaurus-lookup-dwim)
-   ("s" . eww-search-duckduckgo-en)
-   ("w" . eww-search-wikipedia-en)
+         :prefix-map lookup-english-map
+         :prefix-docstring "Keymap for English lookup"
+         :prefix "C-h C-l e"
+         ("a" . eww-search-amazon-en)
+         ("d" . define-word-at-point)
+         ("h" . hatsuon-play-audio)
+         ("j" . eww-search-justapedia-en)
+         ("p" . powerthesaurus-lookup-dwim)
+         ("s" . eww-search-duckduckgo-en)
+         ("w" . eww-search-wikipedia-en)
 
-   :prefix-map lookup-japanese-map
-   :prefix-docstring "Keymap for Japanese lookup"
-   :prefix "C-h C-l j"
-   ("a" . eww-search-amazon-ja)
-   ("d" . eww-search-weblio)
-   ("s" . eww-search-duckduckgo-ja)
-   ("w" . eww-search-wikipedia-ja)))
+         :prefix-map lookup-japanese-map
+         :prefix-docstring "Keymap for Japanese lookup"
+         :prefix "C-h C-l j"
+         ("a" . eww-search-amazon-ja)
+         ("d" . eww-search-weblio)
+         ("s" . eww-search-duckduckgo-ja)
+         ("w" . eww-search-wikipedia-ja)))
 
-;; Dictionaries
+;; DICTIONARIES
 
 (use-package define-word
   ;; Display the definition of word at point.
   :custom (define-word-default-service 'wordnik))
 
-
 (use-package powerthesaurus
   ;; Pull from doomelpa to avoid aggressive `transient' load issue (#41)
   :straight (:host github :repo "doomelpa/powerthesaurus"))
-
 
 (use-package synosaurus
   ;; An extensible thesaurus mode.
@@ -53,40 +50,35 @@
   :custom (synosaurus-choose-method 'default)
   :ensure-system-package (wn . "sudo apt install -y wordnet"))
 
-
-;; Pronunciation
+;; PRONUNCIATION
 
 (use-package hatsuon
-  :straight
-  (:host github :repo "okomestudio/hatsuon.el" :branch "main"))
+  :straight (:host github :repo "okomestudio/hatsuon.el" :branch "main"))
 
-
-;; Translation
+;; TRANSLATION
 
 (use-package go-translate
   ;; Translator framework.
-  :custom
-  (gt-buffer-render-split-width-threshold 120)
-  (gt-debug-p nil)
-  (gt-langs '(en ja))
-  (gt-default-translator
-   (gt-translator
-    :taker (gt-taker :prompt t)
-    :engines (list (gt-deepl-engine)
-                   (gt-google-engine :parse (gt-google-summary-parser))
-                   (gt-google-rpc-engine :parse (gt-google-rpc-parser))
-                   (gt-bing-engine))
-    :render (gt-buffer-render))))
+  :custom ((gt-buffer-render-split-width-threshold 120)
+           (gt-debug-p nil)
+           (gt-langs '(en ja))
+           (gt-default-translator
+            (gt-translator
+             :taker (gt-taker :prompt t)
+             :engines (list
+                       (gt-deepl-engine)
+                       (gt-google-engine :parse (gt-google-summary-parser))
+                       (gt-google-rpc-engine :parse (gt-google-rpc-parser))
+                       (gt-bing-engine))
+             :render (gt-buffer-render)))))
 
-
-;; Misc.
+;; MISC.
 
 (use-package list-unicode-display
   ;; Search for and list unicode characters.
   ;;
-  ;; Type list-unicode-display to search for unicode chars.
+  ;; Type `list-unicode-display' to search for unicode chars.
   )
-
 
 (use-package greppu
   :straight (:host github :repo "okomestudio/greppu.el")
