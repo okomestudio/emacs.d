6a4f402e311327caa881bf7c41d0a606e22af83e
Taro Sato <okomestudio@gmail.com>
2024-09-11 00:33:09 -0700

    Move repo dependent settings

--- lisp/anki.el
+++ lisp/anki.el
@@ -17,35 +17,8 @@
   :commands (anki-editor-push-note-at-point)
   :ensure-system-package (curl . "sudo apt install -y curl")
   :custom ((anki-editor-latex-style 'mathjax)
-
-           ;; Use Org tags for Anki, but specify those to be ignored
            (anki-editor-org-tags-as-anki-tags t)
-           (anki-editor-ignored-org-tags '("anki"
-                                           "export"
-                                           "m"
-                                           "main"
-                                           "noexport"
-                                           "z"))
            ;; (request-log-level 'debug)  ; for debugging
-           )
-  :init
-  (with-eval-after-load 'org
-    (let ((props '(("ANKI_DECK" . ("Default"))
-                   ("ANKI_NOTE_ID")
-                   ("ANKI_NOTE_TYPE" . ("Basic"
-                                        "Basic (and reversed card)"
-                                        "Cloze")))))
-      (dolist (prop props)
-        (let ((keyword (car prop))
-              (values (cdr prop)))
-          (when values
-            (add-to-list 'org-global-properties-fixed
-                         `(,(concat keyword "_ALL")
-                           .
-                           ,(mapconcat (lambda (s)
-                                         (format "\"%s\"" s))
-                                       values
-                                       " "))))
-          (add-to-list 'org-default-properties keyword))))))
+           ))
 
 ;;; anki.el ends here
