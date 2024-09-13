e448b71972afbad65bed70dceb6e46ca60a23f07
Taro Sato <okomestudio@gmail.com>
2024-09-12 22:34:48 -0700

    Add alert

---
+++ init.d/80-alert.el
@@ -0,0 +1,1 @@
+../lisp/alert.el
---
+++ lisp/alert.el
@@ -0,0 +1,20 @@
+;;; alert.el --- Alert  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Alert configuration.
+;;
+;;; Code:
+
+(use-package alert
+  :custom ((alert-default-style 'notifications))
+  :config
+  ;; Use `notify-move-time' with `cancel-timer' to suppress this alert
+  (setq notify-move-time
+        (run-with-timer
+         1800 1800
+         (lambda ()
+           (alert "Move away from computer!"
+                  :title "MOVE!!"
+                  :severity 'high)))))
+
+;;; alert.el ends here
