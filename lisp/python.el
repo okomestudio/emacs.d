56d7d5c973426ffa04e4a8eb6a812c8aa4e0d4d8
Taro Sato <okomestudio@gmail.com>
2024-09-13 20:44:16 -0700

    Use ruff-format

--- lisp/python.el
+++ lisp/python.el
@@ -7,17 +7,10 @@
 
 (use-package python
   :straight nil
-  :bind (;
-         :map python-mode-map
-         ("C-c b" . python-ok-format-code)
-         :map python-ts-mode-map
-         ("C-c b" . python-ok-format-code))
-
   :custom ((python-indent-guess-indent-offset-verbose nil)
            (python-indent-offset 4)
            (python-shell-interpreter (locate-user-emacs-file
                                       "bin/python-shell-interpreter"))
-
            ;; lsp
            (lsp-diagnostics-provider :flycheck)
            (lsp-lens-enable t)
@@ -35,51 +28,10 @@
            ;; ruff-lsp
            (lsp-ruff-lsp-log-level 'debug)
            (lsp-ruff-lsp-show-notifications 'always))
-
   :ensure-system-package
   (ipython . "pip install ipython")
 
   :config
-  (defun python-ok-format-code ()
-    (interactive)
-    (let ((this-file (or (buffer-file-name)
-                         (buffer-name)))
-          (saved-point (point))
-          (output-buffer "*ruff*")
-          (error-buffer "*ruff-error*")
-          (command-format "ruff format --stdin-filename %s")
-          (command-lint "ruff check --fix --select \"I\" --stdin-filename %s")
-          (orig-content (buffer-string))
-          exit-code)
-      (setq exit-code
-            (shell-command-on-region (point-min)
-                                     (point-max)
-                                     (format command-format this-file)
-                                     output-buffer
-                                     t
-                                     error-buffer))
-      (if (> exit-code 0)
-          (progn
-            (replace-region-contents (point-min)
-                                     (point-max)
-                                     (lambda () orig-content))
-            (display-buffer error-buffer #'display-buffer-pop-up-window))
-        (setq exit-code
-              (shell-command-on-region (point-min)
-                                       (point-max)
-                                       (format command-lint this-file)
-                                       output-buffer
-                                       t
-                                       error-buffer))
-        (when (> exit-code 0)
-          (replace-region-contents (point-min)
-                                   (point-max)
-                                   (lambda () orig-content))
-          (display-buffer error-buffer #'display-buffer-pop-up-window)))
-      ;; `save-execursion' does not work with destructive
-      ;; `shell-command-on-region'
-      (goto-char saved-point)))
-
   (add-hook 'python-base-mode-hook #'lsp-deferred 90)
   (add-hook 'python-sql-base-mode-hook #'lsp-deferred 90))
 
@@ -102,6 +54,14 @@
   (pyflakes . "pip install pyflakes")
   :preface
   (put 'pyimport-develop-packages 'safe-local-variable #'listp))
+
+(use-package ruff-format
+  :bind (:map python-mode-map
+              ("C-c b" . ruff-format-buffer)
+              :map python-ts-mode-map
+              ("C-c b" . ruff-format-buffer))
+  :hook (((python-mode
+           python-ts-mode) . ruff-format-on-save-mode)))
 
 ;; VIRTUAL ENVS
 
