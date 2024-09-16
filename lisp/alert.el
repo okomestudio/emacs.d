5589a8964713db68dbdc80f411e5118420399fe0
Taro Sato <okomestudio@gmail.com>
2024-09-16 11:05:06 -0700

    Improve alert logic

--- lisp/alert.el
+++ lisp/alert.el
@@ -9,21 +9,35 @@
   :demand t
   :custom ((alert-default-style 'notifications))
   :config
-  (defvar notify-move-time nil
+  (defcustom do-this-now-interval 2400
+    "Time in second before alert trigger.")
+
+  (defvar do-this-now--timer nil
     "Timer object for alert. Use `cancel-timer' to suppress the alert.")
 
-  (defun start-alert ()
-    (run-with-timer 2400 2400
-                    (lambda ()
-                      (alert "Move away from computer!"
-                             :title "MOVE!!"
-                             :severity 'high))))
+  (defun do-this-now-alert ()
+    (alert "Move away from computer!"
+           :title "MOVE!!"
+           :severity 'high)
+    (do-this-now-setup-next-alert))
 
-  (defun restart-alert ()
-    (interactive)
-    (cancel-timer notify-move-time)
-    (setq notify-move-time (start-alert)))
+  (defun do-this-now-start-timer-for-next-alert ()
+    (when do-this-now--timer
+      (cancel-timer do-this-now--timer))
+    (setq do-this-now--timer
+          (run-with-timer do-this-now-interval nil #'do-this-now-alert)))
 
-  (setq notify-move-time (start-alert)))
+  (defun do-this-now--hook ()
+    (message "BEG do-this-now-timer-for-next-alert")
+    (do-this-now-start-timer-for-next-alert)
+    (remove-hook 'post-command-hook #'do-this-now--hook)
+    (message "END do-this-now-timer-for-next-alert"))
+
+  (defun do-this-now-setup-next-alert ()
+    (message "BEG do-this-now-setup-next-alert")
+    (add-hook 'post-command-hook #'do-this-now--hook)
+    (message "END do-this-now-setup-next-alert"))
+
+  (do-this-now-setup-next-alert))
 
 ;;; alert.el ends here
