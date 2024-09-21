4f20722e20f8b47fb360be5546a3bc3f42c449fa
Taro Sato <okomestudio@gmail.com>
2024-09-20 19:19:07 -0700

    Clean up

--- lisp/elisp-inactive.el
+++ lisp/elisp-inactive.el
@@ -2,6 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package elsa
+  :ensure-system-package
+  (eask . "curl -fsSL https://raw.githubusercontent.com/emacs-eask/cli/master/webinstall/install.sh | sh")
+  :config (elsa-lsp-register))
+
+(use-package flycheck-elsa
+  :custom (flycheck-elsa-backend 'eask)
+  :hook (emacs-lisp-mode . flycheck-elsa-setup))
+
 (use-package nameless
   ;; See `read-symbol-shorthands' for built-in approach.
   :hook (emacs-lisp-mode . nameless-mode)
