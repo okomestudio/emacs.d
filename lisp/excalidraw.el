a5da9da1667cd1cfdfb9ebd174abe26385a1d907
Taro Sato <okomestudio@gmail.com>
2024-09-25 23:54:53 -0700

    Load org-excalidraw on event

--- lisp/excalidraw.el
+++ lisp/excalidraw.el
@@ -8,6 +8,7 @@
 (use-package org-excalidraw
   :straight (:host github :repo "4honor/org-excalidraw")
   :commands (org-excalidraw-create-drawing)
+  :hook (on-first-input . (lambda () (require 'org-excalidraw)))
   :ensure-system-package
   (kroki . "go install github.com/yuzutech/kroki-cli/cmd/kroki@latest"))
 
