f31d1e78f261851a1e4d2afe54286f97162d5fdc
Taro Sato <okomestudio@gmail.com>
2024-10-02 13:18:38 -0700

    Customize Kroki exec path

--- lisp/excalidraw.el
+++ lisp/excalidraw.el
@@ -9,6 +9,7 @@
   :straight (:host github :repo "4honor/org-excalidraw"
                    :fork (:host github :repo "okomestudio/org-excalidraw"
                                 :branch "custom-exec"))
+  :custom ((kroki-exec "~/.config/emacs/bin/kroki"))
   :commands (org-excalidraw-create-drawing)
   :hook (on-first-input . (lambda () (require 'org-excalidraw)))
   :ensure-system-package
