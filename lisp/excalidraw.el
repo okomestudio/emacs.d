2f961fa1e6c11ef78a4363a8ba53688424ec9768
Taro Sato <okomestudio@gmail.com>
2024-10-04 13:30:52 -0700

    Add TODO

--- lisp/excalidraw.el
+++ lisp/excalidraw.el
@@ -3,6 +3,11 @@
 ;;
 ;; Excalidraw configuration.
 ;;
+;; TODO: Enable SVG rendering. The current version Excalidraw does not
+;; render texts properly. Wait till the fix in PR
+;; https://github.com/excalidraw/excalidraw/pull/8016 gets merged and
+;; released to see if that resolves the issue.
+;;
 ;;; Code:
 
 (use-package org-excalidraw
