46efbb967aae53fdbd77ae8da2cf8dbadfc2014e
Taro Sato <okomestudio@gmail.com>
2024-10-27 00:29:22 -0700

    Update tempel templates

--- etc/tempel/templates.el
+++ etc/tempel/templates.el
@@ -1,36 +1,31 @@
 emacs-lisp-mode
 
-(elisp-begins-here ";;; replace-me.el --- Replace Me  -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
-")
+(template-begins
+ ";;; " module ".el --- " module "  -*- lexical-binding: t -*-" n)
 
-(elisp-copyright ";; Copyright (C) 2024 Taro Sato
-;;
-;; Author: Taro Sato <okomestudio@gmail.com>
-;; URL: https://github.com/okomestudio/
-;; Version: 0.1
-;; Keywords:
-;; Package-Requires: ((emacs \"29.1\"))
-;;
-;;; License:
-;;
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
-")
+(template-copyright
+ (include-file "elisp-copyright.el" ((module . module))))
 
-(elisp-ends-here "(provide 'replace-me)
-;;; replace-me ends here")
+(template-commentary
+ ";;; Commentary:" n
+ ";;" n
+ ";;" n
+ ";;" n
+ ";;; Code:" n)
+
+(template-ends
+ "(provide '" module ")" n
+ ";;; " module ".el ends here" n)
+
+(template
+ (p "module: " module t)
+ (i template-begins)
+ ";;" n
+ (i template-copyright)
+ ";;" n
+ (i template-commentary)
+ n n n
+ (i template-ends))
 
 (locvar-nameless ";; Local Variables:
 ;; nameless-aliases: ((\"\" . \"prefix\"))
