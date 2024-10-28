cb8c07473e9eb53be9413aef213772de7090f06b
Taro Sato <okomestudio@gmail.com>
2024-10-28 16:43:21 -0700

    Add auth-source.el

---
+++ lisp/auth-source.el
@@ -0,0 +1,23 @@
+;;; auth-source.el --- auth-source  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; The auth-source configuration.
+;;
+;;; Code:
+
+(use-package auth-source
+  :straight nil
+  :custom ((auth-sources `(,(no-littering-expand-var-file-name
+                             "auth-source/authinfo.gpg")))
+           (authinfo-hidden (format "\\(%s\\)"
+                                    (mapconcat 'identity
+                                               '("api_key"
+                                                 "api_token"
+                                                 "authkey"
+                                                 "cookie"
+                                                 "secret"
+                                                 "passphrase"
+                                                 "password")
+                                               "\\|")))))
+
+;;; auth-source.el ends here
