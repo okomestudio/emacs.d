0bd5028aa5fe448126d317d63b626e11e3a2deeb
Taro Sato <okomestudio@gmail.com>
2024-10-30 09:16:44 -0700

    Add writing-en init file

---
+++ lisp/writing-en.el
@@ -0,0 +1,11 @@
+;;; writing-en.el --- writing-en  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Configure helpers for English writing.
+;;
+;;; Code:
+
+(use-package writegood
+  :hook ((org-mode . writegood-mode)))
+
+;;; writing-en.el ends here
