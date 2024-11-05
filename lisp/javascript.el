7ca5a6047b3c979d0285a39c4f1e6e6244abf6ab
Taro Sato <okomestudio@gmail.com>
2024-11-04 16:38:12 -0800

    Adjust fill-column

--- lisp/javascript.el
+++ lisp/javascript.el
@@ -15,11 +15,15 @@
          ("\\.jsx\\'" . js-ts-mode)  ; equivalent to `js-jsx-mode'
          ("\\.tsx?\\'" . typescript-ts-mode))
   :hook ((js-ts-mode
-          typescript-ts-mode) . lsp-deferred)
+          typescript-ts-mode) . js-ok--init)
   :config
   ;; Setting the following custom variable in :custom does not get
   ;; respected...
-  (setopt js-indent-level 2))
+  (setopt js-indent-level 2)
+
+  (defun js-ok--init ()
+    (setq-local fill-column 90)
+    (lsp-deferred)))
 
 (use-package prettier-js
   :ensure-system-package (prettier . "npm install -g prettier"))
