cace3f3b71e5f8acc24664590c3ad2775d6397c4
Taro Sato <okomestudio@gmail.com>
2024-11-04 16:16:02 -0800

    Use prettier for CSS formatting

--- lisp/css.el
+++ lisp/css.el
@@ -1,13 +1,16 @@
 ;;; css.el --- CSS  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; CSS mode configuration.
+;; The CSS mode configuration.
 ;;
 ;;; Code:
 
 (use-package css-ts-mode
   :straight nil
-  :hook (css-ts-mode . lsp)
-  :custom (css-indent-offset 2))
+  :bind (:map
+         css-ts-mode-map
+         ("C-c b" . (lambda () (interactive) (prettier-js))))
+  :custom (css-indent-offset 2)
+  :hook (css-ts-mode . lsp))
 
 ;;; css.el ends here
