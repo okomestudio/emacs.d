cd230073c9df39c1e835540dea53f2a07102375f
Taro Sato <okomestudio@gmail.com>
2024-12-25 18:14:21 -0800

    Archive init-webkit

---
+++ lisp/archive/init-webkit.el
@@ -0,0 +1,21 @@
+;;; init-webkit.el --- webkit Setup  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Set up WebKit, aka a fully fledged browser inside Emacs.
+;;
+;;; Code:
+
+;; NOTE: xwidget-webkit doesn't work since webkit2gtk version 2.42.1.
+;;
+;; See mail.gnu.org/archive/html/bug-gnu-emacs/2023-09/msg01905.html
+
+(use-package webkit
+  :straight (webkit :type git
+                    :host github
+                    :repo "akirakyle/emacs-webkit"
+                    :branch "main"
+                    :files (:defaults "*.js" "*.css" "*.so")
+                    :pre-build ("make")))
+
+(provide 'init-webkit)
+;;; init-webkit.el ends here
