0007d30a513bbd099846beaf8448ce0d2e00b696
Taro Sato <okomestudio@gmail.com>
2024-12-25 19:14:40 -0800

    Rename to maj-prog-mode

---
+++ lisp/maj-prog-mode.el
@@ -0,0 +1,12 @@
+;;; maj-prog-mode.el --- Prog Major Mode  -*- lexical-binding: t -*-
+;;; Commentary:
+;;
+;; Set up the `prog-mode'.
+;;
+;;; Code:
+
+;; (use-package prog-mode
+;;   :straight nil)
+
+(provide 'maj-prog-mode)
+;;; maj-prog-mode.el ends here
