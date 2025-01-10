8882ac50f0d4408003caef29a6695c863ed625b2
Taro Sato <okomestudio@gmail.com>
2025-01-10 13:03:56 -0800

    Add docstring

--- bin/emacs-setup.bash
+++ bin/emacs-setup.bash
@@ -1,5 +1,9 @@
-# Source this file in ~/.bashrc
+# Emacs-specific configuration for Bash.
+#
+# Source this file in ~/.bashrc to run any setup operations
+# specifically for Emacs.
 
+# vterm
 if [[ "$INSIDE_EMACS" = 'vterm' ]]; then
   SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 	source "$SCRIPT_DIR/vterm-bash.sh"
