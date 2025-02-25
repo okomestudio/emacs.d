54a6e900a1bf793a2f9d82b0ae7cb39289fc59be
Taro Sato <okomestudio@gmail.com>
2025-02-24 21:00:23 -0800

    Move utility functions to plugin

--- lisp/subsys-org-roam.el
+++ lisp/subsys-org-roam.el
@@ -22,10 +22,10 @@
          ("C-c n i" . org-roam-node-insert)
          ("C-c n l" . org-roam-buffer-toggle)
          :map org-mode-map
-         ("C-c C-q" . org-roam-ok-tag-add-or-remove)
-         ("C-c a" . org-roam-ok-alias-add-or-remove)
-         ("C-c r f" . org-roam-ok-ref-find)
-         ("C-M-i" . completion-at-point))
+         ("C-M-i" . completion-at-point)
+         ("C-c C-q" . org-roam-ok-node-tag-add-or-remove)
+         ("C-c a" . org-roam-ok-node-alias-add-or-remove)
+         ("C-c r f" . org-roam-ok-node-ref-find))
   :bind-keymap ("C-c n d" . org-roam-dailies-map)
   :custom ((org-roam-completion-everywhere nil)
            (org-roam-database-connector 'sqlite-builtin)
@@ -46,49 +46,7 @@
                                                    " ${orp-timestamp:10}")))
   :config
   (setopt find-file-visit-truename t)  ; see "5.3 Setting up Org-roam"
-
-  (defun org-roam-ok-tag-add-or-remove (&optional arg)
-    "Add an Org filetags or heading tag.
-When called with the `\\[universal-argument]'
-`\\[universal-argument]' `\\[universal-argument]' prefix
-argument, the tag removal action gets triggered."
-    (interactive "P")
-    (call-interactively
-     (if (not (org-before-first-heading-p))
-         ;; For non-filetags tags, use `org-set-tags-command' for both
-         ;; addition and removal:
-         #'org-set-tags-command
-       (pcase arg
-         ('(64) #'org-roam-tag-remove)
-         (_ #'org-roam-tag-add)))))
-
-  (defun org-roam-ok-alias-add-or-remove (&optional arg)
-    "Add an Org Roam alias.
-When called with the `\\[universal-argument]' prefix argument,
-the alias removal action gets triggered."
-    (interactive "P")
-    (call-interactively (pcase arg
-                          ('(4) #'org-roam-alias-remove)
-                          (_ #'org-roam-alias-add))))
-
-  (defun org-roam-ok-ref-find (&optional arg)
-    "Call the enhanced version of `org-roam-ref-find'.
-If the point is on a link and it is a cite link, then
-`org-roam-ref-find' is given the citekey as the initial string.
-Otherwise, it is the same as the vanilla version of
-`org-roam-ref-find'."
-    (interactive)
-    (let* ((link (org-element-lineage (org-element-context) '(link) t))
-           (type (org-element-property :type link))
-           (path (org-element-property :path link))
-           (ref (cond
-                 ((string= type "cite")
-                  (string-trim-left path "&"))
-                 (t ""))))
-      (org-roam-ref-find ref)))
-
   (require 'org-roam-dailies)
-
   (org-roam-db-sync)
   (org-roam-db-autosync-mode 1)
   (org-roam-gt-mode 1)
@@ -103,9 +61,11 @@ Otherwise, it is the same as the vanilla version of
 
 (use-package org-roam-gt
   ;; See github.com/org-roam/org-roam/issues/2474
-  :straight (:host github :repo "dmgerman/org-roam-gt"
-                   :fork (:host github
-                                :repo "okomestudio/org-roam-gt" :branch "ok"))
+  :straight (org-roam-gt :host github
+                         :repo "dmgerman/org-roam-gt"
+                         :fork (:host github
+                                      :repo "okomestudio/org-roam-gt"
+                                      :branch "ok"))
   :demand t)
 
 (use-package org-roam-ok
