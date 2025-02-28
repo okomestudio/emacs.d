192ef73b5b9b360f7af0500054ada5d25ffe85b1
Taro Sato <okomestudio@gmail.com>
2025-02-28 10:51:16 -0800

    Turn off indent-tabs and aggressive-indent modes when active

--- lisp/maj-prog-mode.el
+++ lisp/maj-prog-mode.el
@@ -15,6 +15,15 @@
          ("C-x n d" . narrow-to-defun)
          ("C-x n w" . widen)))
 
+(use-package whitespace
+  :straight nil
+  :hook (whitespace-mode . whitespace-mode-ok--hook)
+  :config
+  (defun whitespace-mode-ok--hook ()
+    (when (and (boundp 'aggressive-indent-mode) aggressive-indent-mode)
+      (aggressive-indent-mode -1))
+    (indent-tabs-mode 1)))
+
 ;;; FORMATTING
 (use-package prettier-js
   :commands (prettier-js)
