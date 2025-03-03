33d66a5b6455ebdc09642fb9d6a9d8396e8ca2b8
Taro Sato <okomestudio@gmail.com>
2025-03-02 16:19:10 -0800

    Use mozc-isearch to enable mozc in isearch

--- lisp/subsys-ime.el
+++ lisp/subsys-ime.el
@@ -17,6 +17,18 @@
   ;; Setting this to non-nil uses the IME on OS:
   (setq pgtk-use-im-context-on-new-connection nil))
 
+(use-package mozc-isearch
+  :straight (mozc-isearch :host github :repo "iRi-E/mozc-el-extensions")
+  :hook (isearch-mode . mozc-isearch-ok--fix-ace-isearch)
+  :init (require 'mozc-isearch)
+  :config
+  (defun mozc-isearch-ok--fix-ace-isearch (&rest _)
+    "Disable `ace-isearch-mode' when `mozc' is active."
+    ;; NOTE(2025-03-02): Mozc doesn't play nice with ace-isearch.
+    (if (and (boundp 'mozc-mode) mozc-mode)
+        (ace-isearch-mode -1)
+      (ace-isearch-mode 1))))
+
 (use-package mozc-posframe
   :straight (mozc-posframe :host github :repo "derui/mozc-posframe")
   :after mozc
