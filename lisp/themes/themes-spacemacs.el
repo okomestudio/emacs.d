0bd69b9898ad0d85205cda8cc94dd55102250570
Taro Sato <okomestudio@gmail.com>
2025-03-03 22:44:22 -0800

    Use before-enable-theme-functions hook for finer control

--- lisp/themes/themes-spacemacs.el
+++ lisp/themes/themes-spacemacs.el
@@ -5,22 +5,23 @@
 (require 'ok)
 
 (use-package spacemacs-theme
-  :hook (enable-theme-functions . spacemacs-theme-ok--prepare)
+  :hook (before-enable-theme-functions . spacemacs-theme-ok--prepare)
   :config
   (defun spacemacs-theme-ok--prepare (theme)
-    (when (or (eq theme 'spacemacs-dark) (eq theme 'spacemacs-light))
-      (let ((mode (frame-parameter nil 'background-mode)))
-        (if (string= mode "light")
-            (setq spacemacs-theme-custom-colors
-                  `((base . "#322938") ;; #655370 for light, true-color
-
-                    ;; Make some colors slightly darker
-                    (head3 . ,(ok-face-color-scale "#67b11d" 0.80))
-                    (head4 . ,(ok-face-color-scale "#b1951d" 0.80))
-                    (cyan . ,(ok-face-color-scale "#21b8c7" 0.95))))))
-
+    (when (member theme '(spacemacs-dark spacemacs-light))
       (setopt window-divider-default-bottom-width 1
-              window-divider-default-right-width 1))))
+              window-divider-default-right-width 1)
+      (pcase theme
+        ('spacemacs-dark
+         (setq spacemacs-theme-custom-colors nil))
+        ('spacemacs-light
+         (setq spacemacs-theme-custom-colors
+               `((base . "#322938") ;; #655370 for light, true-color
+
+                 ;; Make some colors slightly darker
+                 (head3 . ,(ok-face-color-scale "#67b11d" 0.80))
+                 (head4 . ,(ok-face-color-scale "#b1951d" 0.80))
+                 (cyan . ,(ok-face-color-scale "#21b8c7" 0.95)))))))))
 
 (load-theme 'spacemacs-dark t t)
 (load-theme 'spacemacs-light t t)
