7ffd79994792900fb1ac599b94d3d12dbb561b58
Taro Sato <okomestudio@gmail.com>
2025-03-12 23:32:31 -0700

    Use popup to avoid posframe issue with Cairo

--- lisp/subsys-ime.el
+++ lisp/subsys-ime.el
@@ -13,19 +13,20 @@
                   ;; divergence of submodules refs to cause dirty
                   ;; repo.
                   :commit "14afac9728dd3f04e3d73633f4fa925d38589368")
-  :custom `((default-input-method "japanese-mozc")
+  :custom ((default-input-method "japanese-mozc")
 
-            ;; NOTE(2025-03-03): `posframe' would be best, but it
-            ;; frequently crashed with cairo. `overlay' runs sluggish.
-            ;; `echo-area' is sufficient and stable.
-            (mozc-candidate-style ,(if (display-graphic-p)
-                                       'posframe
-                                     'echo-area))
-            (mozc-leim-title "🇯🇵"))
+           ;; NOTE(2025-03-12): `posframe' would be best, but it
+           ;; frequently crashed with Cairo in a hard-to-debug way.
+           ;; `overlay' runs sluggish. `echo-area' is sufficient and
+           ;; stable, but far from the point of input. `popup' seems
+           ;; to be the best compromise.
+           (mozc-candidate-style (if (display-graphic-p) 'popup 'echo-area))
+
+           (mozc-leim-title "🇯🇵"))
   :commands (toggle-input-method))
 
 (use-package mozc
-  :if (and (eq system-type 'gnu/linux) (memq window-system '(pgtk)))
+  ;; :if (and (eq system-type 'gnu/linux) (memq window-system '(pgtk)))
   :init
   ;; Setting this to non-nil uses the IME on OS:
   (setq pgtk-use-im-context-on-new-connection nil))
@@ -42,13 +43,18 @@
         (ace-isearch-mode -1)
       (ace-isearch-mode 1))))
 
+;;; Posframe
+
 (use-package mozc-cand-posframe
+  ;; For `posframe', this package may be the simplest option.
+  :disabled
   :straight (mozc-cand-posframe :host github :repo "akirak/mozc-posframe")
   :if (display-graphic-p)
   :after mozc
   :init (require 'mozc-cand-posframe))
 
 (use-package mozc-posframe
+  ;; This might work, but not out of box.
   :disabled
   :straight (mozc-posframe :host github :repo "derui/mozc-posframe")
   :after mozc
@@ -56,33 +62,54 @@
          (enable-theme-functions . mozc-posframe-ok--theme))
   :config
   (defun mozc-posframe-ok--theme (theme)
-    (when (and (boundp 'corfu-mode) corfu-mode)
-      (let* ((face-base 'corfu-default)
-             (face-current 'corfu-current)
-             (face-annotations 'corfu-annotations)
-             (foreground (face-attribute face-base :foreground))
-             (background (face-attribute face-base :background)))
-        (set-face-attribute 'mozc-cand-overlay-even-face nil
+    (require 'corfu)
+    (let* ((face-base 'corfu-default)
+           (face-current 'corfu-current)
+           (face-annotations 'corfu-annotations)
+           (foreground (face-attribute face-base :foreground))
+           (background (face-attribute face-base :background)))
+      (dolist (face '(mozc-cand-overlay-even-face
+                      mozc-cand-overlay-odd-face
+                      mozc-cand-overlay-description-face
+                      mozc-cand-overlay-footer-face))
+        (set-face-attribute face nil
                             :foreground foreground
                             :background background
-                            :inherit face-base)
-        (set-face-attribute 'mozc-cand-overlay-odd-face nil
+                            :inherit face-base))
+      (set-face-attribute 'mozc-cand-overlay-focused-face nil
+                          :foreground (face-attribute face-current :foreground)
+                          :background (face-attribute face-current :background)
+                          :inherit face-current))))
+
+;;; Popup
+
+(use-package mozc-popup
+  :straight (mozc-popup :host github
+                        :repo "d5884/mozc-popup"
+                        :fork (:branch "reduce-refresh"))
+  :after mozc
+  :hook ((on-first-input . (lambda () (require 'mozc-popup)))
+         (enable-theme-functions . mozc-popup-ok--theme))
+  :config
+  (defun mozc-popup-ok--theme (theme)
+    (require 'corfu)
+    (let* ((face-base 'corfu-default)
+           (face-current 'corfu-current)
+           (face-annotations 'corfu-annotations)
+           (foreground (face-attribute face-base :foreground))
+           (background (face-attribute face-base :background)))
+      (dolist (face '(mozc-cand-overlay-even-face
+                      mozc-cand-overlay-odd-face
+                      mozc-cand-overlay-description-face
+                      mozc-cand-overlay-footer-face))
+        (set-face-attribute face nil
                             :foreground foreground
                             :background background
-                            :inherit face-base)
-        (set-face-attribute 'mozc-cand-overlay-footer-face nil
-                            :foreground foreground
-                            :background background
-                            :inherit face-base)
-        (set-face-attribute 'mozc-cand-overlay-focused-face nil
-                            :foreground (face-attribute face-current :foreground)
-                            :background (face-attribute face-current :background)
-                            :inherit face-current)
-        (with-eval-after-load 'mozc-posframe
-          (set-face-attribute 'mozc-cand-overlay-description-face nil
-                              :foreground (face-attribute face-annotations :foreground)
-                              :background (face-attribute face-annotations :background)
-                              :inherit face-annotations))))))
+                            :inherit face-base))
+      (set-face-attribute 'mozc-cand-overlay-focused-face nil
+                          :foreground (face-attribute face-current :foreground)
+                          :background (face-attribute face-current :background)
+                          :inherit face-current))))
 
 (provide 'subsys-ime)
 ;;; subsys-ime.el ends here
