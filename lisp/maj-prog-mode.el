5f1878448851ac9caa00a1e98b461ec4ae71a0b0
Taro Sato <okomestudio@gmail.com>
2025-03-21 13:36:45 -0700

    Add hideshaw

--- lisp/maj-prog-mode.el
+++ lisp/maj-prog-mode.el
@@ -9,6 +9,44 @@
   :straight nil)
 
 ;;; EDITING
+
+(use-package hideshow
+  :straight nil
+  :bind ( :map hs-minor-mode-map
+          ("<backtab>" . hs-ok-toggle-hiding-all)
+          ("C-c @ C-a" . hs-show-all)
+          ("C-c @ C-t" . hs-hide-all))
+  :hook (prog-mode . hs-minor-mode)
+  :config
+  (defun hs-ok-toggle-hiding-all ()
+    "Toggle hiding of all program blocks."
+    (interactive)
+    (hs-life-goes-on
+     (save-excursion
+       (forward-sexp)
+       (if (hs-already-hidden-p) (hs-show-all) (hs-hide-all)))))
+
+  (defun hs-ok-toggle-hiding--ad (fun &rest _)
+    "Advise `indent-for-tab-command' to add hiding toggle behavior."
+    ;; (declare (debug t))
+    (if (and (boundp 'hs-minor-mode) (null hs-minor-mode))
+        (apply fun _)
+      (pcase (hs-already-hidden-p)
+        (`nil (pcase (eq (point)
+                         (save-excursion
+                           (unless (eq (point) (end-of-line)) (forward-char))
+                           (beginning-of-defun)
+                           (point)))
+                (`t (hs-hide-block)
+                    (unless (eq (point) (end-of-line)) (forward-char))
+                    (beginning-of-defun))
+                (`nil (apply fun _))))
+        (t (save-excursion (hs-show-block))
+           (unless (eq (point) (end-of-line)) (forward-char))
+           (beginning-of-defun)))))
+
+  (advice-add #'indent-for-tab-command :around #'hs-ok-toggle-hiding--ad))
+
 (use-package lisp
   :straight nil
   :bind (;; The following work in many prog modes, but defined in lisp.el:
