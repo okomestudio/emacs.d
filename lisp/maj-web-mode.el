febcbf71329b7b08542a9311af0a9d772d9cebca
Taro Sato <okomestudio@gmail.com>
2025-03-22 20:29:50 -0700

    Add djhtml formatter

--- lisp/maj-web-mode.el
+++ lisp/maj-web-mode.el
@@ -15,6 +15,25 @@
            (web-mode-script-padding 2)
            (web-mode-style-padding 2)))
 
+(use-package reformatter
+  ;; Django template HTML formatter.
+  ;;
+  ;; To active per-project basis, have the following line in
+  ;; .dir-locals.el:
+  ;;
+  ;;   (define-key web-mode-map (kbd "C-c b") #'djhtml-format-buffer)
+  ;;
+  :ensure-system-package (djhtml . "pip install djhtml")
+  :config
+  (reformatter-define djhtml-format
+    ;; Provides `djhtml-format-buffer' and `djhtml-format-on-save-mode'.
+    :program (locate-user-emacs-file "bin/djhtml")
+    :args '("-")
+    :stdin t
+    :stdout t
+    :lighter " DJHTML"
+    :group 'djhtml-format))
+
 (use-package lorem-ipsum)
 
 (provide 'maj-web-mode)
