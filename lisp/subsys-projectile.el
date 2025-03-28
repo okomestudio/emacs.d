150eed18df4f62a4a54879a2582d62280e54278d
Taro Sato <okomestudio@gmail.com>
2025-03-28 03:50:11 -0700

    Use projectile to set safe-local-variable-directories

--- lisp/subsys-projectile.el
+++ lisp/subsys-projectile.el
@@ -5,8 +5,10 @@
 ;;
 ;;; Code:
 
+(require 'dash)
+
 (use-package projectile
-  :after project                        ; ensures the overriding of `C-x p'
+  :after project                   ; ensures the `C-x p' override
   :bind-keymap ("C-x p" . projectile-command-map)
   :custom ((projectile-auto-discover nil)
            (projectile-enable-caching nil)
@@ -24,10 +26,28 @@
            (projectile-project-search-path
             `(,(ok-file-expand-user-emacs-file "straight" "repos/")
               (,(expand-file-name "github.com/" (getenv "HOME")) . 2))))
+  :hook (on-first-file . projectile-ok-set-safe-local-variable-directories)
   :ensure-system-package ((ag . "sudo apt install -y silversearcher-ag")
                           (fdfind . "sudo apt install -y fd-find"))
   :config
   (projectile-mode +1)
+
+  ;; Add directories to `safe-local-variable-directories' using
+  ;; projectile, so that we don't have to add variables separately to
+  ;; `safe-local-variable', e.g.,
+  ;;
+  ;;   (put 'my-symbol 'safe-local-variable #'integerp)
+  ;;   (ok-safe-local-variable-add my-symbol integerp)
+  ;;
+  (defun projectile-ok-set-safe-local-variable-directories ()
+    (interactive)
+    (let* ((projectile-current-project-on-switch 'keep)
+           (ds (projectile-relevant-known-projects))
+           (symlink-tree (getenv "SYMLINKTO_TREE")))
+      (when symlink-tree
+        (setq ds (append ds (--map (string-replace "~" symlink-tree it) ds))))
+      (setopt safe-local-variable-directories (delete-dups ds))))
+
   (pcase-dolist
       (`(,key ,doc) '(("C-x p" "projectile")
                       ("C-x p 4" "projectile-other-window")
