654276cb668b82b5bc1dc94bcd65e950d549237f
Taro Sato <okomestudio@gmail.com>
2025-05-21 15:24:40 -0700

    Refactor the use of enable-theme-functions hook

--- lisp/themes-modeline.el
+++ lisp/themes-modeline.el
@@ -15,19 +15,31 @@
            (doom-modeline-vcs-max-length 12)
            (mode-line-percent-position nil)
            (doom-modeline-checker-simple-format t))
-  :hook (enable-theme-functions . (lambda (_) (doom-modeline-mode)))
+  :hook (enable-theme-functions . doom-modeline-ok--enable)
   :config
   (doom-modeline-def-modeline 'lsp-full
-    '(bar workspace-name window-number modals matches follow buffer-info remote-host buffer-position word-count parrot selection-info)
-    '(compilation objed-state persp-name battery grip irc mu4e gnus github debug repl input-method minor-modes indent-info buffer-encoding major-mode vcs lsp misc-info time))
+    '( bar workspace-name window-number modals matches
+       follow buffer-info remote-host buffer-position word-count
+       parrot selection-info )
+    '( compilation objed-state persp-name battery grip
+       irc mu4e gnus github debug
+       repl input-method minor-modes indent-info buffer-encoding
+       major-mode vcs lsp misc-info time ))
 
   (dolist (mode '(python-ts-mode python-sql-ts-mode))
-    (add-to-list 'doom-modeline-mode-alist `(,mode . lsp-full))))
+    (add-to-list 'doom-modeline-mode-alist `(,mode . lsp-full)))
+
+  (cl-defun doom-modeline-ok--enable (&rest _)
+    (which-function-mode 1)
+    (doom-modeline-mode 1)))
 
 (use-package minions
   ;; A minor-mode menu for the mode line.
   :custom (minions-direct '(projectile-mode))
-  :hook (enable-theme-functions . (lambda (_) (minions-mode))))
+  :hook (enable-theme-functions . minions-ok--enable)
+  :config
+  (cl-defun minions-ok--enable (&rest _)
+    (minions-mode 1)))
 
 (provide 'themes-modeline)
 ;;; themes-modeline.el ends here
