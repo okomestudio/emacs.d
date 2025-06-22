92fab771ccfc73fbcf6048514deffc67076c403e
Taro Sato <okomestudio@gmail.com>
2025-06-22 00:42:25 -0700

    Refactor website reader hook

--- lisp/subsys-readers.el
+++ lisp/subsys-readers.el
@@ -1,24 +1,11 @@
-;;; subsys-readers.el --- hnreader  -*- lexical-binding: t -*-
+;;; subsys-readers.el --- Website Readers  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Set up website reader applications.
+;; Configure website reader applications.
 ;;
 ;;; Code:
 
-(defun ok--apply-face-remappings ()
-  "Apply buffer-local face remappings for `hnreader'."
-  (when (member (buffer-name) '("*HN*" "*HNComments*"))
-    (let ((height (face-attribute 'default :height)))
-      (face-remap-add-relative 'org-level-1 :height height)
-      (face-remap-add-relative 'org-level-2 :height height)
-      (face-remap-add-relative 'org-level-3 :height height)
-      (face-remap-add-relative 'org-level-4 :height height)
-      (face-remap-add-relative 'org-level-5 :height height)
-      (face-remap-add-relative 'org-level-6 :height height)
-      (face-remap-add-relative 'org-level-7 :height height)
-      (face-remap-add-relative 'org-level-8 :height height))))
-
-(add-hook 'org-mode-hook #'ok--apply-face-remappings 98)
+(defvar ok-reader-buffers nil)
 
 (use-package hnreader
   ;; Browser Hacker News in `org-mode'.
@@ -26,12 +13,28 @@
   ;; NOTE(2025-06-19): https://github.com/agzam/consult-hn might be of
   ;; interest.
   :bind ( :map reader-app-prefix-map
-          ("h" . hnreader-news) ))
+          ("h" . hnreader-news) )
+  :config
+  (dolist (buffer '("*HN*" "*HNComments*"))
+    (add-to-list 'ok-reader-buffers buffer)))
 
 (use-package reddigg
   ;; Browse Reddit in `org-mode'.
   :bind ( :map reader-app-prefix-map
-          ("r" . reddigg-view-sub) ))
+          ("r" . reddigg-view-sub) )
+  :config
+  (dolist (buffer '("*reddigg*" "reddigg-comments*"))
+    (add-to-list 'ok-reader-buffers buffer)))
+
+(defun ok-reader-buffers--init ()
+  "Apply buffer-local face remappings for `hnreader'."
+  (when (member (buffer-name) ok-reader-buffers)
+    (let ((height (face-attribute 'default :height)))
+      (dolist (face '( org-level-1 org-level-2 org-level-3 org-level-4
+                       org-level-5 org-level-6 org-level-7 org-level-8 ))
+        (face-remap-add-relative face :height height :weight 'normal)))))
+
+(add-hook 'org-mode-hook #'ok-reader-buffers--init 98)
 
 (provide 'subsys-readers)
 ;;; subsys-readers.el ends here
