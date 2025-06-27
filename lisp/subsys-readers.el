5891023200e0c2ae15e52b401cd2eb3c35aef2cb
Taro Sato <okomestudio@gmail.com>
2025-06-26 18:06:36 -0700

    Add private initializer

--- lisp/subsys-readers.el
+++ lisp/subsys-readers.el
@@ -5,6 +5,8 @@
 ;;
 ;;; Code:
 
+(require 'ok)
+
 (defvar ok-reader-buffers nil)
 
 (use-package hnreader
@@ -24,15 +26,17 @@
           ("r" . reddigg-view-sub) )
   :config
   (dolist (buffer '("*reddigg*" "reddigg-comments*"))
-    (add-to-list 'ok-reader-buffers buffer)))
+    (add-to-list 'ok-reader-buffers buffer))
+
+  (load (ok-file-expand-etc "reddigg/init")))
 
 (defun ok-reader-buffers--init ()
   "Apply buffer-local face remappings for `hnreader'."
   (when (member (buffer-name) ok-reader-buffers)
-    (let ((height (face-attribute 'default :height)))
-      (dolist (face '( org-level-1 org-level-2 org-level-3 org-level-4
-                       org-level-5 org-level-6 org-level-7 org-level-8 ))
-        (face-remap-add-relative face :height height :weight 'normal)))))
+    (let ((faces '( org-level-1 org-level-2 org-level-3 org-level-4
+                    org-level-5 org-level-6 org-level-7 org-level-8 )))
+      (dolist (face faces)
+        (face-remap-add-relative face :weight 'normal)))))
 
 (add-hook 'org-mode-hook #'ok-reader-buffers--init 98)
 
