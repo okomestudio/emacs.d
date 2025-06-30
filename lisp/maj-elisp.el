5b6ff7aa5f694ef4785de1d3a7c127c0781b2791
Taro Sato <okomestudio@gmail.com>
2025-06-30 13:41:20 -0700

    Activate ipretty & some refactoring

--- lisp/maj-elisp.el
+++ lisp/maj-elisp.el
@@ -13,26 +13,32 @@
           ("C-c b" . elisp-mode-ok--format)
           :map lisp-data-mode-map
           ("C-c b" . elisp-mode-ok--format) )
-  :hook ((emacs-lisp-mode
-          lisp-data-mode) . elisp-mode-ok--capf-set)
+  :hook (((emacs-lisp-mode lisp-data-mode) . elisp-mode-ok--style)
+         ((emacs-lisp-mode lisp-data-mode) . elisp-mode-ok--capf-set))
   :config
   (defun elisp-mode-ok--format ()
-    "Format Elisp buffer."
+    "Format the current Emacs Lisp buffer."
     (interactive)
     (save-excursion
-      (indent-region (progn (beginning-of-buffer) (point))
-                     (progn (end-of-buffer) (point)))))
+      (indent-region (point-min) (point-max))))
 
-  (setq elisp-mode-ok--capf (cape-capf-inside-code #'cape-elisp-symbol))
-  (defun elisp-mode-ok--capf (&rest _) (apply elisp-mode-ok--capf _))
+  (defun elisp-mode-ok--style ()
+    "Enforce the coding style in the Emacs Lisp buffer."
+    (setq-local emacs-lisp-docstring-fill-column 80
+                fill-column 80
+                indent-tabs-mode nil
+                tab-width 2))
+
+  (defun elisp-mode-ok--capf (&rest args)
+    (apply (cape-capf-inside-code #'cape-elisp-symbol) args))
+
   (defun elisp-mode-ok--capf-set ()
-    "Set CAPFs for `elisp-mode'."
+    "Set CAPFs for the Emacs Lisp mode."
     ;; See github.com/jwiegley/use-package/issues/1077#issuecomment-2266642373
     (add-hook 'completion-at-point-functions #'elisp-mode-ok--capf -99 t)))
 
 (use-package aggressive-indent
-  :hook ((emacs-lisp-mode
-          lisp-data-mode) . aggressive-indent-mode))
+  :hook ((emacs-lisp-mode lisp-data-mode) . aggressive-indent-mode))
 
 (use-package elp
   ;; Emacs Lisp Profiler.
@@ -51,17 +57,25 @@
 (use-package erefactor)
 
 (use-package eros
-  :hook ((emacs-lisp-mode
-          lisp-data-mode) . eros-mode))
+  :hook ((emacs-lisp-mode lisp-data-mode) . eros-mode))
 
 (use-package flycheck-package
-  :hook ((emacs-lisp-mode
-          lisp-data-mode) . flycheck-package-setup))
+  :hook ((emacs-lisp-mode lisp-data-mode) . flycheck-package-setup))
 
 (use-package ipretty
-  ;; Provides interactive functions to pretty-print the result of an
-  ;; expression
-  :config (ipretty-mode 1))
+  ;; Provides interactive functions to pretty-print the result of an expression.
+  :bind ( :map emacs-lisp-mode-map
+          ("C-x C-S-e" . ipretty-ok-last-sexp)
+          :map lisp-interaction-mode-map
+          ("C-x C-S-e" . ipretty-ok-last-sexp)
+          :map lisp-data-mode-map
+          ("C-x C-S-e" . ipretty-ok-last-sexp) )
+  :config
+  (defun ipretty-ok-last-sexp (arg)
+    (interactive "P")
+    (pcase arg
+      ('(4) (ipretty-last-sexp-other-buffer))
+      (_ (ipretty-last-sexp)))))
 
 (use-package macrostep
   ;; Interactive macro-expander.
@@ -69,16 +83,14 @@
 
 (use-package paredit
   ;; Parentheses editing.
-  :hook ((emacs-lisp-mode
-          lisp-data-mode) . paredit-ok--enable-paredit)
+  :hook ((emacs-lisp-mode lisp-data-mode) . paredit-ok--enable-paredit)
   :config
   (setq paredit-comment-prefix-margin "; ")
 
   (defun paredit-ok--enable-paredit ()
     (setq-local comment-column 30)
 
-    ;; Turn off `electric-pair-mode' to avoid conflict with
-    ;; `paredit-mode':
+    ;; Turn off `electric-pair-mode' to avoid conflict with `paredit-mode':
     (electric-pair-local-mode -1)
     (enable-paredit-mode)))
 
@@ -87,18 +99,15 @@
 (use-package highlight-defined
   :custom (highlight-defined-face-use-itself t)
   :hook ((help-mode . highlight-defined-mode)
-         ((emacs-lisp-mode
-           lisp-data-mode) . highlight-defined-mode)))
+         ((emacs-lisp-mode lisp-data-mode) . highlight-defined-mode)))
 
 (use-package highlight-quoted
-  :hook ((emacs-lisp-mode
-          lisp-data-mode) . highlight-quoted-mode))
+  :hook ((emacs-lisp-mode lisp-data-mode) . highlight-quoted-mode))
 
 (use-package highlight-sexp
-  ;; Highlight the current zone according to its context (sexp,
-  ;; comment, string, etc.).
-  :hook (((emacs-lisp-mode
-           lisp-data-mode) . highlight-sexp-mode)
+  ;; Highlight the current zone according to its context (sexp, comment, string,
+  ;; etc.).
+  :hook (((emacs-lisp-mode lisp-data-mode) . highlight-sexp-mode)
          (enable-theme-functions . highlight-sexp-ok--refresh))
   :config
   (defun highlight-sexp-ok--refresh (theme)
