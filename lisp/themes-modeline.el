43c2821da61898392c1d7a9331a6a33a42443402
Taro Sato <okomestudio@gmail.com>
2025-09-24 21:35:51 -0700

    Consolidate modeline display for major mode

--- lisp/themes-modeline.el
+++ lisp/themes-modeline.el
@@ -1,7 +1,7 @@
 ;;; themes-modeline.el --- Modeline Theme  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Set up modeline theme.
+;; Configure the modeline theme.
 ;;
 ;;; Code:
 
@@ -17,20 +17,72 @@
            (doom-modeline-checker-simple-format t))
   :hook (enable-theme-functions . doom-modeline-ok--enable)
   :config
+  (defsubst doom-modeline--buffer-mode-icon-or-text ()
+    "Show buffer-mode-icon or major-mode segment."
+    (let ((help-echo (string-join `(,(format "Major mode: %s" mode-name)
+                                    "mouse-1: Display major mode menu"
+                                    "mouse-2: Show help for major mode"
+                                    "mouse-3: Toggle minor modes")
+                                  "\n")))
+      (if (doom-modeline--buffer-mode-icon)
+          (propertize (doom-modeline--buffer-mode-icon)
+                      'help-echo help-echo
+                      'local-map mode-line-major-mode-keymap)
+        (propertize (format-mode-line
+                     (or (and (boundp 'delighted-modes)
+                              (cadr (assq major-mode delighted-modes)))
+                         mode-name))
+                    'help-echo help-echo
+                    'face (doom-modeline-face 'doom-modeline-buffer-major-mode)
+                    'mouse-face 'doom-modeline-highlight
+                    'local-map mode-line-major-mode-keymap))))
+
+  (doom-modeline-def-segment env
+    "Environment."
+    (when (and doom-modeline-env-version doom-modeline-env--version)
+      (format "[%s]" doom-modeline-env--version)))
+
+  (doom-modeline-def-segment text-scale
+    "Text scale."
+    (let ((face (doom-modeline-face 'doom-modeline-buffer-major-mode)))
+      (concat
+       (and (boundp 'text-scale-mode-amount)
+            (/= text-scale-mode-amount 0)
+            (propertize
+             (format
+              (if (> text-scale-mode-amount 0) " (%+d)" " (%-d)")
+              text-scale-mode-amount)
+             'face face)))))
+
+  (doom-modeline-def-segment buffer-major-mode
+    "Buffer major mode."
+    (let ((face (doom-modeline-face 'doom-modeline-buffer-major-mode)))
+      (concat
+       ;; (doom-modeline-spc)
+       (doom-modeline--buffer-mode-icon-or-text))))
+
+  (doom-modeline-def-segment buffer-state
+    "Buffer state, e.g., file name and state."
+    (concat
+     ;; (doom-modeline-spc)
+     (doom-modeline--buffer-state-icon)
+     (doom-modeline--buffer-name)))
+
   (doom-modeline-def-modeline 'lsp-full
-    '( bar workspace-name window-number modals matches
-       follow buffer-info remote-host buffer-position word-count
+    '( bar workspace-name window-number modals matches follow
+       buffer-major-mode buffer-state
+       text-scale remote-host buffer-position word-count
        parrot selection-info )
     '( compilation objed-state persp-name battery grip
        irc mu4e gnus github debug
-       repl input-method minor-modes indent-info buffer-encoding
-       major-mode vcs lsp misc-info time ))
+       repl input-method misc-info minor-modes indent-info buffer-encoding
+       vcs env lsp time ))
 
-  (dolist (mode '(python-ts-mode python-sql-ts-mode))
+  (dolist (mode '(prog-mode text-mode))
     (add-to-list 'doom-modeline-mode-alist `(,mode . lsp-full)))
 
   (cl-defun doom-modeline-ok--enable (&rest _)
-    (which-function-mode 1)
+    (which-function-mode -1)   ; Enable to show function/section name in modeline.
     (doom-modeline-mode 1)))
 
 (use-package minions
