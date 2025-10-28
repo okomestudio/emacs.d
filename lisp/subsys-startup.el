d4c5682b52d8958b18395abcad0c3fc835e18bed
Taro Sato <okomestudio@gmail.com>
2025-10-28 15:25:27 -0700

    Enable imperial units

--- lisp/subsys-startup.el
+++ lisp/subsys-startup.el
@@ -155,7 +155,7 @@
 
 ;;; Registration
 
-(use-package register
+(use-package emacs
   ;; Save various states for later use.
   :bind ( ("C-x r j" . jump-to-register)
           ("C-x r w" . window-configuration-to-register) )
@@ -168,15 +168,20 @@
 (use-package boem-weather
   ;; A weather forecast client for the Open Metro API.
   ;;
-  ;; Type `M-x boam-weather RET' to open the child frame.
-  ;;
-  ;; TODO(2025-10-27): Display temperature in Fahrenheit.
+  ;; Type `M-x boem-weather RET' to open the child frame.
   ;;
   :custom (;; San Francisco, CA
-           (boem-weather-latitude 37.779379)
-           (boem-weather-longitude 122.418433)
+           (boem-weather-latitude 37.7793)
+           (boem-weather-longitude -122.4193)
            (boem-weather-time-zone "America/Los_Angeles") )
-  :commands boem-weather)
+  :commands (boem-weather)
+  :config
+  (defun ok-boem-weather--url (fun &rest _rest)
+    (let ((url (apply fun _rest))
+          (params '(("temperature_unit" "fahrenheit")
+                    ("wind_speed_unit" "mph"))))
+      (concat url "&" (url-build-query-string params))))
+  (advice-add #'boem-weather--url :around #'ok-boem-weather--url))
 
 ;;; Misc.
 
