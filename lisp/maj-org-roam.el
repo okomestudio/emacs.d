479e56b7e6f68158e57441758e69f2d38806d2f4
Taro Sato <okomestudio@gmail.com>
2026-02-04 16:32:00 -0800

    Clean up org-id-ext

--- lisp/maj-org-roam.el
+++ lisp/maj-org-roam.el
@@ -208,20 +208,7 @@
 
 ;;; Misc.
 
-(use-package org-id-ext
-  :config
-  (defun org-id-ext-new-from-ctime ()
-    "Generate new ts-b62 ID from CTIME added by org-roam-timestamps."
-    (interactive)
-    (let* ((time (org-roam-timestamps-encode (org-entry-get nil "CTIME" t)))
-           (jitter-us (* (random 1000) 1000)) ; millisec jitter in microsec
-           (time (time-add time (list 0 0 jitter-us 0)))
-           (ts-ms (floor (* 1000 (float-time time))))
-           (unique (org-id-ext-int-to-base62 ts-ms))
-           (prefix (when org-id-prefix (format "%s:" org-id-prefix)))
-           (id (concat prefix unique)))
-      (kill-new id)
-      id)))
+(use-package org-id-ext)
 
 (use-package org-roam-fztl
   ;; Org Roam plugin for folgezettel IDs.
