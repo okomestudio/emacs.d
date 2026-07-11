f9e0cfb1904b96ae030222e17c332074f6539c7d
Taro Sato <okomestudio@gmail.com>
2026-07-11 16:15:28 -0700

    Refactor treesit configuration

--- lisp/subsys-treesit.el
+++ lisp/subsys-treesit.el
@@ -1,28 +1,15 @@
-;;; subsys-treesit.el --- Tree-sitter Subsystem  -*- lexical-binding: t -*-
+;;; subsys-treesit.el --- Tree-sitter  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Set up Tree-sitter subsystem.
+;; Configure the Tree-sitter subsystem.
 ;;
 ;;; Code:
 
 (use-package treesit
-  :custom (treesit-font-lock-level 4) ; usually 3 is enough
   :demand t
-  :ensure-system-package (tree-sitter . "npm install -g tree-sitter-cli")
-  :config
-  ;; NOTE(2025-12-15): To avoid 'Warning (treesit): Cannot activate tree-sitter,
-  ;; because language grammar for javascript is unavailable (version-mismatch):
-  ;; 15'. Manually run `treesit-install-language-grammar' for JavaScript.
-  (setq treesit-language-source-alist
-        '((javascript "https://github.com/tree-sitter/tree-sitter-javascript"
-                      "v0.23.1" "src"))))
-
-(use-package treesit-auto
-  :demand t
-  :custom (treesit-auto-install 'prompt)
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-  (global-treesit-auto-mode))
+  :custom ((treesit-auto-install-grammar 'always)
+           (treesit-enabled-modes t)
+           (treesit-font-lock-level 4))) ; usually 3 is enough
 
 (use-package treesit-fold)
 
