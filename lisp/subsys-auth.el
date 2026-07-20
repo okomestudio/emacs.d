7fbd7af53e92c7637200de952281017ed046ff55
Taro Sato <okomestudio@gmail.com>
2026-07-19 23:03:53 -0700

    Refactor auth

--- lisp/subsys-auth.el
+++ lisp/subsys-auth.el
@@ -1,22 +1,35 @@
-;;; subsys-auth.el --- Auth Subsystem  -*- lexical-binding: t -*-
+;;; subsys-auth.el --- Auth  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; Set up the auth subsystem.
+;; Configure the auth subsystem.
 ;;
 ;;; Code:
 
 (use-package auth-source
-  :custom ((auth-sources `(,(fs-emacs-etc "auth-source/.authinfo.gpg")))
-           (authinfo-hidden (format "\\(%s\\)"
-                                    (mapconcat 'identity
-                                               '("api_key"
-                                                 "api_token"
-                                                 "authkey"
-                                                 "cookie"
-                                                 "secret"
-                                                 "passphrase"
-                                                 "password")
-                                               "\\|")))))
+  :custom ((auth-sources (list (fs-emacs-etc "auth-source/.authinfo.gpg"))))
+  :config
+  (setopt authinfo-hidden
+          (regexp-opt '("api_key" "api_token" "authkey"
+                        "cookie"
+                        "key"
+                        "secret"
+                        "passphrase" "password")))
+
+  ;; (font-lock-add-keywords 'authinfo-mode ... doesn't work due to a timing
+  ;; (?) issue. That's the reason for using the mode init hook below.
+
+  (defun authinfo-mode--init ()
+    (font-lock-add-keywords
+     nil
+     `((,(regexp-opt '("api_key" "api_token"
+                       "cookie"
+                       "key" "keyname"
+                       "userid" "username")
+                     'symbols)
+        (1 font-lock-comment-delimiter-face keep))))
+    (font-lock-flush))
+
+  :hook (authinfo-mode . authinfo-mode--init))
 
 (provide 'subsys-auth)
 ;;; subsys-auth.el ends here
