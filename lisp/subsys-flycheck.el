2fae2a74ed23859d90913fb9a11fad35b23e4daa
Taro Sato <okomestudio@gmail.com>
2026-08-06 15:30:14 -0700

    Configure flycheck-languagetool

--- lisp/subsys-flycheck.el
+++ lisp/subsys-flycheck.el
@@ -8,11 +8,11 @@
 (use-package flycheck
   :custom ((flycheck-python-mypy-executable (fs-emacs-bin "mypy"))
            (flycheck-rst-executable (fs-emacs-bin "rst2pseudoxml")))
-  :hook (((emacs-lisp-mode lisp-data-mode) . flycheck-mode))
   :config
-  (which-key-add-key-based-replacements "C-c !" "flycheck-mode-map"))
+  (which-key-add-key-based-replacements "C-c !" "flycheck-mode-map")
+  :hook (((emacs-lisp-mode lisp-data-mode) . flycheck-mode)))
 
-;;; Textlint with Flycheck
+;;; Textlint Integration
 
 (use-package flycheck
   :custom ((flycheck-textlint-config "default"))
@@ -51,9 +51,52 @@ The function returns nil, if the file does not exists."
           (when (file-exists-p filename)
             filename))))))
 
-  (add-to-list 'flycheck-locate-config-file-functions
-               #'flycheck-locate-config-file-textlint)
+  (add-to-list 'flycheck-locate-config-file-functions #'flycheck-locate-config-file-textlint)
   (add-to-list 'flycheck-textlint-plugin-alist '(org-mode . "org")))
+
+;;; LanguageTool
+
+(use-package flycheck-languagetool
+  :custom (flycheck-languagetool-language "en-US")
+  :init
+  (defun flycheck-languagetool--on-init ()
+    (require 'ok-network)
+    (ok-network-port-open-async
+     "localhost" 8081
+     (lambda (alive event)
+       (if alive
+           (progn
+             (setq flycheck-languagetool-url "http://localhost:8081")
+             (flycheck-languagetool-setup))
+         (message "LanguageTool service unreachable (%s)" event)
+         (let ((buf-name "*LanguageTool Launcher"))
+           (message "Trying to start LanguageTool service...")
+           (add-hook 'comint-mode-hook
+                     (lambda ()
+                       (when (string= (buffer-name) buf-name)
+                         (remove-hook 'comint-output-filter-functions
+                                      'ansi-color-process-output t))))
+           (async-shell-command (fs-emacs-bin "docker-languagetool")
+                                buf-name))))))
+  :hook (text-mode . flycheck-languagetool--on-init))
+
+;;; write good
+
+(use-package flycheck
+  ;; write good - Naive linter for English prose
+  :disabled
+  :ensure-system-package (write-good . "npm install -g write-good")
+  :config
+  (flycheck-define-checker write-good
+    "The write-good prose checker."
+    :command ("write-good" "--no-thereIs" "--parse" source-inplace)
+    :standard-input nil
+    :error-patterns ((warning
+                      line-start
+                      (file-name) ":" line ":" column ":" (message)
+                      line-end))
+    :modes (gfm-mode markdown-mode org-mode text-mode))
+  (add-to-list 'flycheck-checkers 'write-good))
 
 ;;; Notification
 
@@ -65,8 +108,8 @@ The function returns nil, if the file does not exists."
 (use-package flycheck-posframe
   :custom ((flycheck-posframe-border-use-error-face t)
            (flycheck-posframe-border-width 1))
-  :hook (flycheck-mode . flycheck-posframe-mode)
-  :config (flycheck-posframe-configure-pretty-defaults))
+  :config (flycheck-posframe-configure-pretty-defaults)
+  :hook (flycheck-mode . flycheck-posframe-mode))
 
 ;;; Eglot
 
