1dd4b4c8d58b88089de92e28dd5b03fade0f3643
Taro Sato <okomestudio@gmail.com>
2026-08-13 20:14:22 -0700

    Refactor tesseract

--- lisp/subsys-ocr.el
+++ lisp/subsys-ocr.el
@@ -1,57 +1,46 @@
-;;; subsys-ocr.el --- subsys-ocr  -*- lexical-binding: t -*-
+;;; subsys-ocr.el --- OCR  -*- lexical-binding: t -*-
 ;;; Commentary:
 ;;
-;; The OCR subsystem.
+;; Configure the OCR subsystem.
 ;;
 ;;; Code:
 
-(defcustom tesseract-ok-paste-cmd "wl-paste -t %s"
-  "Image pasting shell command."
-  :type 'string
-  :group 'tessearact)
-
 (use-package tesseract
-  :bind ("C-x C-y" . tesseract-ok-ocr-clipboard-image)
+  ;; Tesseract OCR integration.
+  :bind ("C-x C-y" . tesseract-ocr-clipboard-image)
   :custom ((doc-view-scale-internally nil)
            (tessearct/default-language "eng"))
+  :config
+  (defun tesseract-ocr-clipboard-image ()
+    "Run OCR on an image in clipboard and paste into current position.
+With the prefix argument, the command will prompt for a language."
+    (interactive)
+    (let* ((image-type "image/png")
+           (lang (if current-prefix-arg
+                     (completing-read "OCR language: "
+                                      (tesseract/list-languages) nil t "")
+                   tesseract/current-language))
+           (cmd-paste (cond
+                       ;; Wayland:
+                       ((and (eq system-type 'gnu/linux)
+                             (or (eq window-system 'pgtk)
+                                 (getenv "WAYLAND_DISPLAY")))
+                        "wl-paste -t %s")
+                       ;; X:
+                       ((and (eq system-type 'gnu/linux)
+                             (and (eq window-system 'x)
+                                  (not (getenv "WAYLAND_DISPLAY"))))
+                        "xclip -selection clipboard -t %s -l 1 -o")))
+           (cmd (format "%s | tesseract -l %s stdin stdout"
+                        (format cmd-paste image-type) lang)))
+      (insert (shell-command-to-string cmd))))
+
   :commands (tesseract-change-language)
   :ensure-system-package
   ("/usr/bin/tesseract" . "sudo apt install -y tesseract-ocr")
-  ("/usr/share/tesseract-ocr/5/tessdata/eng.traineddata"
-   . "sudo apt install -y tesseract-ocr-eng")
-  ("/usr/share/tesseract-ocr/5/tessdata/jpn.traineddata"
-   . "sudo apt install -y tesseract-ocr-jpn")
-  ("/usr/share/tesseract-ocr/5/tessdata/jpn_vert.traineddata"
-   . "sudo apt install -y tesseract-ocr-jpn-vert")
-
-  :config
-  (defun tesseract-ok-ocr-clipboard-image (&optional arg)
-    "Run OCR on an image in clipboard and paste into current position.
-With a prefix argument, the function prompts for the language."
-    (interactive "P")
-    ;; (setq tesseract-ok-paste-cmd "wl-paste -t %s")
-    (let* ((image-type "image/png")
-           (cmd (format tesseract-ok-paste-cmd image-type))
-           (lang (pcase arg
-                   ('(4)
-                    (completing-read "OCR language: "
-                                     (tesseract/list-languages)
-                                     nil t ""))
-                   (_ tesseract/current-language))))
-      (insert (shell-command-to-string
-               (format "%s | tesseract -l %s stdin stdout" cmd lang))))))
-
-(use-package tesseract        ; for Wayland
-  :if (and (eq system-type 'gnu/linux)
-           (or (eq window-system 'pgtk)
-               (getenv "WAYLAND_DISPLAY")))
-  :custom (tesseract-ok-paste-cmd "wl-paste -t %s"))
-
-(use-package tesseract        ; for X
-  :if (and (eq system-type 'gnu/linux)
-           (and (eq window-system 'x)
-                (not (getenv "WAYLAND_DISPLAY"))))
-  :custom (tesseract-ok-paste-cmd "xclip -selection clipboard -t %s -l 1 -o"))
+  ("/usr/share/tesseract-ocr/5/tessdata/eng.traineddata" . "sudo apt install -y tesseract-ocr-eng")
+  ("/usr/share/tesseract-ocr/5/tessdata/jpn.traineddata" . "sudo apt install -y tesseract-ocr-jpn")
+  ("/usr/share/tesseract-ocr/5/tessdata/jpn_vert.traineddata" . "sudo apt install -y tesseract-ocr-jpn-vert"))
 
 (provide 'subsys-ocr)
 ;;; subsys-ocr.el ends here
