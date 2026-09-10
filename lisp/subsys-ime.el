eb0daef858bc5cdff644dc13b2f18df54c9d06f8
Taro Sato <okomestudio@gmail.com>
2026-09-10 15:33:24 -0700

    Refactor mozc

--- lisp/subsys-ime.el
+++ lisp/subsys-ime.el
@@ -73,97 +73,67 @@ This is set to nil when input method is not active.")
 (use-package mozc
   :bind (("C-z" . toggle-input-method) ("C-\\" . nil))
   :custom ((default-input-method "japanese-mozc")
-
-           ;; NOTE(2025-03-12): `posframe' would be best, but it frequently
-           ;; crashed with Cairo in a hard-to-debug way. `overlay' is sluggish.
-           ;; `echo-area' is sufficient and stable, but far from the point of
-           ;; input. `popup' seems to be the best compromise?
-           (mozc-candidate-style 'echo-area)
-
            (mozc-leim-title "🇯🇵"))
+  :init
+  ;; Setting this to non-nil uses the IME on OS:
+  (setq pgtk-use-im-context-on-new-connection nil)
+
+  (defun mozc-candidate-overlay-theme (theme)
+    "Style Mozc candidate overlays for THEME."
+    (with-eval-after-load 'mozc
+      (let ((height (face-attribute 'default :height)))
+        (let ((faces (append '(mozc-cand-overlay-even-face
+                               mozc-cand-overlay-odd-face
+                               mozc-cand-overlay-footer-face)
+                             (when (facep 'mozc-cand-overlay-description-face)
+                               '(mozc-cand-overlay-description-face))))
+              (parent (if (featurep 'corfu) 'corfu-default 'fixed-pitch)))
+          (dolist (face faces)
+            (set-face-attribute face nil
+                                :foreground (face-attribute parent :foreground)
+                                :background (face-attribute parent :background)
+                                :inherit parent :weight 'medium :height height)))
+        (let ((faces '(mozc-cand-overlay-focused-face))
+              (parent (if (featurep 'corfu) 'corfu-current 'fixed-pitch)))
+          (dolist (face faces)
+            (set-face-attribute face nil
+                                :foreground (face-attribute parent :foreground)
+                                :background (face-attribute parent :background)
+                                :inherit parent :weight 'medium :height height))))))
+
   :commands (toggle-input-method)
-  :config
-  (when nil
-    ;; NOTE(2025-05-26): Experiement with toggling IME.
-    ;;
-    ;; NOTE(2025-09-17): This extra toggle layer is unnecessary and often causes
-    ;; conflicts with existing features. Remove on the next clean-up.
-    (defun mozc-ok--im-deactivate (&rest r)
-      (deactivate-input-method))
-    (pcase-dolist (`(,feat ,fun) '((simple execute-extended-command)
-                                   (emacs completing-read)
-                                   (consult consult--read)))
-      (with-eval-after-load feat
-        (advice-add fun :before #'mozc-ok--im-deactivate)))))
+  :hook ((enable-theme-functions . mozc-candidate-overlay-theme)))
+
+;;; Candidate Styling
+;;
+;; NOTE(2025-03-12): `posframe' would be best, but it frequently crashed
+;; with Cairo in a hard-to-debug way. `overlay' is sluggish. `echo-area'
+;; is sufficient and stable, but far from the point of input. `popup'
+;; seems to be the best compromise?
 
 (use-package mozc
   :if (and (eq system-type 'gnu/linux) (memq window-system '(pgtk)))
-  :init
-  ;; Setting this to non-nil uses the IME on OS:
-  (setq pgtk-use-im-context-on-new-connection nil))
+  :custom (mozc-candidate-style 'overlay))
 
-;;; Posframe
+(use-package mozc-popup
+  :disabled
+  :after mozc
+  :hook ((on-first-input . (lambda () (require 'mozc-popup)))))
 
 (use-package mozc-cand-posframe
   ;; For `posframe', this package may be the simplest option.
   :disabled
-  :if (display-graphic-p)
+  :if (and (eq system-type 'gnu/linux) (not (memq window-system '(pgtk))))
   :after mozc
-  :init (require 'mozc-cand-posframe)
-  :config
-  (defun mozc-posframe-ok--theme (theme)
-    (require 'corfu)
-    (let* ((face-base 'corfu-default)
-           (face-current 'corfu-current)
-           (foreground (face-attribute face-base :foreground))
-           (background (face-attribute face-base :background)))
-      (dolist (face '(mozc-cand-posframe-normal-face
-                      mozc-cand-posframe-footer-face))
-        (set-face-attribute face nil
-                            :foreground foreground
-                            :background background
-                            :inherit face-base))
-      (set-face-attribute 'mozc-cand-posframe-focused-face nil
-                          :foreground (face-attribute face-current :foreground)
-                          :background (face-attribute face-current :background)
-                          :inherit face-current)))
-
-  :hook ((enable-theme-functions . mozc-posframe-ok--theme)))
+  :init (require 'mozc-cand-posframe))
 
 (use-package mozc-posframe
   ;; This might work, but not out of box.
-  :if (display-graphic-p)
+  :if (and (eq system-type 'gnu/linux) (not (memq window-system '(pgtk))))
   :custom (mozc-candidate-style 'posframe)
-  :config
-  (defun mozc-posframe-ok--theme (theme)
-    (require 'corfu)
-    (let* ((scale (pcase (frame-parameter nil 'background-mode)
-                    ('dark 1.07) ('light 0.93)))
-           (face-b 'corfu-default)
-           (fg-b (face-attribute face-b :foreground))
-           (bg-b (ok-face-color-scale
-                  (or (when (stringp (face-attribute face-b :background))
-                        (face-attribute face-b :background))
-                      (face-attribute 'default :background))
-                  scale))
-           (face-c 'corfu-current)
-           (fg-c (face-attribute face-c :foreground))
-           (bg-c (ok-face-color-scale
-                  (or (when (stringp (face-attribute face-c :background))
-                        (face-attribute face-c :background))
-                      (face-attribute 'default :background))
-                  scale)))
-      (dolist (face '(mozc-cand-overlay-even-face
-                      mozc-cand-overlay-odd-face
-                      mozc-cand-overlay-description-face
-                      mozc-cand-overlay-footer-face))
-        (set-face-attribute face nil
-                            :foreground fg-b :background bg-b :inherit face-b))
-      (set-face-attribute 'mozc-cand-overlay-focused-face nil
-                          :foreground fg-c :background bg-c :inherit face-c)))
+  :hook ((on-first-input . mozc-posframe-initialize)))
 
-  :hook ((on-first-input . mozc-posframe-initialize)
-         (enable-theme-functions . mozc-posframe-ok--theme)))
+;;; Migemo
 
 (use-package migemo
   :disabled
